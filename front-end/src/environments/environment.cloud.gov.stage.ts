36c1f647ec1f2a1348ffdb1a1d113cfa6c27aac4
Al Crowley <albert.crowley@tcg.com>
2022-01-31 15:01:17 -0500

    - Adding the stage environment.ts

--- front-end/src/environments/environment.cloud.gov.stage.ts
+++ front-end/src/environments/environment.cloud.gov.stage.ts
@@ -3,16 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  name: 'development',
-  apiUrl: 'http://localhost/api/v1',
-  appTitle: 'FECfile',
-  validateSuccess: 'All required fields have passed validation.',
-  awsRegion: 'us-east-1',
-  awsIdentityPoolId: 'us-east-1:f0f414b2-8e9f-4488-9cc1-34a5918a1a1d',
-  ACCESS_KEY: "_process.env.ACCESS_KEY", //TODO: fix this. Hacked to get Angular 8 upgrade working
-  SECRET_KEY: "_process.env.SECRET_KEY", //TODO: fix this. Hacked to get Angular 8 upgrade working
-  dcfConverterApiUrl: 'https://dev-efile-api.efdev.fec.gov/dcf_converter/v1'
+    production: false,
+    name: 'development',
+    apiUrl: 'https://fecfile-web-api-stage.app.cloud.gov/api/v1',
+    appTitle: 'FECfile',
+    validateSuccess: 'All required fields have passed validation.',
+    awsRegion: 'us-east-1',
+    awsIdentityPoolId: 'us-east-1:f0f414b2-8e9f-4488-9cc1-34a5918a1a1d',
+    ACCESS_KEY: "_process.env.ACCESS_KEY", //TODO: fix this. Hacked to get Angular 8 upgrade working
+    SECRET_KEY: "_process.env.SECRET_KEY", //TODO: fix this. Hacked to get Angular 8 upgrade working
+    dcfConverterApiUrl: 'https://dev-efile-api.efdev.fec.gov/dcf_converter/v1'
 };
 
 /*
