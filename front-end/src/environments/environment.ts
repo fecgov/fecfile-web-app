09aef868f023b38b7c96c1e59c8bbee61d2d1f8a
Al Crowley <albert.crowley@tcg.com>
2022-02-04 14:45:55 -0500

    * Fixing stage/prod env files to show correct name * Removing the unused ACCESS and SECRET keys in the environment files.

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -5,13 +5,11 @@
 export const environment = {
     production: false,
     name: 'development',
-    apiUrl: 'https://fecfile-web-api-dev.app.cloud.gov/api/v1',
+    apiUrl: 'https://localhost/api/v1',
     appTitle: 'FECfile',
     validateSuccess: 'All required fields have passed validation.',
     awsRegion: 'us-east-1',
     awsIdentityPoolId: 'us-east-1:f0f414b2-8e9f-4488-9cc1-34a5918a1a1d',
-    ACCESS_KEY: "_process.env.ACCESS_KEY", //TODO: fix this. Hacked to get Angular 8 upgrade working
-    SECRET_KEY: "_process.env.SECRET_KEY", //TODO: fix this. Hacked to get Angular 8 upgrade working
     dcfConverterApiUrl: 'https://dev-efile-api.efdev.fec.gov/dcf_converter/v1'
 };
 
