91e2ecd7ef92056a4b33b151a4ead82bfd4858d4
Matt Travers <matt.travers@tcg.com>
2022-03-13 16:05:19 -0400

    Fixed reference to missing style file

--- front-end/src/app/layout/sidebar/sidebar.component.ts
+++ front-end/src/app/layout/sidebar/sidebar.component.ts
@@ -4,7 +4,7 @@ import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss'],
+  // styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
   items: MenuItem[] = [];
