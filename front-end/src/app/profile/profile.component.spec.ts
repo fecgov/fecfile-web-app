566c780654cf8f2fbc1d2de05897f165967e388e
toddlees <todd.lees@tcg.com>
2022-04-05 11:08:26 -0400

    fixes testing modules for profile and fec-api

--- front-end/src/app/profile/profile.component.spec.ts
+++ front-end/src/app/profile/profile.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { ProfileComponent } from './profile.component';
 
@@ -7,7 +8,75 @@ describe('ProfileComponent', () => {
   let fixture: ComponentFixture<ProfileComponent>;
 
   beforeEach(async () => {
+    let userLoginData: any = {'committee_id':'C00601211'}
+    let committeeAccount: any = {
+      "affiliated_committee_name": "NONE",
+      "candidate_ids": [],
+      "city": "FORT LAUDERDALE",
+      "committee_id": "C00601211",
+      "committee_type": "O",
+      "committee_type_full": "Super PAC (Independent Expenditure-Only)",
+      "custodian_city": "FORT LAUDERDALE",
+      "custodian_name_1": "JOSH",
+      "custodian_name_2": "LAROSE",
+      "custodian_name_full": "LAROSE, JOSH",
+      "custodian_name_middle": null,
+      "custodian_name_prefix": null,
+      "custodian_name_suffix": null,
+      "custodian_name_title": "PRESIDENT",
+      "custodian_phone": "8007686650",
+      "custodian_state": "FL",
+      "custodian_street_1": "1900 WEST OAKLAND PARK BLVD.",
+      "custodian_street_2": "# 9961",
+      "custodian_zip": "33310",
+      "cycles": [2016],
+      "designation": "U",
+      "designation_full": "Unauthorized",
+      "email": "USPOLITICALACTIONCOMMITTEES@GMAIL.COM",
+      "fax": null,
+      "filing_frequency": "A",
+      "first_f1_date": "2016-01-01",
+      "first_file_date": "2016-01-01",
+      "form_type": "F1",
+      "last_f1_date": "2016-01-01",
+      "last_file_date": "2016-02-08",
+      "leadership_pac": null,
+      "lobbyist_registrant_pac": null,
+      "name": "WORLD CAPITALIST PARADISE",
+      "organization_type": null,
+      "organization_type_full": null,
+      "party": null,
+      "party_full": null,
+      "party_type": null,
+      "party_type_full": null,
+      "sponsor_candidate_ids": null,
+      "state": "FL",
+      "state_full": "Florida",
+      "street_1": "1900 WEST OAKLAND PARK BLVD.",
+      "street_2": "# 9961",
+      "treasurer_city": "FORT LAUDERDALE",
+      "treasurer_name": "LAROSE, JOSH",
+      "treasurer_name_1": "JOSH",
+      "treasurer_name_2": "LAROSE",
+      "treasurer_name_middle": null,
+      "treasurer_name_prefix": null,
+      "treasurer_name_suffix": null,
+      "treasurer_name_title": "TREASURER",
+      "treasurer_phone": "8007686650",
+      "treasurer_state": "FL",
+      "treasurer_street_1": "1900 WEST OAKLAND PARK BLVD.",
+      "treasurer_street_2": "# 9961",
+      "treasurer_zip": "33310",
+      "website": "WWW.UNITEDSTATESPOLITICALACTIONCOMMITTEESDIRECTORY.COM",
+      "zip": "33310",
+    };
     await TestBed.configureTestingModule({
+      providers: [ provideMockStore({
+        initialState: {fecfile_online_userLoginData: userLoginData, fecfile_online_committeeAccount: committeeAccount},
+        selectors: [
+          { selector: 'selectCommitteeAccount', value: committeeAccount }
+        ]
+      }) ],
       declarations: [ ProfileComponent ]
     })
     .compileComponents();
--- front-end/src/app/shared/services/FecApiService/fec-api.service.spec.ts
+++ front-end/src/app/shared/services/FecApiService/fec-api.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FecApiService } from './fec-api.service';
 
 describe('FecApiService', () => {
   let service: FecApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(FecApiService);
   });
 
