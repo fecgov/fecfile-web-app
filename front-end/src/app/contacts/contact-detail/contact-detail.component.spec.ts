1e1be9a910ef983ddf2f3a7faa39a93fc1701881
Matt Travers <matt.travers@tcg.com>
2022-04-08 21:32:03 -0400

    Updates to various unit tests

--- front-end/src/app/contacts/contact-detail/contact-detail.component.spec.ts
+++ front-end/src/app/contacts/contact-detail/contact-detail.component.spec.ts
@@ -1,14 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { MessageService } from 'primeng/api';
 import { ContactDetailComponent } from './contact-detail.component';
+import { UserLoginData } from '../../shared/models/user.model';
+import { Roles } from '../../shared/models/role.model';
+import { FormBuilder } from '@angular/forms';
+import { DialogModule } from 'primeng/dialog';
 
 describe('ContactDetailComponent', () => {
   let component: ContactDetailComponent;
   let fixture: ComponentFixture<ContactDetailComponent>;
 
   beforeEach(async () => {
+    const userLoginData: UserLoginData = {
+      committee_id: 'C00000000',
+      email: 'email@fec.com',
+      role: Roles.COMMITTEE_ADMIN,
+      token: 'jwttokenstring',
+    };
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, DialogModule],
       declarations: [ContactDetailComponent],
+      providers: [
+        FormBuilder,
+        MessageService,
+        provideMockStore({
+          initialState: { fecfile_online_userLoginData: userLoginData },
+          selectors: [{ selector: 'selectUserLoginData', value: userLoginData }],
+        }),
+      ],
     }).compileComponents();
   });
 
@@ -18,7 +39,7 @@ describe('ContactDetailComponent', () => {
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
--- front-end/src/app/shared/services/contact.service.spec.ts
+++ front-end/src/app/shared/services/contact.service.spec.ts
@@ -1,16 +1,66 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { ContactService } from './contact.service';
+import { UserLoginData } from 'app/shared/models/user.model';
+import { Contact } from '../models/contact.model';
+import { ListRestResponse } from 'app/shared/models/rest-api.model';
+import { environment } from 'environments/environment';
 
 describe('ContactService', () => {
   let service: ContactService;
+  const userLoginData: UserLoginData = {
+    committee_id: 'C00101212',
+    email: 'test@fec.gov',
+    role: null,
+    token: null,
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        provideMockStore({
+          initialState: { fecfile_online_userLoginData: userLoginData },
+          selectors: [{ selector: 'selectUserLoginData', value: userLoginData }],
+        }),
+      ],
+    });
+
+    // httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ContactService);
   });
 
-  xit('should be created', () => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  // describe('#getTableData()', () => {
+  //   it('should return a list of contacts', () => {
+  //     const mockResponse: ListRestResponse = {
+  //       count: 1,
+  //       next: 'http://next-page',
+  //       previous: 'http://previous-page',
+  //       results: [
+  //         Contact.fromJSON({
+  //           id: 'C00000001'
+  //         }),
+  //         Contact.fromJSON({
+  //           id: 'C00000002'
+  //         }),
+  //       ];
+  //     };
+
+  //     service.getTableData().subscribe((response: ListRestResponse) => {
+  //       expect(response).toEqual(mockResponse);
+  //     });
+
+  //     const req = httpTestingController.expectOne(
+  //       `${environment.apiUrl}/contacts/?page=1`
+
+  //     expect(req.request.method).toEqual('GET');
+  //     req.flush(response);
+  //   });
+  // });
 });
