475e890e28d1c2edce8292793ae7c732b9747b6a
Matt Travers <matt.travers@tcg.com>
2022-04-20 17:10:53 -0400

    Added unit test for table list base component

---
+++ front-end/src/app/shared/components/table-list-base.component.spec.ts
@@ -0,0 +1,80 @@
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { Observable, of } from 'rxjs';
+import { TableListService } from '../services/table-list-service.interface';
+import { ListRestResponse } from '../models/rest-api.model';
+import { TableListBaseComponent } from './table-list-base.component';
+
+class TestTableListBaseComponent extends TableListBaseComponent<string> {
+  protected getEmptyItem(): string {
+    return '';
+  }
+
+  public override loadItemService(itemService: TableListService<string>) {
+    this.itemService = itemService;
+  }
+}
+
+class TestTableListService implements TableListService<string> {
+  getTableData(pageNumber: number): Observable<ListRestResponse> {
+    return of({
+      count: pageNumber,
+      next: 'https://next',
+      previous: 'https://previous',
+      results: ['abc', 'def'],
+    });
+  }
+  create(item: string): Observable<string> {
+    return of(item);
+  }
+  update(item: string): Observable<string> {
+    return of(item);
+  }
+  delete(): Observable<null> {
+    return of(null);
+  }
+}
+
+describe('TableListBaseComponent', () => {
+  let component: TestTableListBaseComponent;
+  let fixture: ComponentFixture<TestTableListBaseComponent>;
+  let testTableListService: TestTableListService;
+  let confirmationService: ConfirmationService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TestTableListBaseComponent],
+      providers: [ConfirmationService, MessageService, TestTableListService],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    testTableListService = TestBed.inject(TestTableListService);
+    confirmationService = TestBed.inject(ConfirmationService);
+    fixture = TestBed.createComponent(TestTableListBaseComponent);
+    component = fixture.componentInstance;
+    component.loadItemService(testTableListService);
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('#loadTableItems should load items', () => {
+    component.loadTableItems({});
+    expect(component.items[0]).toBe('abc');
+    expect(component.totalItems).toBe(1);
+    expect(component.loading).toBe(false);
+  });
+
+  it('#onSelectAllChange should update selected property values', () => {
+    component.onSelectAllChange({ checked: true, event: {} as PointerEvent });
+    expect(component.selectedItems[0]).toBe('abc');
+    expect(component.selectAll).toBe(true);
+
+    component.onSelectAllChange({ checked: false, event: {} as PointerEvent });
+    expect(component.selectedItems.length).toBe(0);
+    expect(component.selectAll).toBe(false);
+  });
+});
