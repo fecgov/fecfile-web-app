ac69b11ebb2fae313808af817abb05bc4def2ce5
Matt Travers <matt.travers@tcg.com>
2022-04-26 14:09:46 -0400

    Removed code compile errors

--- front-end/src/app/reports/report-list/report-list.component.ts
+++ front-end/src/app/reports/report-list/report-list.component.ts
@@ -3,19 +3,19 @@ import { ConfirmationService, MessageService } from 'primeng/api';
 import { TableListBaseComponent } from 'app/shared/components/table-list-base/table-list-base.component';
 
 import { F3xSummaryService } from '../../shared/services/f3x-summary.service';
+import { F3xSummary } from 'app/shared/models/f3x-summary.model';
 
 @Component({
-  selector: 'app-contact-list',
-  templateUrl: './contact-list.component.html',
+  selector: 'app-report-list',
+  templateUrl: './report-list.component.html',
 })
-export class ReportListComponent extends TableListBaseComponent<F3XSummary> implements OnInit {
-  override item: Contact = new Contact();
-  contactTypeLabels: LabelList = ContactTypeLabels;
+export class ReportListComponent extends TableListBaseComponent<F3xSummary> implements OnInit {
+  // override item: Contact = new Contact();
+  // contactTypeLabels: LabelList = ContactTypeLabels;
 
   constructor(
     protected override messageService: MessageService,
-    protected override confirmationService: ConfirmationService,
-    protected override itemService: ReportService
+    protected override confirmationService: ConfirmationService // protected override itemService: ReportService
   ) {
     super(messageService, confirmationService);
   }
@@ -25,18 +25,18 @@ export class ReportListComponent extends TableListBaseComponent<F3XSummary> impl
     this.loadItemService(this.itemService);
   }
 
-  protected getEmptyItem(): Contact {
-    return new Contact();
+  protected getEmptyItem(): F3xSummary {
+    return new F3xSummary();
   }
 
   public override addItem() {
     super.addItem();
-    this.isNewContact = true;
+    // this.isNewF3xSummary = true;
   }
 
-  public override editItem(item: Contact) {
+  public override editItem(item: F3xSummary) {
     super.editItem(item);
-    this.isNewContact = false;
+    // this.isNewContact = false;
   }
 
   /**
@@ -44,11 +44,11 @@ export class ReportListComponent extends TableListBaseComponent<F3XSummary> impl
    * @param item
    * @returns {string} Returns the appropriate name of the contact for display in the table.
    */
-  public displayName(item: Contact): string {
-    if ([ContactTypes.INDIVIDUAL, ContactTypes.CANDIDATE].includes(item.type)) {
-      return `${item.first_name} ${item.last_name}`;
-    } else {
-      return item.name || '';
-    }
-  }
+  // public displayName(item: Contact): string {
+  //   if ([ContactTypes.INDIVIDUAL, ContactTypes.CANDIDATE].includes(item.type)) {
+  //     return `${item.first_name} ${item.last_name}`;
+  //   } else {
+  //     return item.name || '';
+  //   }
+  // }
 }
--- front-end/src/app/shared/services/report.service.ts
+++ front-end/src/app/shared/services/report.service.ts
@@ -1,39 +1,38 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Report } from '../models/report.model';
-import { TableListService } from '../interfaces/table-list-service.interface';
-import { ListRestResponse } from '../models/rest-api.model';
+// import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+// import { Report } from '../models/report.model';
+// import { TableListService } from '../interfaces/table-list-service.interface';
+// import { ListRestResponse } from '../models/rest-api.model';
+// import { F3xSummaryService } from './f3x-summary.service';
+// import { F3xSummary } from '../models/f3x-summary.model';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class ReportService implements TableListService<Report> {
-  constructor() {}
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class ReportService implements TableListService<Report> {
+//   constructor(private f3xSummaryService: F3xSummaryService) {}
 
-  public getTableData(pageNumber = 1): Observable<ListRestResponse> {
-    return this.apiService.get<ListRestResponse>(`/f3x-summaries/?page=${pageNumber}`).pipe(
-      map((response: ListRestResponse) => {
-        response.results = response.results.map((item) => F3xSummary.fromJSON(item));
-        return response;
-      })
-    );
-  }
+//   public getTableData(pageNumber = 1): Observable<ListRestResponse> {
+//     return this.apiService.get<ListRestResponse>(`/f3x-summaries/?page=${pageNumber}`).pipe(
+//       map((response: ListRestResponse) => {
+//         response.results = response.results.map((item) => F3xSummary.fromJSON(item));
+//         return response;
+//       })
+//     );
+//   }
 
-  public create(f3xSummary: F3xSummary): Observable<F3xSummary> {
-    const payload = f3xSummary.toJson();
-    return this.apiService
-      .post<F3xSummary>(`/f3x-summaries/`, payload)
-      .pipe(map((response) => F3xSummary.fromJSON(response)));
-  }
+//   public create(data: Report): Observable<F3xSummary> {
+//     return this.f3xSummaryService.create(data);
+//   }
 
-  public update(f3xSummary: F3xSummary): Observable<F3xSummary> {
-    const payload = f3xSummary.toJson();
-    return this.apiService
-      .put<F3xSummary>(`/f3x-summaries/${f3xSummary.id}/`, payload)
-      .pipe(map((response) => F3xSummary.fromJSON(response)));
-  }
+//   public update(f3xSummary: F3xSummary): Observable<F3xSummary> {
+//     const payload = f3xSummary.toJson();
+//     return this.apiService
+//       .put<F3xSummary>(`/f3x-summaries/${f3xSummary.id}/`, payload)
+//       .pipe(map((response) => F3xSummary.fromJSON(response)));
+//   }
 
-  public delete(f3xSummary: F3xSummary): Observable<null> {
-    return this.apiService.delete<null>(`/f3x-summaries/${f3xSummary.id}`);
-  }
-}
+//   public delete(f3xSummary: F3xSummary): Observable<null> {
+//     return this.apiService.delete<null>(`/f3x-summaries/${f3xSummary.id}`);
+//   }
+// }
