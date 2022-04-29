2436792b2f2219af56e9af248128a17d060c653f
Elaine Krauss <elaine@ekrauss.com>
2022-04-29 17:02:13 -0400

    Refactored and expanded functionality to include testing submitting email, committee ID, and password.  Many more tests to come

--- front-end/cypress/integration/login_spec.ts
+++ front-end/cypress/integration/login_spec.ts
@@ -1,21 +1,45 @@
 // @ts-check
+//import { environment } from "src/environments/environment.local.ts";
+
+const email         = "rlanz@fec.gov";
+const committeeID   = "C00601211";
+const testPassword  = "test";
+
+const fieldEmail      = ".login-email-id";
+const fieldCommittee  = ".login-committee-id";
+const fieldPassword   = ".login-password";
+
+function fill_login_form(){
+  cy.get(fieldEmail)
+    .type(email);
+  cy.get(fieldCommittee)
+    .type(committeeID);
+  cy.get(fieldPassword)
+    .type(testPassword); 
+}
 
 describe('Testing login', () => {
-  
-  it('Logs in', () => {
-    const email = "rlanz@fec.gov";
-    const committeeID = "C00601211";
-    const testPwd = "test";
-
-    cy.visit('/');
-    cy.get('.login-email-id')
-      .type(email)
+    beforeEach(() => {
+      cy.visit('/');
+    });
+    
+  it('Accepts input', () => {
+    fill_login_form();
+    
+    cy.get(fieldEmail)
       .should('have.value', email);
-    cy.get('.login-committee-id')
-      .type(committeeID)
+    cy.get(fieldCommittee)
       .should('have.value', committeeID);
-    cy.get('.login-password')
-      .type(testPwd)
-      .should('have.value', testPwd);
+    cy.get(fieldPassword)
+      .should('have.value', testPassword);
+  });
+
+  it('Logs in', () => {
+    fill_login_form();
+
+    cy.get(fieldPassword)
+      .type('{enter}');
+    cy.url()
+      .should('contain', "/twoFactLogin");
   });
 });
