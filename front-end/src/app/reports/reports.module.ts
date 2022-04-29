5b9ea12010179f688b7696161fd0ed7286011824
toddlees <todd.lees@tcg.com>
2022-04-29 16:06:11 -0400

    wip create report step 1

--- front-end/src/app/reports/reports.module.ts
+++ front-end/src/app/reports/reports.module.ts
@@ -2,22 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportListComponent } from './report-list/report-list.component';
-import { ReportTypeComponent } from './report-detail/report-type.component';
-import { FormsModule } from '@angular/forms';
+import { CreateReportStep1 } from './create-workflow/create-report-step-1.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // PrimeNG
 import { ButtonModule } from 'primeng/button';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { CalendarModule } from 'primeng/calendar';
 import { SharedModule } from 'app/shared/shared.module';
+import { DropdownModule } from 'primeng/dropdown';
 
 @NgModule({
-  declarations: [ReportListComponent, ReportTypeComponent],
+  declarations: [ReportListComponent, CreateReportStep1],
   imports: [
     FormsModule,
+    ReactiveFormsModule,
     ButtonModule,
     CalendarModule,
     CommonModule,
+    DropdownModule,
     ReportsRoutingModule,
     RadioButtonModule,
     SharedModule,
