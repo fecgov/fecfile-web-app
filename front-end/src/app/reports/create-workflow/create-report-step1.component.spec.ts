c5c550e03db7a5b5d9470e3ab4e69a64424de5ce
toddlees <todd.lees@tcg.com>
2022-05-02 23:19:05 -0400

    fix unit tests

--- front-end/src/app/reports/create-workflow/create-report-step1.component.spec.ts
+++ front-end/src/app/reports/create-workflow/create-report-step1.component.spec.ts
@@ -1,4 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { provideMockStore } from '@ngrx/store/testing';
+import { UserLoginData } from 'app/shared/models/user.model';
+import { LabelPipe } from 'app/shared/pipes/label.pipe';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { SelectButtonModule } from 'primeng/selectbutton';
 import { CreateReportStep1Component } from './create-report-step1.component';
 
 describe('CreateReportStep1Component', () => {
@@ -6,8 +13,22 @@ describe('CreateReportStep1Component', () => {
   let fixture: ComponentFixture<CreateReportStep1Component>;
 
   beforeEach(async () => {
+    const userLoginData: UserLoginData = {
+      committee_id: 'C00000000',
+      email: 'email@fec.com',
+      is_allowed: true,
+      token: 'jwttokenstring',
+    };
     await TestBed.configureTestingModule({
-      declarations: [CreateReportStep1Component],
+      declarations: [CreateReportStep1Component, LabelPipe],
+      providers: [
+        FormBuilder,
+        MessageService,
+        provideMockStore({
+          initialState: { fecfile_online_userLoginData: userLoginData },
+          selectors: [{ selector: 'selectUserLoginData', value: userLoginData }],
+        }),
+      ],
     }).compileComponents();
   });
 
