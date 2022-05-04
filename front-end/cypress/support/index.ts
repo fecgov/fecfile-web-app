a7e53942895d65421dc40a75305f85fe7f34c1c4
Elaine Krauss <elaine@ekrauss.com>
2022-05-04 10:37:51 -0400

    Added a custom Cypress command to index.ts that handles logging in.  Eventually it should be expanded with different roles and options

--- front-end/cypress/support/index.ts
+++ front-end/cypress/support/index.ts
@@ -15,3 +15,51 @@
 
 // When a command from ./commands is ready to use, import with `import './commands'` syntax
 // import './commands';
+
+/*
+
+    Supporting Functions
+
+*/
+
+function support_login(){
+
+  //Dummy login information
+  const email         = Cypress.env("EMAIL");
+  const committeeID   = Cypress.env("COMMITTEE_ID");
+  const testPassword  = Cypress.env("PASSWORD");
+  const testPIN       = Cypress.env("PIN");
+
+  //login page form-fields' id's (or classes where elements have no id's)
+  const fieldEmail      = "#login-email-id";
+  const fieldCommittee  = "#login-committee-id";
+  const fieldPassword   = "#login-password";
+
+  //two-factor authentication page form-fields' ids
+  const fieldTwoFactorEmail     = "#email";
+  const fieldTwoFactorSubmit    = ".action__btn.next"; 
+
+  //security code page form-fields' ids
+  const fieldSecurityCodeText = ".form-control"
+
+  cy.visit("/");
+  
+  cy.get(fieldEmail)
+  .type(email);
+  cy.get(fieldCommittee)
+  .type(committeeID);
+  cy.get(fieldPassword)
+  .type(testPassword)
+  .type("{enter}");
+
+  cy.get(fieldTwoFactorEmail)
+  .check();
+  cy.get(fieldTwoFactorSubmit)
+  .click();
+
+  cy.get(fieldSecurityCodeText)
+  .type(testPIN)
+  .type("{enter}");
+}
+
+Cypress.Commands.add("login",support_login);
