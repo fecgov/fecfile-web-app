12a4dc08423eb29b090bbeffd2a3c81861cdb575
Elaine Krauss <elaine@ekrauss.com>
2022-05-06 16:22:52 -0400

    QA Script #110, Steps 1-10 are complete.  There's a bug in Cypress's interaction with two dropdown menus that needs to be resolved

--- front-end/cypress/integration/sprint-6-test-110.spec.ts
+++ front-end/cypress/integration/sprint-6-test-110.spec.ts
@@ -1,41 +1,24 @@
 // @ts-check
 
+import { GenerateContactObject } from "../support/contacts.spec";
+
 /*
 
         Individual
 
 */
 
-//      Contact Values
-const individual_last_name    = "Jorgenson";
-const individual_first_name   = "Bill";
-const individual_middle_name  = "Thomas";
-const individual_prefix       = "Dr";
-const individual_suffix       = "IV";
-
-//      Address Values
-const individual_street       = "118 Test Rd";
-const individual_apartment    = "Apt 22";
-const individual_city         = "Testopolis";
-const individual_zip          = "12121";
-const individual_state        = "Maryland";
-const individual_phone        = "8000010001";
-
-//      Employer Values
-const individual_employer     = "University of Testopolis";
-const individual_occupation   = "Historian";
-
 describe('QA Test Script #110 (Sprint 6)', () => {
   beforeEach(() => {
-    cy.login();
+    //cy.login();
   });
 
 
   afterEach(() => {
-    cy.logout();
+    //cy.logout();
   });
 
-  it('Navigate to contacts page (Step #1)', () => {
+  it('Step 1: Navigate to contacts page', () => {
       cy.url()
         .should("contain","/dashboard");
       cy.get(".p-menubar")
@@ -46,75 +29,73 @@ describe('QA Test Script #110 (Sprint 6)', () => {
         .should("contain","/contacts");
   });
 
-  it("Creates a random contact", () => {
-    var contact = cy.CreateContactIndividual();
+  it("Steps 2-5: Creates a contact", () => {
+    const contact = GenerateContactObject();
+    cy.CreateContactIndividual(contact, false);
 
-  });
-
-  it('Create new Contact, Individual (Steps 2-11)', () => {
-    cy.visit('/contacts');
-    cy.wait(500);
-
-    cy.get('#button-contacts-new')
-      .click();
-
-    cy.get("app-contact-detail")
-      .should("contain", "Add Contact");
-
-    //Contact
     cy.get("#last_name")
-      .type(individual_last_name)
-      .should("have.value", individual_last_name);
+      .should("have.value", contact["last_name"]);
     cy.get("#first_name")
-      .type(individual_first_name)
-      .should("have.value", individual_first_name);
+      .should("have.value", contact["first_name"]);
     cy.get("#middle_name")
-      .type(individual_middle_name)
-      .should("have.value", individual_middle_name);
+      .should("have.value", contact["middle_name"]);
     cy.get("#prefix")
-      .type(individual_prefix)
-      .should("have.value", individual_prefix);
+      .should("have.value", contact["prefix"]);
     cy.get("#suffix")
-      .type(individual_suffix)
-      .should("have.value", individual_suffix);
+      .should("have.value", contact["suffix"]);
 
     //Address
     cy.get("#street_1")
-      .type(individual_street)
-      .should("have.value", individual_street);
+      .should("have.value", contact["street"]);
     cy.get("#street_2")
-      .type(individual_apartment)
-      .should("have.value", individual_apartment);
+      .should("have.value", contact["apartment"]);
     cy.get("#city")
-      .type(individual_city)
-      .should("have.value", individual_city);
+      .should("have.value", contact["city"]);
     cy.get("#zip")
-      .type(individual_zip)
-      .should("have.value", individual_zip);
+      .should("have.value", contact["zip"]);
     cy.get("#telephone")
-      .type(individual_phone)
-      .should("have.value", individual_phone);
+      .should("have.value", contact["phone"]);
     cy.get("[inputid='state']") //Get the field for State based on its "inputid" attribute
       .find("div [role='button']") //Get the field's child element, div, based on its role element
       .click();
     cy.get("span")
-      .contains(individual_state)
+      .contains(contact["state"])
       .click();
     cy.get("[inputid='state']") //Gets the field for the input for State
-      .should("contain", individual_state);
+      .should("contain", contact["state"]);
 
     cy.get("#employer")
-      .type(individual_employer)
-      .should("have.value", individual_employer);
+      .should("have.value", contact["employer"]);
     cy.get("#occupation")
-      .type(individual_occupation)
-      .should("have.value", individual_occupation);
+      .should("have.value", contact["occupation"]);
 
     cy.get(".p-button-primary > .p-button-label")
       .contains("Save")
-      .click();
+      .click(); 
 
-    cy.pause();
+    cy.wait(250);
+    cy.get("tbody")
+      .find("tr")
+      .contains(`${contact["first_name"]} ${contact["last_name"]}`).parent()
+      .should("contain","Individual")
+      .should("contain",contact["employer"])
+      .should("contain",contact["occupation"]);
+  });
+
+  it('Steps 6-11: Test "Save & Add More"', () => {
+    cy.CreateContactIndividual(GenerateContactObject(), false);
+
+    cy.get(".p-button-label")
+      .contains("Save & Add More")
+      .click()
+    
+    cy.get(".p-toast-message")
+      .should("contain","Contact Created");
+
+    cy.get("#last_name")
+      .should("have.value", "");
+    cy.get("#first_name")
+      .should("have.value", "");
   });
 
 });
--- front-end/cypress/support/contacts.spec.ts
+++ front-end/cypress/support/contacts.spec.ts
@@ -5,30 +5,55 @@ function RandomInt(min=0, max=1){
     return Math.round(Math.random()*(max-min))+min;
 }
 
-export function CreateContactIndividual(contact_given={}, save=true){
-
+export function GenerateContactObject(contact_given={}){
   const contact_random = { //_.sample : standard js object method (hence _.); takes a random element from a given list.  Much more readable than [randomint % list.length] on every list
+    //Names were provided by a random name generator
     last_name:_.sample(['Savage','Skinner','Glenn','Barajas','Moreno','King','Meadows','Grimes','Glover','Carrillo','Stephens','Pacheco','Rios','Olsen','Dorsey','Livingston','Mclaughlin','Orozco','Preston','Rush','Malone','Graham','Patton','Middleton','Boyer']),
     first_name:_.sample(['Khalil','Anika','Demarion','Kenneth','Albert','Ainsley','Brenton','Mira','Kianna','Adalynn','Bentley','Aden','Cristina','Dayanara','Ronan','Makena','Ramon','Gaige','Matilda','Sebastian','Octavio','Walker','Gia','Gloria','Theresa']),
     middle_name:_.sample(['Kenzie','Nathaly','Wendy','Deborah','Eve','Raiden','Adam','Teagan','Erika','Levi','Xzavier','Myla','Trystan','Reagan','Barrett','Leroy','Jesus','Adrien','Alexia','Harley','Zaire','Bernard','Kade','Anabelle','Sincere']),
-    prefix:_.sample(['Dr','','','','','','']), //Extra empty strings artificially raises the likelihood of the value being an empty string
+    prefix:_.sample(['Dr','','','','','','']), //Extra empty strings artificially raise the likelihood of the value being an empty string
     suffix:_.sample(['Sr', 'Jr', 'III', 'IV', 'V', 'VI','','','','','','','','','','','','','']),
     street:`${RandomInt(1,9999)} Test ${_.sample(['Rd','Ln','St','Ave','Ct'])}`,
     apartment:_.sample([`Apt ${RandomInt(1,200)}`, '', '']),
-    city:"Testopolis",
-    zip:"12121",
-    state:_.sample(['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'IllinoisIndiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'MontanaNebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'PennsylvaniaRhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']),
+    city:_.sample(["Testopolis","Testville","Testburg","Testland","Testford","Testham","Testmouth","Testborough","Testbury"]),
+    zip:RandomInt(1,9999).toString().padStart(5, "0"), //Zipcodes starting with 0 are pretty uncommon
+    state:_.sample(['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois','Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania','Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']),
     phone:`${RandomInt(1,999).toString().padStart(3, "0")}${RandomInt(1000000, 1999999).toString()}`, //Area code from 001 to 999 || 7-digit number starting with between 100 to 199 because (supposedly) that range always corresponds to fake numbers
     employer:"",
+    //Jobs provided by another random generator
     occupation:_.sample(['Police Officer','Radiologic Technologist','Accountant','Statistician','Surveyor','Physician','Teacher','Microbiologist','Insurance Agent','Economist','Childcare worker','Automotive mechanic','Physical Therapist','Coach','Librarian','Painter','High','Software Engineer','Editor','Interpreter','Chef','','','','','','','','','','','','','','','']),
   }
 
   let contact = {...contact_random, ...contact_given}; //Merges the provided contact with the randomly generated one, overwriting the random one with any fields found in the provided
- 
+  return contact
+}
+
+
+
+export function CreateContactIndividual(contact={}, save=true){
   cy.visit('/contacts');
 
   cy.get('#button-contacts-new')
     .click();
+  cy.wait(250);
+
+  
+  cy.get("[inputid='type']")
+    .find("div[role='button']")
+    .click()
+  cy.get("span")
+    .contains("Candidate")
+    .click();
+  //cy.get("p-dropdownitem[ng-reflect-label='Candidate']"
+  //  .click();
+
+  cy.get("[inputid='type']") //Get the field for State based on its "inputid" attribute
+    .find("div [role='button']") //Get the field's child element, div, based on its role element
+    .click();
+
+  
+  cy.wait(100);
+  cy.pause();
 
   //Contact
   cy.get("#last_name")
@@ -72,6 +97,11 @@ export function CreateContactIndividual(contact_given={}, save=true){
       .click();
   }
 
-  cy.wait(500); //Gives the database time to process the request.  It gets a little funky otherwise...
-  return cy.wrap(contact);
+  const promise = new Promise<any>( (resolve, reject) =>{
+    setTimeout( function(): void {
+      resolve( contact );
+    }, 2500);
+  });
+
+  cy.wait(250); //Gives the database time to process the request.  It gets a little funky otherwise...
 }
