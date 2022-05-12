a08bfde165f05c639125fa9fe5a32b27299d378c
toddlees <todd.lees@tcg.com>
2022-05-12 12:19:52 -0400

    update tests

--- front-end/src/app/shared/resolvers/transaction.resolver.spec.ts
+++ front-end/src/app/shared/resolvers/transaction.resolver.spec.ts
@@ -1,8 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { ActivatedRouteSnapshot, convertToParamMap } from '@angular/router';
 import { TransactionMeta } from '../interfaces/transaction-meta.interface';
+// Import transaction schemas
+import { schema as OFFSET_TO_OPEX } from 'fecfile-validate/fecfile_validate_js/dist/OFFSET_TO_OPEX';
 
 import { TransactionResolver } from './transaction.resolver';
+import { SchATransaction } from '../models/scha-transaction.model';
+import { Schedule } from '../interfaces/schedule.interface';
 
 describe('TransactionResolver', () => {
   let resolver: TransactionResolver;
@@ -15,9 +19,12 @@ describe('TransactionResolver', () => {
   it('should be created', () => {
     expect(resolver).toBeTruthy();
   });
-  it('should return a tramsaction', () => {
+  it('should return a transaction', () => {
     const transaction: TransactionMeta = {
-      scheduleId: '999',
+      scheduleId: 'A',
+      componentGroupId: 'B',
+      schema: OFFSET_TO_OPEX,
+      transaction: { id: 999, transaction_type_identifier: 'OFFSET_TO_OPEX' } as Schedule,
     } as TransactionMeta;
     const route = {
       paramMap: convertToParamMap({ transactionId: '999' }),
