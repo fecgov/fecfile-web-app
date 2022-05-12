20a1882f3c6e4328a6f34ec136af0e7eefa5455d
Elaine Krauss <elaine@ekrauss.com>
2022-05-12 17:00:57 -0400

    Technically satisfies requirements of QA Script #137, but it will need further work

--- front-end/cypress/integration/sprint-7-test-137.spec.ts
+++ front-end/cypress/integration/sprint-7-test-137.spec.ts
@@ -4,13 +4,33 @@ describe('QA Test Script #137 (Sprint 7)', () => {
   it('Step 1: Navigate to contacts page', () => {
     cy.visit('/dashboard');
     cy.url().should('contain', '/dashboard');
-    cy.get('.p-menubar').find('.p-menuitem-link').contains('Contacts').click();
-    cy.url().should('contain', '/contacts');
+    cy.get('.p-menubar').find('.p-menuitem-link').contains('Reports').click();
+    cy.url().should('contain', '/reports');
   });
 
-  it('Step 2: Open a New Contact', () => {
-    cy.get("button[label='New']").click();
+  it('Step 2: Open a New Report', () => {
+    cy.get("button[label='Create a new report']").click();
     cy.wait(50);
-    cy.get("div[role='dialog']").contains('Add Contact').should('exist');
+    cy.get('app-create-f3x-step1').contains('FEC Form 3X').should('exist');
+  });
+
+  it('Step 3: Check for the existence of the FROM and TO fields', () => {
+    cy.get('label[for="coverage_from_date"]').should('exist');
+    cy.get('label[for="coverage_through_date"]').should('exist');
+  });
+
+  it('Step 4: Enter any date into the FROM and TO fields', () => {
+    cy.get('p-calendar[FormControlName="coverage_from_date"]').click();
+    cy.get('.p-datepicker-group').find('td').first().click();
+    cy.wait(250);
+
+    cy.get('p-calendar[FormControlName="coverage_through_date"]').click();
+    cy.get('.p-datepicker-group').find('td').first().click();
+    cy.wait(250);
+  });
+
+  it('Step 5: Checks that the dates entered are displayed in their fields', () => {
+    cy.get('p-calendar[FormControlName="coverage_from_date"]').find('input').should('have.value', '05/01/2022');
+    cy.get('p-calendar[FormControlName="coverage_through_date"]').find('input').should('have.value', '05/01/2022');
   });
 });
