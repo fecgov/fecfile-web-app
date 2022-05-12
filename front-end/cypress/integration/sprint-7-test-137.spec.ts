9ead60ea2cab9fd5369f55604e3115ffc03571f1
Elaine Krauss <elaine@ekrauss.com>
2022-05-12 11:10:03 -0400

    Starts test script #137

---
+++ front-end/cypress/integration/sprint-7-test-137.spec.ts
@@ -0,0 +1,16 @@
+// @ts-check
+
+describe('QA Test Script #119 (Sprint 7)', () => {
+  it('Step 1: Navigate to contacts page', () => {
+    cy.visit('/dashboard');
+    cy.url().should('contain', '/dashboard');
+    cy.get('.p-menubar').find('.p-menuitem-link').contains('Contacts').click();
+    cy.url().should('contain', '/contacts');
+  });
+
+  it('Step 2: Open a New Contact', () => {
+    cy.get("button[label='New']").click();
+    cy.wait(50);
+    cy.get("div[role='dialog']").contains('Add Contact').should('exist');
+  });
+});
