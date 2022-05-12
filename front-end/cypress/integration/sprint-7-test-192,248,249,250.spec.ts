45f528656f44edf2a49af799aa71b59e66161017
Elaine Krauss <elaine@ekrauss.com>
2022-05-12 15:15:57 -0400

    Little bit of cleanup

--- front-end/cypress/integration/sprint-7-test-192,248,249,250.spec.ts
+++ front-end/cypress/integration/sprint-7-test-192,248,249,250.spec.ts
@@ -63,7 +63,7 @@ function randomString(length, type = 'alphanumeric') {
   // prettier-ignore
   const alphabet =   ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9',];
   // prettier-ignore
-  const numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; //
+  const numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
   var characters = [];
   if (type == 'alphanumeric') {
