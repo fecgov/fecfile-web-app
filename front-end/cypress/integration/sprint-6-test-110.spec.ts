9006cd9a51b1e8afaf11887d9b126b1f876d3e4d
Elaine Krauss <elaine@ekrauss.com>
2022-05-17 11:39:57 -0400

    Updated to better reflect team style guidelines

--- front-end/cypress/integration/sprint-6-test-110.spec.ts
+++ front-end/cypress/integration/sprint-6-test-110.spec.ts
@@ -1,8 +1,6 @@
 // @ts-check
 
-import { before } from 'lodash';
-import { isContext } from 'vm';
-import { GenerateContactObject, EnterContact } from '../support/contacts.spec';
+import { GenerateContactObject } from '../support/contacts.spec';
 
 /*
 
@@ -10,8 +8,8 @@ import { GenerateContactObject, EnterContact } from '../support/contacts.spec';
 
 */
 
-var contact_type;
-var contacts = { Individual: {}, Candidate: {}, Committee: {}, Organization: {} };
+let ContactType: string;
+let Contacts: object = { Individual: {}, Candidate: {}, Committee: {}, Organization: {} };
 
 describe('QA Test Script #110 (Sprint 6)', () => {
   function before() {
@@ -19,8 +17,8 @@ describe('QA Test Script #110 (Sprint 6)', () => {
   }
 
   function after() {
-    cy.get('p-button[icon="pi pi-trash"]').each((element) => {
-      cy.wrap(element).click();
+    cy.get('p-button[icon="pi pi-trash"]').each((Element) => {
+      cy.wrap(Element).click();
       cy.wait(50);
       cy.get('.p-confirm-dialog-accept').click();
       cy.wait(50);
@@ -28,12 +26,12 @@ describe('QA Test Script #110 (Sprint 6)', () => {
     cy.logout();
   }
 
-  for (var i = 0; i < Object.keys(contacts).length; i++) {
-    contact_type = Object.keys(contacts)[i];
-    contacts[contact_type] = GenerateContactObject({ contact_type: contact_type });
+  for (var i = 0; i < Object.keys(Contacts).length; i++) {
+    ContactType = Object.keys(Contacts)[i];
+    Contacts[ContactType] = GenerateContactObject({ contact_type: ContactType });
 
-    context(`QA Script #110 - ${contact_type}`, (c_type = contact_type) => {
-      var contact = contacts[c_type];
+    context(`QA Script #110 - ${ContactType}`, (c_type = ContactType) => {
+      let Contact: object = Contacts[c_type];
       it('Step 1: Navigate to contacts page', () => {
         before();
 
@@ -43,62 +41,62 @@ describe('QA Test Script #110 (Sprint 6)', () => {
       });
 
       it('Steps 2-5: Creates a contact', () => {
-        cy.EnterContact(contact, false);
+        cy.EnterContact(Contact, false);
 
         if (c_type == 'Individual' || c_type == 'Candidate') {
-          cy.get('#last_name').should('have.value', contact['last_name']);
-          cy.get('#first_name').should('have.value', contact['first_name']);
-          cy.get('#middle_name').should('have.value', contact['middle_name']);
-          cy.get('#prefix').should('have.value', contact['prefix']);
-          cy.get('#suffix').should('have.value', contact['suffix']);
+          cy.get('#last_name').should('have.value', Contact['last_name']);
+          cy.get('#first_name').should('have.value', Contact['first_name']);
+          cy.get('#middle_name').should('have.value', Contact['middle_name']);
+          cy.get('#prefix').should('have.value', Contact['prefix']);
+          cy.get('#suffix').should('have.value', Contact['suffix']);
 
-          cy.get('#employer').should('have.value', contact['employer']);
-          cy.get('#occupation').should('have.value', contact['occupation']);
+          cy.get('#employer').should('have.value', Contact['employer']);
+          cy.get('#occupation').should('have.value', Contact['occupation']);
         }
 
         if (c_type == 'Candidate') {
-          cy.get('#candidate_id').should('have.value', contact['candidate_id']);
-          cy.get("p-dropdown[formcontrolname='candidate_office']").should('contain', contact['candidate_office']);
+          cy.get('#candidate_id').should('have.value', Contact['candidate_id']);
+          cy.get("p-dropdown[formcontrolname='candidate_office']").should('contain', Contact['candidate_office']);
 
-          if (contact['candidate_office'] != 'Presidential') {
-            cy.get("p-dropdown[formcontrolname='candidate_state']").should('contain', contact['candidate_state']);
+          if (Contact['candidate_office'] != 'Presidential') {
+            cy.get("p-dropdown[formcontrolname='candidate_state']").should('contain', Contact['candidate_state']);
 
-            if (contact['candidate_office'] == 'House') {
+            if (Contact['candidate_office'] == 'House') {
               cy.get("p-dropdown[formcontrolname='candidate_district']") //Gets the field for the input for State
-                .should('contain', contact['candidate_district']);
+                .should('contain', Contact['candidate_district']);
             }
           }
         }
 
         if (c_type == 'Committee' || c_type == 'Organization') {
-          cy.get('#name').should('have.value', contact['name']);
+          cy.get('#name').should('have.value', Contact['name']);
 
           if (c_type == 'Committee') {
-            cy.get('#committee_id').should('have.value', contact['committee_id']);
+            cy.get('#committee_id').should('have.value', Contact['committee_id']);
           }
         }
 
         //Address
-        cy.get('#street_1').should('have.value', contact['street']);
-        cy.get('#street_2').should('have.value', contact['apartment']);
-        cy.get('#city').should('have.value', contact['city']);
-        cy.get('#zip').should('have.value', contact['zip']);
-        cy.get('#telephone').should('have.value', contact['phone']);
-        //cy.dropdown_set_value("p-dropdown[formcontrolname='state']", contact['state']);
+        cy.get('#street_1').should('have.value', Contact['street']);
+        cy.get('#street_2').should('have.value', Contact['apartment']);
+        cy.get('#city').should('have.value', Contact['city']);
+        cy.get('#zip').should('have.value', Contact['zip']);
+        cy.get('#telephone').should('have.value', Contact['phone']);
+        //cy.dropdown_set_value("p-dropdown[formcontrolname='state']", Contact['state']);
         cy.get("p-dropdown[formcontrolname='state']") //Gets the field for the input for State
-          .should('contain', contact['state']);
+          .should('contain', Contact['state']);
 
         cy.get('.p-button-primary > .p-button-label').contains('Save').click();
 
         cy.wait(250);
-        cy.get('tbody').find('tr').contains(`${contact['name']}`).parent().should('contain', contact['contact_type']);
+        cy.get('tbody').find('tr').contains(`${Contact['name']}`).parent().should('contain', Contact['contact_type']);
         if (c_type == 'Individual' || c_type == 'Candidate') {
           cy.get('tbody')
             .find('tr')
-            .contains(`${contact['name']}`)
+            .contains(`${Contact['name']}`)
             .parent()
-            .should('contain', contact['employer'])
-            .should('contain', contact['occupation']);
+            .should('contain', Contact['employer'])
+            .should('contain', Contact['occupation']);
         }
       });
 
