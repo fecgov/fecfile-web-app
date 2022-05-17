167bc5d093256dfa39969f544459ae65924ad735
Elaine Krauss <elaine@ekrauss.com>
2022-05-17 12:49:26 -0400

    Updated to better reflect team style guidelines

--- front-end/cypress/integration/sprint-7-test-192,248,249,250.spec.ts
+++ front-end/cypress/integration/sprint-7-test-192,248,249,250.spec.ts
@@ -2,7 +2,7 @@
 
 import * as _ from 'lodash';
 
-const contact_fields = {
+const ContactFields: object = {
   //Contains the max allowable length for any given field
   Individual: {
     last_name: 30,
@@ -52,34 +52,43 @@ const contact_fields = {
   },
 };
 
-const fields_required = ['street_1', 'city', 'zip', 'name', 'first_name', 'last_name', 'committee_id', 'candidate_id'];
+const FieldsRequired: Array<string> = [
+  'street_1',
+  'city',
+  'zip',
+  'name',
+  'first_name',
+  'last_name',
+  'committee_id',
+  'candidate_id',
+];
 
-var contact_type;
-var contacts = { Individual: {}, Candidate: {}, Committee: {}, Organization: {} };
+let ContactType: string;
+let Contacts: object = { Individual: {}, Candidate: {}, Committee: {}, Organization: {} };
 
-function randomString(length, type = 'alphanumeric') {
+function RandomString(Length, Type = 'alphanumeric'): string {
   // prettier-ignore
-  const alphanumeric = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9',];
+  const Alphanumeric: Array<string> = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9',];
   // prettier-ignore
-  const alphabet =   ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9',];
+  const Alphabet: Array<string> =   ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9',];
   // prettier-ignore
-  const numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const Numeric: Array<string> = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-  var characters = [];
-  if (type == 'alphanumeric') {
-    characters = alphanumeric;
-  } else if (type == 'numeric') {
-    characters = numeric;
-  } else if (type == 'alphabet') {
-    characters = alphabet;
+  let Characters: Array<string> = [];
+  if (Type == 'alphanumeric') {
+    Characters = Alphanumeric;
+  } else if (Type == 'numeric') {
+    Characters = Numeric;
+  } else if (Type == 'alphabet') {
+    Characters = Alphabet;
   } else {
     console.log('RandomString: Invalid type');
     return '';
   }
 
-  var outString = '';
-  while (outString.length < length) {
-    outString += _.sample(characters);
+  let outString: string = '';
+  while (outString.length < Length) {
+    outString += _.sample(Characters);
   }
   return outString;
 }
@@ -92,11 +101,11 @@ describe('QA Test Scripts #192, #248, #249, & #250 (Sprint 7)', () => {
     cy.url().should('contain', '/contacts');
   });
 
-  for (var i = 0; i < Object.keys(contacts).length; i++) {
-    contact_type = Object.keys(contacts)[i];
+  for (let i: number = 0; i < Object.keys(Contacts).length; i++) {
+    ContactType = Object.keys(Contacts)[i];
 
-    context(`---> ${contact_type}`, (c_type = contact_type) => {
-      var contact = contacts[c_type];
+    context(`---> ${ContactType}`, (c_type = ContactType) => {
+      let Contact: object = Contacts[c_type];
       it('Check every field for required/optional and maximum length', () => {
         cy.get("button[label='New']").click();
         cy.wait(50);
@@ -107,34 +116,34 @@ describe('QA Test Scripts #192, #248, #249, & #250 (Sprint 7)', () => {
         cy.get("button[label='Save']").click();
         cy.wait(50);
 
-        for (var j = 0; j < Object.keys(contact_fields[c_type]).length; j++) {
-          var field = Object.keys(contact_fields[c_type])[j];
-          var length = contact_fields[c_type][field];
+        for (let j: number = 0; j < Object.keys(ContactFields[c_type]).length; j++) {
+          let Field: string = Object.keys(ContactFields[c_type])[j];
+          let Length: number = ContactFields[c_type][Field];
 
-          if (fields_required.includes(field)) {
-            cy.get('#' + field)
+          if (FieldsRequired.includes(Field)) {
+            cy.get('#' + Field)
               .parent()
               .should('contain', 'This is a required field');
           }
 
-          var rString = '';
-          if (field == 'telephone') {
-            rString = randomString(length, 'numeric');
+          let RandString: string = '';
+          if (Field == 'telephone') {
+            RandString = RandomString(Length, 'numeric');
           } else {
-            rString = randomString(length);
+            RandString = RandomString(Length);
           }
 
-          cy.get('#' + field)
-            .type(rString)
+          cy.get('#' + Field)
+            .type(RandString)
             .parent()
             .find('app-error-messages')
             .children()
             .should('have.length', 0);
-          cy.get('#' + field)
+          cy.get('#' + Field)
             .type('0')
             .parent()
             .find('app-error-messages')
-            .should('contain', `This field cannot contain more than ${length}`);
+            .should('contain', `This field cannot contain more than ${Length}`);
         }
 
         cy.get("p-dropdown[FormControlName='country']").should('contain', 'United States of America');
