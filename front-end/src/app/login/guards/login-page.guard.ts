36381cb669aaf83578fe304ebc599c93d601dacf
David Heitzer <david.heitzer@tcg.com>
2022-06-07 14:33:20 -0400

    109 linting fix

--- front-end/src/app/login/guards/login-page.guard.ts
+++ front-end/src/app/login/guards/login-page.guard.ts
@@ -8,9 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class LoginGuard implements CanActivate {
   constructor(private apiService: ApiService, private router: Router) {}
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (this.apiService.isAuthenticated()) {
       this.router.navigate(['dashboard']);
     }
