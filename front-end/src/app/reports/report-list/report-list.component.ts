324fc1b55a5c0b5110fbcb1bf6f7b240f8b74948
Elaine Krauss <Elaine@Krauss@tcg.com>
2022-06-10 11:02:31 -0400

    Removes an unused import

--- front-end/src/app/reports/report-list/report-list.component.ts
+++ front-end/src/app/reports/report-list/report-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { ConfirmationService, MessageService } from 'primeng/api';
