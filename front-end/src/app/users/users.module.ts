8b0c310a75ba393a69f4161ddae6652ec31758d5
Elaine Krauss <Elaine@Krauss@tcg.com>
2022-06-16 10:34:33 -0400

    Removes unnecessary imports

--- front-end/src/app/users/users.module.ts
+++ front-end/src/app/users/users.module.ts
@@ -5,15 +5,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // PrimeNG
 import { TableModule } from 'primeng/table';
-import { DropdownModule } from 'primeng/dropdown';
 import { ToastModule } from 'primeng/toast';
 import { ToolbarModule } from 'primeng/toolbar';
-import { FileUploadModule } from 'primeng/fileupload';
-import { ButtonModule } from 'primeng/button';
-import { CheckboxModule } from 'primeng/checkbox';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { DialogModule } from 'primeng/dialog';
-import { InputTextModule } from 'primeng/inputtext';
 
 // App
 import { SharedModule } from '../shared/shared.module';
@@ -28,15 +22,9 @@ import { UserListComponent } from './user-list/user-list.component';
     ReactiveFormsModule,
     UsersRoutingModule,
     TableModule,
-    DropdownModule,
     ToastModule,
     ToolbarModule,
-    FileUploadModule,
-    ButtonModule,
-    CheckboxModule,
     ConfirmDialogModule,
-    DialogModule,
-    InputTextModule,
     SharedModule,
   ],
 })
