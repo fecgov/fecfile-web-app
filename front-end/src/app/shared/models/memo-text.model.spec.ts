be4726c68bbc3248573dbfdfab5657d0fc351d40
David Heitzer <david.heitzer@tcg.com>
2022-07-26 11:46:18 -0400

    fix test

--- front-end/src/app/shared/models/memo-text.model.spec.ts
+++ front-end/src/app/shared/models/memo-text.model.spec.ts
@@ -10,13 +10,13 @@ describe('MemoText', () => {
       id: 999,
       report_id: 123,
       rec_type: 'test_rec_type',
-      text: 'test_text'
+      text4000: 'test_text'
     };
     const memoText: MemoText = MemoText.fromJSON(data);
     expect(memoText).toBeInstanceOf(MemoText);
     expect(memoText.id).toBe(999);
     expect(memoText.report_id).toBe(123);
     expect(memoText.rec_type).toBe('test_rec_type');
-    expect(memoText.text).toBe('test_text');
+    expect(memoText.text4000).toBe('test_text');
   });
 });
