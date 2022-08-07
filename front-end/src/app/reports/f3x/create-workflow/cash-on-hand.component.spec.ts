77d2d01de32791620de365cddf95898f8e27d988
Matt Travers <matt.travers@tcg.com>
2022-08-07 18:57:31 -0400

    Init unit tests for new components

--- front-end/src/app/reports/f3x/create-workflow/cash-on-hand.component.spec.ts
+++ front-end/src/app/reports/f3x/create-workflow/cash-on-hand.component.spec.ts
@@ -1,19 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Router } from '@angular/router';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'app/shared/shared.module';
+import { MessageService } from 'primeng/api';
+import { CalendarModule } from 'primeng/calendar';
+import { ButtonModule } from 'primeng/button';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { selectCohNeededStatus } from 'app/store/coh-needed.selectors';
 import { CashOnHandComponent } from './cash-on-hand.component';
+import { ToastModule } from 'primeng/toast';
+import { CardModule } from 'primeng/card';
 
 describe('CashOnHandComponent', () => {
   let component: CashOnHandComponent;
+  let router: Router;
   let fixture: ComponentFixture<CashOnHandComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CashOnHandComponent ]
-    })
-    .compileComponents();
+      declarations: [CashOnHandComponent],
+      imports: [
+        ToastModule,
+        CardModule,
+        HttpClientTestingModule,
+        SharedModule,
+        CalendarModule,
+        InputNumberModule,
+        ButtonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule.withRoutes([]),
+      ],
+      providers: [
+        FormBuilder,
+        MessageService,
+        provideMockStore({
+          initialState: { fecfile_online_cohNeeded: false },
+          selectors: [{ selector: selectCohNeededStatus, value: true }],
+        }),
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(CashOnHandComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- front-end/src/app/shared/guards/cash-on-hand.guard.spec.ts
+++ front-end/src/app/shared/guards/cash-on-hand.guard.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideMockStore } from '@ngrx/store/testing';
+import { selectCohNeededStatus } from '../../store/coh-needed.selectors';
 import { CashOnHandGuard } from './cash-on-hand.guard';
 
 describe('CashOnHandGuard', () => {
   let guard: CashOnHandGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideMockStore({
+          initialState: { fecfile_online_cohNeeded: false },
+          selectors: [{ selector: selectCohNeededStatus, value: true }],
+        }),
+      ],
+    });
     guard = TestBed.inject(CashOnHandGuard);
   });
 
