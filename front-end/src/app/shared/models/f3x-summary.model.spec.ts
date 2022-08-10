ca7ce5836634b84724de96f349f79815c1f94cd3
Elaine Krauss <elaine.krauss@tcg.com>
2022-08-10 14:08:50 -0400

    Adds unit test to make sure that the upload status is properly instantiated within an F3xSummary instance

--- front-end/src/app/shared/models/f3x-summary.model.spec.ts
+++ front-end/src/app/shared/models/f3x-summary.model.spec.ts
@@ -1,4 +1,5 @@
 import { F3xSummary, F3xFormTypes } from './f3x-summary.model';
+import { FECUploadStatus } from './fec-upload-status.model';
 
 describe('F3XSummary', () => {
   it('should create an instance', () => {
@@ -17,5 +18,21 @@ describe('F3XSummary', () => {
     expect(f3xSummary.form_type).toBe(F3xFormTypes.F3XT);
     expect(f3xSummary.committee_name).toBe('foo');
     expect(f3xSummary.election_code).toBe(null);
+    expect(f3xSummary.upload_status).toBe(null);
+  });
+
+  it('#fromJSON() should return an F3xSummary instance with a valid FECUploadStatus instance', ()=>{
+    const data = {
+      upload_status: {
+        fec_report_id: "FEC-1234567",
+        acceptance_datetime: "12/12/2012",
+      }
+    };
+
+    const f3xSummary = F3xSummary.fromJSON(data);
+    expect(f3xSummary.upload_status).toBeInstanceOf(FECUploadStatus);
+    expect(f3xSummary.upload_status?.fec_report_id).toBe("FEC-1234567");
+    expect(f3xSummary.upload_status?.acceptance_datetime).toBeInstanceOf(Date);
+    expect(f3xSummary.upload_status?.acceptance_datetime?.getFullYear()).toBe(2012);
   });
 });
