66dc5ae11221818ac8586cfd37e110c635dd3349
Elaine Krauss <elaine.krauss@tcg.com>
2022-08-12 09:14:46 -0400

    Adds a unit test for the report-is-editable service

--- front-end/src/app/shared/services/report-is-editable.service.spec.ts
+++ front-end/src/app/shared/services/report-is-editable.service.spec.ts
@@ -1,21 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { provideMockStore } from '@ngrx/store/testing';
-import { ListRestResponse } from '../models/rest-api.model';
-import { environment } from '../../../environments/environment';
-import { CommitteeUser } from '../models/user.model';
-import { UsersService } from './users.service';
 import { F3xSummary } from '../models/f3x-summary.model';
 import { FECUploadStatus } from '../models/fec-upload-status.model';
 import { selectActiveReport } from '../../store/active-report.selectors';
+import { ReportIsEditableService } from './report-is-editable.service'
 
 describe('TransactionService', () => {
-  let service: UsersService;
   let httpTestingController: HttpTestingController;
+  let service: ReportIsEditableService;
 
   const activeReport: F3xSummary = F3xSummary.fromJSON({
-    upload_status: FECUploadStatus.fromJSON({
-      status:"ACCEPTED"
+    upload_submission: FECUploadStatus.fromJSON({
+      fec_status:"ACCEPTED"
     })
   });
 
@@ -23,7 +20,7 @@ describe('TransactionService', () => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [
-        UsersService,
+        ReportIsEditableService,
         provideMockStore({
           initialState: { activeReport: activeReport },
           selectors: [{ selector: selectActiveReport, value: activeReport }],
@@ -31,50 +28,17 @@ describe('TransactionService', () => {
       ],
     });
     httpTestingController = TestBed.inject(HttpTestingController);
-    service = TestBed.inject(UsersService);
+    service = TestBed.inject(ReportIsEditableService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('#getTableData() should return a list of contacts', () => {
-    const mockResponse: ListRestResponse = {
-      count: 2,
-      next: 'https://next-page',
-      previous: 'https://previous-page',
-      results: [
-        CommitteeUser.fromJSON({
-          first_name: 'John',
-          last_name: 'Smith',
-          email: 'john_smith@test.com',
-          role: 'C_ADMIN',
-          is_active: true,
-          id: 1,
-        }),
-        CommitteeUser.fromJSON({
-          first_name: 'Jane',
-          last_name: 'Smith',
-          email: 'jane_smith@test.com',
-          role: 'C_ADMIN',
-          is_active: true,
-          id: 2,
-        }),
-      ],
-    };
-
-    service.getTableData().subscribe((response: ListRestResponse) => {
-      expect(response).toEqual(mockResponse);
-    });
-
-    const req = httpTestingController.expectOne(`${environment.apiUrl}/committee/users/?page=1`);
-    expect(req.request.method).toEqual('GET');
-    req.flush(mockResponse);
-    httpTestingController.verify();
-  });
-
   it('should have stubbed out "Delete" methods', () => {
-    const cUser = new CommitteeUser();
-    expect(service.delete(cUser)).toBeTruthy();
+    const observe = service.isEditable();
+    observe.subscribe((bool: boolean)=>{
+      expect(bool).toBe(false);
+    })
   });
 });
