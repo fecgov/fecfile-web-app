78e6e0263f925e31b4c5b62cef72d23cb22114d5
Elaine Krauss <elaine.krauss@tcg.com>
2022-08-16 10:17:29 -0400

    Fixes the committee account service unit tests

--- front-end/src/app/shared/services/committee-account.service.spec.ts
+++ front-end/src/app/shared/services/committee-account.service.spec.ts
@@ -1,12 +1,33 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { UserLoginData } from '../models/user.model';
+import { selectUserLoginData } from 'app/store/login.selectors';
+
 
 import { CommitteeAccountService } from './committee-account.service';
 
 describe('CommitteeAccountService', () => {
   let service: CommitteeAccountService;
+  const userLoginData: UserLoginData = {
+    committee_id: 'C00000000',
+    email: 'email@fec.com',
+    is_allowed: true,
+    token: 'jwttokenstring',
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        CommitteeAccountService,
+        provideMockStore({
+          initialState: { fecfile_online_userLoginData: userLoginData },
+          selectors: [{ selector: selectUserLoginData, value: userLoginData }],
+        }),
+      ],
+    });
+
     service = TestBed.inject(CommitteeAccountService);
   });
 
