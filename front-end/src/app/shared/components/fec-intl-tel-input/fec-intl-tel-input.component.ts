e5515604af6f95b4359f461e007547821dbb07ba
David Heitzer <david.heitzer@tcg.com>
2022-08-29 12:59:36 -0400

    removed wrapper component

---
+++ front-end/src/app/shared/components/fec-intl-tel-input/fec-intl-tel-input.component.ts
@@ -0,0 +1,74 @@
+import { AfterViewInit, Component, ElementRef, OnDestroy, Self, ViewChild } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
+import * as intlTelInput from 'intl-tel-input';
+
+@Component({
+  selector: 'app-fec-intl-tel-input',
+  templateUrl: './fec-intl-tel-input.component.html',
+  styleUrls: ['./fec-intl-tel-input.component.scss'],
+})
+export class FecIntlTelInputComponent implements AfterViewInit, OnDestroy, 
+  ControlValueAccessor {
+  @ViewChild('telInput') telInput: ElementRef<HTMLInputElement> | null = null;
+
+  private iti: intlTelInput.Plugin | null = null;
+  private itiOptions: intlTelInput.Options = {
+    separateDialCode: true,
+  }
+  private countryCode: string | undefined = '';
+  private number = '';
+
+  constructor(@Self() private ngControl: NgControl) {
+    this.ngControl.valueAccessor = this;
+  }
+
+  /**
+   * Write form value to the DOM element (model => view)
+   */
+  writeValue(value: string): void {
+    if (value) {
+      this.iti?.setNumber(value);
+      this.onChange(value);
+    }
+  }
+
+  /**
+   * Update form when DOM element value changes (view => model)
+   */
+  registerOnChange(fn: () => void): void {
+    this.onChange = fn;
+  }
+
+  /**
+   * Update form when DOM element is blurred (view => model)
+   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  registerOnTouched(fn: () => void): void {
+    // noop
+  }
+
+  ngAfterViewInit(): void {
+    if (this.telInput) {
+      this.iti = intlTelInput(this.telInput.nativeElement, this.itiOptions);
+      this.telInput.nativeElement.addEventListener("countrychange", () => {
+        this.countryCode = this.iti?.getSelectedCountryData().dialCode;
+        this.onChange('+' + this.countryCode + ' ' + this.number);
+      });
+    }
+  }
+
+  onKey(event: KeyboardEvent) { // without type info
+    this.number = (event.target as HTMLInputElement).value;
+    this.onChange('+' + this.countryCode + ' ' + this.number);
+  }
+
+  ngOnDestroy() {
+    this.iti?.destroy();
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  private onChange(value: string) {
+    // noop
+  }
+
+}
--- front-end/src/app/shared/shared.module.ts
+++ front-end/src/app/shared/shared.module.ts
@@ -8,11 +8,11 @@ import { LabelPipe } from './pipes/label.pipe';
 import { LongDatePipe } from './pipes/long-date.pipe';
 import { FindOnReportCodePipe } from './pipes/report-code-label-list.pipe';
 
-import { IntlTelInputComponent } from './components/intl-tel-input/intl-tel-input.component';
+import { FecIntlTelInputComponent } from './components/fec-intl-tel-input/fec-intl-tel-input.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule],
-  declarations: [LabelPipe, ErrorMessagesComponent, FecDatePipe, LongDatePipe, FindOnReportCodePipe, DefaultZeroPipe, IntlTelInputComponent],
-  exports: [FecDatePipe, LongDatePipe, LabelPipe, ErrorMessagesComponent, FindOnReportCodePipe, DefaultZeroPipe, IntlTelInputComponent],
+  declarations: [LabelPipe, ErrorMessagesComponent, FecDatePipe, LongDatePipe, FindOnReportCodePipe, DefaultZeroPipe, FecIntlTelInputComponent],
+  exports: [FecDatePipe, LongDatePipe, LabelPipe, ErrorMessagesComponent, FindOnReportCodePipe, DefaultZeroPipe, FecIntlTelInputComponent],
 })
 export class SharedModule {}
