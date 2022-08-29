a040f289f8be573ad03e8ba413b34df77a89b0f7
David Heitzer <david.heitzer@tcg.com>
2022-08-29 13:53:02 -0400

    111 fix tests

--- front-end/src/app/shared/components/fec-intl-tel-input/fec-intl-tel-input.component.spec.ts
+++ front-end/src/app/shared/components/fec-intl-tel-input/fec-intl-tel-input.component.spec.ts
@@ -1,32 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder, FormsModule } from '@angular/forms';
-import { JsonSchema } from 'app/shared/interfaces/json-schema.interface';
+import { FormsModule } from '@angular/forms';
 import { ValidateService } from 'app/shared/services/validate.service';
 import { FecIntlTelInputComponent } from './fec-intl-tel-input.component';
 
-describe('ErrorMessagesComponent', () => {
+describe('FecIntlTelInputComponent', () => {
   let component: FecIntlTelInputComponent;
   let fixture: ComponentFixture<FecIntlTelInputComponent>;
-  let validateService: ValidateService;
 
-  const testSchema: JsonSchema = {
-    $schema: 'http://json-schema.org/draft-07/schema#',
-    $id: 'https://unit-test',
-    type: 'object',
-    required: [],
-    properties: {
-      in_between: {
-        type: 'string',
-        minLength: 10,
-        maxLength: 20,
-      },
-      low_high: {
-        type: 'number',
-        minimum: 0,
-        maximum: 10,
-      },
-    },
-  };
+
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -37,7 +19,7 @@ describe('ErrorMessagesComponent', () => {
   });
 
   beforeEach(() => {
-    validateService = TestBed.inject(ValidateService);
+
     fixture = TestBed.createComponent(FecIntlTelInputComponent);
     component = fixture.componentInstance;
 
