19bcdd27d4cf5b84de754b0a2ea5e4815211dcf2
Elaine Krauss <elaine.krauss@tcg.com>
2022-09-02 14:01:37 -0400

    Test no longer fails if run in the month of September or on the first 9 days of the month...

--- front-end/cypress/e2e/reports/Reports-Web-Print.cy.ts
+++ front-end/cypress/e2e/reports/Reports-Web-Print.cy.ts
@@ -1,8 +1,7 @@
 // @ts-check
 
-import { generateReportObject } from "../../support/generators/reports.spec";
-import { generateTransactionObject } from "../../support/generators/transactions.spec";
-
+import { generateReportObject } from '../../support/generators/reports.spec';
+import { generateTransactionObject } from '../../support/generators/transactions.spec';
 
 const report = generateReportObject();
 const transaction = generateTransactionObject();
@@ -13,34 +12,38 @@ describe('Test creating a report and submitting it for web print', () => {
     cy.deleteAllReports();
   });
 
-  it('Creates a report', ()=>{
+  it('Creates a report', () => {
     cy.get('.p-menubar').find('.p-menuitem-link').contains('Reports').click();
     cy.shortWait();
     cy.createReport(report);
   });
 
-  it('Progresses to the Transaction Management Page', ()=>{
+  it('Progresses to the Transaction Management Page', () => {
     cy.navigateToTransactionManagement();
   });
 
-  it('Creates a transaction', ()=>{
+  it('Creates a transaction', () => {
     cy.createTransactionSchA(transaction);
   });
 
-  it('Submits the report to web print', ()=>{
+  it('Submits the report to web print', () => {
     cy.navigateReportSidebar('Review', 'View print preview');
     cy.get('button[label="Compile PDF"]').click();
-    
+
     const today = new Date();
-    const weekdays = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
-    const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Aug','Sep','Nov','Dec'];
+    let strDay: string = today.getDate().toString();
+    if (strDay.length == 1) {
+      strDay = '0' + strDay;
+    }
+    const weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-    const dateString = `${weekdays[today.getDay()-1]} ${months[today.getMonth()]} `+
-                        `${today.getDate()} ${today.getFullYear()}`;
+    const dateString =
+      `${weekdays[today.getDay() - 1]} ${months[today.getMonth()]} ` + `${strDay} ${today.getFullYear()}`;
 
     cy.longWait();
     cy.longWait();
-    cy.contains('body', dateString).should('exist');
+    cy.contains('h2', dateString).should('exist');
   });
 
   after('Cleanup', () => {
