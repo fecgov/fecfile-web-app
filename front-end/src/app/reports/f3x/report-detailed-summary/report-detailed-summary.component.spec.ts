e6a78d459ad13c2d2a2fd9f17405ce6fe624b8d4
toddlees <todd.lees@tcg.com>
2022-09-05 19:30:56 -0400

    test coverage

--- front-end/src/app/reports/f3x/report-detailed-summary/report-detailed-summary.component.spec.ts
+++ front-end/src/app/reports/f3x/report-detailed-summary/report-detailed-summary.component.spec.ts
@@ -3,7 +3,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { selectReportCodeLabelList } from 'app/store/label-lookup.selectors';
 import { selectActiveReport } from 'app/store/active-report.selectors';
 import { ActivatedRoute } from '@angular/router';
-import { provideMockStore } from '@ngrx/store/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { F3xSummary } from 'app/shared/models/f3x-summary.model';
 import { SharedModule } from 'app/shared/shared.module';
 import { CardModule } from 'primeng/card';
@@ -12,6 +12,7 @@ import { ReportDetailedSummaryComponent } from './report-detailed-summary.compon
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReportService } from 'app/shared/services/report.service';
 import { BehaviorSubject, Subject } from 'rxjs';
+import { Report } from 'app/shared/interfaces/report.interface';
 
 describe('ReportDetailedSummaryComponent', () => {
   let component: ReportDetailedSummaryComponent;
@@ -58,8 +59,22 @@ describe('ReportDetailedSummaryComponent', () => {
 
   describe('CALCULATING', () => {
     beforeEach(async () => {
-      f3x.calculation_status = 'CALCULATING';
-      f3xSubject.next({ report: f3x });
+      const f3x = {
+        id: 999,
+        form_type: 'F3XN',
+        report_code: 'Q1',
+        calculation_status: 'CALCULATING',
+        filer_committee_id_number: null,
+        coverage_through_date: null,
+        coverage_from_date: null,
+        webprint_submission: null,
+        upload_submission: null,
+        created: null,
+        updated: null,
+      } as Report;
+      TestBed.inject(MockStore).overrideSelector(selectActiveReport, f3x);
+      TestBed.inject(MockStore).refreshState();
+      fixture.detectChanges();
       return component.refreshSummary();
     });
     it('should create', () => {
