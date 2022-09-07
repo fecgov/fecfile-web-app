427e9442c0d120898ae2566d6ac4c27758f6c36b
David Heitzer <david.heitzer@tcg.com>
2022-09-07 15:58:33 -0400

    328 add dev branch back to tasks.py

--- tasks.py
+++ tasks.py
@@ -57,7 +57,7 @@ def _detect_branch(repo):
 DEPLOY_RULES = (
     ("prod", lambda _, branch: branch == "main"),
     ("stage", lambda _, branch: branch.startswith("release")),
-    ("dev", lambda _, branch: branch == "feature/328-logout-for-login-dot-gov"),
+    ("dev", lambda _, branch: branch == "develop"),
 )
 
 
