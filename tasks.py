2408dbefa45fdaa1f6e2add3d60ac2d5dbc4f6d7
Matt Travers <matt.travers@tcg.com>
2022-09-26 14:41:27 -0400

    Rolled back deploy to prod setting

--- tasks.py
+++ tasks.py
@@ -55,7 +55,7 @@ def _detect_branch(repo):
 
 
 DEPLOY_RULES = (
-    ("prod", lambda _, branch: branch == "new-css"),
+    ("prod", lambda _, branch: branch == "main"),
     ("stage", lambda _, branch: branch.startswith("release")),
     ("dev", lambda _, branch: branch == "develop"),
 )
