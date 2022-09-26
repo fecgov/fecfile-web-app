a47bf7575a55e60ba65c145a9ac7a4176a8c605c
Elaine Krauss <elaine.krauss@tcg.com>
2022-09-26 14:46:26 -0400

    Adds a new E2E test that checks for the memo checkbox being pre-selected and read only on MEMO transactions

---
+++ front-end/cypress/e2e/sprint-13/sprint-13-test-445.cy.ts
@@ -0,0 +1,53 @@
+// @ts-check
+
+import { generateReportObject } from '../../support/generators/reports.spec';
+import { generateTransactionObject } from '../../support/generators/transactions.spec';
+import { createTransactionSchA } from '../../support/transactions.spec';
+
+const columns = [
+  'Transaction type',
+  'Contribution name',
+  'Contribution date',
+  'Memo code',
+  'Contribution amount',
+  'Contribution aggregate',
+  'Actions',
+];
+
+describe('QA Script 244 (Sprint 8)', () => {
+  after(() => {
+    cy.login();
+    cy.visit('/dashboard');
+    cy.deleteAllReports();
+  });
+
+  it('', () => {
+    //Step 1: Log in, navigate to the reports page, create a report, and set it up to be ready for transactions
+    cy.login();
+    cy.visit('/dashboard');
+    cy.url().should('contain', '/dashboard');
+    cy.medWait();
+    cy.get('.p-menubar').find('.p-menuitem-link').contains('Reports').click();
+    cy.url().should('contain', '/reports');
+
+    const report = generateReportObject();
+    cy.createReport(report);
+
+    //Step 2: Select the edit button for the created report
+    cy.navigateToTransactionManagement();
+
+    //Step 3: Verify that you are on the "Transactions" page
+    cy.get("div[role='toolbar']").contains('Transactions').should('exist');
+
+    //Step 4: Create a Joint Fundraising Transfer MEMO transaction
+    const transaction = generateTransactionObject({ TRANSFERS: { 'Joint Fundraising Transfer': {} } });
+    createTransactionSchA(transaction, false);
+    cy.get('button[label="Save & add a Memo"]').click();
+    cy.longWait();
+    cy.longWait();
+
+    //Step 5: Open a Memo Transaction and check for the memo checkbox to be pre-selected and disabled
+    cy.get('p-checkbox[formControlName="memo_code"]').find('span').should('have.class', 'pi-check');
+    cy.get('p-checkbox[formControlName="memo_code"]').parent().should('have.class', 'readonly');
+  });
+});
