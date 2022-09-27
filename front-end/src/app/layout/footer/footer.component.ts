060101d7d1584ed7d80abda30ba9a424bbedccdc
Elaine Krauss <elaine.krauss@tcg.com>
2022-09-27 15:41:37 -0400

    WIP New footer component

--- front-end/src/app/layout/footer/footer.component.ts
+++ front-end/src/app/layout/footer/footer.component.ts
@@ -5,4 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  RADAnalyst = 'John Doe';
+  RADPhone = '(888) 888-8888';
+}
