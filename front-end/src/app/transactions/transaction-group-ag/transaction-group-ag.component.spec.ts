a35ed6aeddc9a7dc88d56b63cd7f9b4a1cc3f4c4
Matt Travers <matt.travers@tcg.com>
2022-10-16 19:12:50 -0400

    Fixed linting issue in AG component unit tests

--- front-end/src/app/transactions/transaction-group-ag/transaction-group-ag.component.spec.ts
+++ front-end/src/app/transactions/transaction-group-ag/transaction-group-ag.component.spec.ts
@@ -4,7 +4,6 @@ import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockStore } from '@ngrx/store/testing';
-import { ContactTypes } from 'app/shared/models/contact.model';
 import { FecDatePipe } from 'app/shared/pipes/fec-date.pipe';
 import { testMockStore } from 'app/shared/utils/unit-test.utils';
 import { AccordionModule } from 'primeng/accordion';
