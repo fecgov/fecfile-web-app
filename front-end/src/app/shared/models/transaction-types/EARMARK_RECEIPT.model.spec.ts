022b54ee2c1412cd960d42e6d63f38d4571935be
toddlees <todd.lees@tcg.com>
2022-10-27 22:51:06 -0400

    more coverage

--- front-end/src/app/shared/models/transaction-types/EARMARK_RECEIPT.model.spec.ts
+++ front-end/src/app/shared/models/transaction-types/EARMARK_RECEIPT.model.spec.ts
@@ -2,12 +2,13 @@ import { SchATransaction, ScheduleATransactionTypes } from '../scha-transaction.
 import { ContactTypes } from '../contact.model';
 import { TransactionTypeUtils } from '../../utils/transaction-type.utils';
 import { TransactionType } from 'app/shared/interfaces/transaction-type.interface';
+import { EARMARK_MEMO } from './EARMARK_MEMO.model';
 
 describe('EARMARK_RECEIPT', () => {
-  let transactionType: TransactionType | undefined;
+  let transactionType: TransactionType;
 
   beforeEach(() => {
-    transactionType = TransactionTypeUtils.factory('EARMARK_RECEIPT');
+    transactionType = TransactionTypeUtils.factory('EARMARK_RECEIPT') as TransactionType;
     if (transactionType) {
       transactionType.transaction = SchATransaction.fromJSON({
         id: 999,
@@ -25,18 +26,22 @@ describe('EARMARK_RECEIPT', () => {
   });
 
   it('#factory() should return a SchATransaction', () => {
-    if (!transactionType) {
-      return;
-    }
     const txn = transactionType.getNewTransaction();
     expect(txn.form_type).toBe('SA11AI');
     expect(txn.transaction_type_identifier).toBe(ScheduleATransactionTypes.EARMARK_RECEIPT);
   });
 
   it('#contributionPurposeDescripReadonly() should return an empty string', () => {
-    if (!transactionType) {
-      return;
-    }
+    const descrip = transactionType.contributionPurposeDescripReadonly();
+    expect(descrip).toBe('');
+  });
+
+  it('#contributionPurposeDescripReadonly() should reflect child', () => {
+    const childTransactionType: TransactionType = TransactionTypeUtils.factory(
+      ScheduleATransactionTypes.EARMARK_MEMO
+    ) as TransactionType;
+    childTransactionType.transaction = childTransactionType.getNewTransaction();
+    transactionType.childTransactionType = childTransactionType;
     const descrip = transactionType.contributionPurposeDescripReadonly();
     expect(descrip).toBe('');
   });
