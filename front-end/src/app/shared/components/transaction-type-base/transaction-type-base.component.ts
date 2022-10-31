584a4c85b9352db5c6bd1c4b37ecf14d96f2f84d
Elaine Krauss <Elaine.Krauss@tcg.com>
2022-10-31 15:09:46 -0400

    Corrects linting error

--- front-end/src/app/shared/components/transaction-type-base/transaction-type-base.component.ts
+++ front-end/src/app/shared/components/transaction-type-base/transaction-type-base.component.ts
@@ -137,7 +137,7 @@ export abstract class TransactionTypeBaseComponent implements OnInit, OnDestroy 
     this.contactId$.complete();
   }
 
-  retrieveMemoText(formValues: any) {
+  retrieveMemoText(formValues: object) {
     const memo_text = MemoText.fromJSON({
       text4000: formValues['memo_text_description'],
       report_id: this.transactionType?.transaction?.report_id,
