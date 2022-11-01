9721c61d6f2946b8c06e6aec192bedb7de4f14c9
toddlees <todd.lees@tcg.com>
2022-11-01 16:14:05 -0400

    PAC RECEIPT

---
+++ front-end/src/app/shared/models/transaction-types/PAC_RECEIPT.model.spec.ts
@@ -0,0 +1,27 @@
+import { SchATransaction, ScheduleATransactionTypes } from '../scha-transaction.model';
+import { PAC_RECEIPT } from './PAC_RECEIPT.model';
+
+describe('PAC_RECEIPT', () => {
+  let transactionType: PAC_RECEIPT;
+
+  beforeEach(() => {
+    transactionType = new PAC_RECEIPT();
+  });
+
+  it('should create an instance', () => {
+    expect(transactionType).toBeTruthy();
+    expect(transactionType.scheduleId).toBe('A');
+    expect(transactionType.componentGroupId).toBe('F');
+  });
+
+  it('#factory() should return a SchATransaction', () => {
+    const txn: SchATransaction = transactionType.getNewTransaction();
+    expect(txn.form_type).toBe('SA11C');
+    expect(txn.transaction_type_identifier).toBe(ScheduleATransactionTypes.PAC_RECEIPT);
+  });
+
+  it('#contributionPurposeDescripReadonly() should return an empty string', () => {
+    const descrip = transactionType.contributionPurposeDescripReadonly();
+    expect(descrip).toBe('');
+  });
+});
