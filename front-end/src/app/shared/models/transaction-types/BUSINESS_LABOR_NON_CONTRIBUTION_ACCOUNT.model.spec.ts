b382b98f9e0151d280321ed7ef7e22c0dfae484d
Elaine Krauss <Elaine.Krauss@tcg.com>
2022-11-17 14:50:54 -0500

    Adds the BUSINESS_LABOR_NON_CONTRIBUTION_ACCOUNT transaction

---
+++ front-end/src/app/shared/models/transaction-types/BUSINESS_LABOR_NON_CONTRIBUTION_ACCOUNT.model.spec.ts
@@ -0,0 +1,27 @@
+import { SchATransaction, ScheduleATransactionTypes } from '../scha-transaction.model';
+import { BUSINESS_LABOR_NON_CONTRIBUTION_ACCOUNT } from './BUSINESS_LABOR_NON_CONTRIBUTION_ACCOUNT.model';
+
+describe('TRIBAL_RECEIPT', () => {
+  let transactionType: BUSINESS_LABOR_NON_CONTRIBUTION_ACCOUNT;
+
+  beforeEach(() => {
+    transactionType = new BUSINESS_LABOR_NON_CONTRIBUTION_ACCOUNT();
+  });
+
+  it('should create an instance', () => {
+    expect(transactionType).toBeTruthy();
+    expect(transactionType.scheduleId).toBe('A');
+    expect(transactionType.componentGroupId).toBe('D');
+  });
+
+  it('#factory() should return a SchATransaction', () => {
+    const txn: SchATransaction = transactionType.getNewTransaction();
+    expect(txn.form_type).toBe('SA17');
+    expect(txn.transaction_type_identifier).toBe(ScheduleATransactionTypes.BUSINESS_LABOR_NON_CONTRIBUTION_ACCOUNT);
+  });
+
+  it('#contributionPurposeDescripReadonly() should return an empty string', () => {
+    const descrip = transactionType.contributionPurposeDescripReadonly();
+    expect(descrip).toBe('Non-contribution Account Receipt');
+  });
+});
