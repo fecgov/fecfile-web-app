0d1b87b199e5a0b16724d31739f864a47d20ddd1
Matt Travers <matt.travers@tcg.com>
2022-11-23 17:50:44 -0500

    Added new schema PAC_NATIONAL_PARTY_RECOUNT_ACCOUNT

---
+++ front-end/src/app/shared/models/transaction-types/PAC_NATIONAL_PARTY_RECOUNT_ACCOUNT.model.ts
@@ -0,0 +1,41 @@
+import { LabelUtils } from 'app/shared/utils/label.utils';
+import { schema } from 'fecfile-validate/fecfile_validate_js/dist/PAC_NATIONAL_PARTY_RECOUNT_ACCOUNT';
+import { TransactionType } from '../../interfaces/transaction-type.interface';
+import {
+  AggregationGroups,
+  SchATransaction,
+  ScheduleATransactionTypeLabels,
+  ScheduleATransactionTypes,
+} from '../scha-transaction.model';
+import {
+  CANCEL_CONTROL,
+  SAVE_ANOTHER_CONTROL,
+  SAVE_LIST_CONTROL,
+  TransactionNavigationControls,
+} from '../transaction-navigation-controls.model';
+
+export class PAC_NATIONAL_PARTY_RECOUNT_ACCOUNT implements TransactionType {
+  scheduleId = 'A';
+  componentGroupId = 'F';
+  isDependentChild = false;
+  title = LabelUtils.get(ScheduleATransactionTypeLabels, ScheduleATransactionTypes.PAC_NATIONAL_PARTY_RECOUNT_ACCOUNT);
+  schema = schema;
+  transaction?: SchATransaction;
+  navigationControls: TransactionNavigationControls = new TransactionNavigationControls(
+    [],
+    [CANCEL_CONTROL],
+    [SAVE_LIST_CONTROL, SAVE_ANOTHER_CONTROL]
+  );
+
+  contributionPurposeDescripReadonly(): string {
+    return 'Recount/Legal Proceedings Account';
+  }
+
+  getNewTransaction() {
+    return SchATransaction.fromJSON({
+      form_type: 'SA17',
+      transaction_type_identifier: ScheduleATransactionTypes.PAC_NATIONAL_PARTY_RECOUNT_ACCOUNT,
+      aggregation_group: AggregationGroups.NATIONAL_PARTY_RECOUNT_ACCOUNT,
+    });
+  }
+}
