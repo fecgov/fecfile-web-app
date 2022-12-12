7d28f3d6cfd8614137c5d7ff9058be4b52197a2d
Elaine Krauss <Elaine.Krauss@tcg.com>
2022-12-12 18:01:08 -0500

    Corrects a unit test

--- front-end/src/app/shared/models/transaction-types/PAC_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO.model.spec.ts
+++ front-end/src/app/shared/models/transaction-types/PAC_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO.model.spec.ts
@@ -1,12 +1,12 @@
-import { INDIVIDUAL_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO } from './INDIVIDUAL_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO.model';
+import { PAC_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO } from './PAC_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO.model';
 import { SchATransaction, ScheduleATransactionTypes } from '../scha-transaction.model';
 import { JOINT_FUNDRAISING_TRANSFER } from './JOINT_FUNDRAISING_TRANSFER.model';
 
-describe('INDIVIDUAL_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO', () => {
-  let transactionType: INDIVIDUAL_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO;
+describe('PAC_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO', () => {
+  let transactionType: PAC_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO;
 
   beforeEach(() => {
-    transactionType = new INDIVIDUAL_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO();
+    transactionType = new PAC_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO();
     transactionType.transaction = transactionType.getNewTransaction();
     transactionType.transaction.parent_transaction = new JOINT_FUNDRAISING_TRANSFER().getNewTransaction();
     (transactionType.transaction.parent_transaction as SchATransaction).contributor_organization_name = 'Test Org';
@@ -15,14 +15,14 @@ describe('INDIVIDUAL_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO', () => {
   it('should create an instance', () => {
     expect(transactionType).toBeTruthy();
     expect(transactionType.scheduleId).toBe('A');
-    expect(transactionType.componentGroupId).toBe('A');
+    expect(transactionType.componentGroupId).toBe('F');
   });
 
   it('#factory() should return a SchATransaction', () => {
     const txn: SchATransaction = transactionType.getNewTransaction();
     expect(txn.form_type).toBe('SA17');
     expect(txn.transaction_type_identifier).toBe(
-      ScheduleATransactionTypes.INDIVIDUAL_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO
+      ScheduleATransactionTypes.PAC_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO
     );
   });
 
