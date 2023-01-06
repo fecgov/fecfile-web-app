43f7a67bf6090a7e614cf22fa130b897bfc12684
David Heitzer <david.heitzer@tcg.com>
2023-01-06 11:21:21 -0500

    code smell

--- front-end/src/app/shared/models/transaction-types/PARTY_RECEIPT.model.ts
+++ front-end/src/app/shared/models/transaction-types/PARTY_RECEIPT.model.ts
@@ -1,17 +1,10 @@
-import { TransactionType } from '../../interfaces/transaction-type.interface';
-import {
-  SchATransaction,
-  ScheduleATransactionTypes,
-  ScheduleATransactionTypeLabels,
-  AggregationGroups,
-} from '../scha-transaction.model';
 import { LabelUtils } from 'app/shared/utils/label.utils';
 import { schema } from 'fecfile-validate/fecfile_validate_js/dist/PARTY_RECEIPT';
 import {
-  CANCEL_CONTROL,
-  SAVE_LIST_CONTROL,
-  SAVE_ANOTHER_CONTROL,
-  TransactionNavigationControls,
+  AggregationGroups, SchATransaction, ScheduleATransactionTypeLabels, ScheduleATransactionTypes
+} from '../scha-transaction.model';
+import {
+  CANCEL_CONTROL, SAVE_ANOTHER_CONTROL, SAVE_LIST_CONTROL, TransactionNavigationControls
 } from '../transaction-navigation-controls.model';
 import { SchaTransactionType } from './SchaTransactionType.model';
 
