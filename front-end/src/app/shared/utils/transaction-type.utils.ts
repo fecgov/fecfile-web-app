b68211ef77122bed30e96fce6a342bf900c4f085
Elaine Krauss <elaine.krauss@tcg.com>
2023-01-11 09:18:45 -0500

    Adds the PAC Return receipt to the transaction type utils

--- front-end/src/app/shared/utils/transaction-type.utils.ts
+++ front-end/src/app/shared/utils/transaction-type.utils.ts
@@ -44,6 +44,7 @@ import { INDIVIDUAL_NATIONAL_PARTY_CONVENTION_ACCOUNT } from '../models/transact
 import { PARTY_NATIONAL_PARTY_CONVENTION_ACCOUNT } from '../models/transaction-types/PARTY_NATIONAL_PARTY_CONVENTION_ACCOUNT.model';
 import { TRIBAL_NATIONAL_PARTY_RECOUNT_ACCOUNT } from '../models/transaction-types/TRIBAL_NATIONAL_PARTY_RECOUNT_ACCOUNT.model';
 import { TRIBAL_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO } from '../models/transaction-types/TRIBAL_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO.model';
+import { PAC_RETURN } from '../models/transaction-types/PAC_RETURN.model';
 
 // prettier-ignore
 const transactionTypeClasses: any = { // eslint-disable-line @typescript-eslint/no-explicit-any
@@ -92,6 +93,7 @@ const transactionTypeClasses: any = { // eslint-disable-line @typescript-eslint/
   INDIVIDUAL_NATIONAL_PARTY_CONVENTION_ACCOUNT,
   PARTY_NATIONAL_PARTY_CONVENTION_ACCOUNT,
   TRIBAL_NATIONAL_PARTY_RECOUNT_ACCOUNT,
+  PAC_RETURN,
 }
 
 export class TransactionTypeUtils {
