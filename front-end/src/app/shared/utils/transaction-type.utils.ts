28d48660e78f490e30ab844e74b5101439b78358
David Heitzer <david.heitzer@tcg.com>
2023-01-12 14:17:09 -0500

    initial dev

--- front-end/src/app/shared/utils/transaction-type.utils.ts
+++ front-end/src/app/shared/utils/transaction-type.utils.ts
@@ -46,6 +46,7 @@ import { TRIBAL_NATIONAL_PARTY_RECOUNT_ACCOUNT } from '../models/transaction-typ
 import { UNREGISTERED_RECEIPT_FROM_PERSON } from '../models/transaction-types/UNREGISTERED_RECEIPT_FROM_PERSON.model';
 import { UNREGISTERED_RECEIPT_FROM_PERSON_RETURN } from '../models/transaction-types/UNREGISTERED_RECEIPT_FROM_PERSON_RETURN.model';
 import { TRIBAL_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO } from '../models/transaction-types/TRIBAL_NATIONAL_PARTY_HEADQUARTERS_JF_TRANSFER_MEMO.model';
+import { RETURN_RECEIPT } from '../models/transaction-types/RETURN_RECEIPT.model';
 
 // prettier-ignore
 const transactionTypeClasses: any = { // eslint-disable-line @typescript-eslint/no-explicit-any
@@ -69,6 +70,7 @@ const transactionTypeClasses: any = { // eslint-disable-line @typescript-eslint/
   PARTY_JF_TRANSFER_MEMO,
   PARTY_RECEIPT,
   PARTY_RECOUNT_RECEIPT,
+  RETURN_RECEIPT,
   TRANSFER,
   TRIBAL_JF_TRANSFER_MEMO,
   TRIBAL_NATIONAL_PARTY_CONVENTION_JF_TRANSFER_MEMO,
