d30468a847814b70fd08ffabd0659b08aae0037a
David Heitzer <david.heitzer@tcg.com>
2023-01-17 15:47:59 -0500

    490 add unit tests

--- front-end/src/app/shared/models/transaction-types/PAC_EARMARK_RECEIPT.model.spec.ts
+++ front-end/src/app/shared/models/transaction-types/PAC_EARMARK_RECEIPT.model.spec.ts
@@ -1,27 +1,27 @@
 import { ScheduleATransactionTypes } from '../scha-transaction.model';
 import { ContactTypes } from '../contact.model';
-import { EARMARK_MEMO } from './EARMARK_MEMO.model';
-import { EARMARK_RECEIPT } from './EARMARK_RECEIPT.model';
+import { PAC_EARMARK_MEMO } from './PAC_EARMARK_MEMO.model';
+import { PAC_EARMARK_RECEIPT } from './PAC_EARMARK_RECEIPT.model';
 
-describe('EARMARK_RECEIPT', () => {
-  let transactionType: EARMARK_RECEIPT;
+describe('PAC_EARMARK_RECEIPT', () => {
+  let transactionType: PAC_EARMARK_RECEIPT;
 
   beforeEach(() => {
-    transactionType = new EARMARK_RECEIPT();
+    transactionType = new PAC_EARMARK_RECEIPT();
   });
 
   it('should create an instance', () => {
     expect(transactionType).toBeTruthy();
     if (transactionType) {
       expect(transactionType.scheduleId).toBe('A');
-      expect(transactionType.componentGroupId).toBe('AG');
+      expect(transactionType.componentGroupId).toBe('FG');
     }
   });
 
   it('#factory() should return a SchATransaction', () => {
     const txn = transactionType.getNewTransaction();
-    expect(txn.form_type).toBe('SA11AI');
-    expect(txn.transaction_type_identifier).toBe(ScheduleATransactionTypes.EARMARK_RECEIPT);
+    expect(txn.form_type).toBe('SA11C');
+    expect(txn.transaction_type_identifier).toBe(ScheduleATransactionTypes.PAC_EARMARK_RECEIPT);
   });
 
   it('#generatePurposeDescription() should generate empty string', () => {
@@ -30,7 +30,7 @@ describe('EARMARK_RECEIPT', () => {
   });
 
   it('#generatePurposeDescription() should reflect child', () => {
-    const childTransactionType: EARMARK_MEMO = new EARMARK_MEMO();
+    const childTransactionType: PAC_EARMARK_MEMO = new PAC_EARMARK_MEMO();
     childTransactionType.transaction = childTransactionType.getNewTransaction();
     childTransactionType.transaction.entity_type = ContactTypes.INDIVIDUAL;
     childTransactionType.transaction.contributor_first_name = 'Joe';
