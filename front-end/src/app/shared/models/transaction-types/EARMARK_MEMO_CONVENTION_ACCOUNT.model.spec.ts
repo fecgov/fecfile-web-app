2b0498a7b4a013f3d560a0054351fdc9b73ff097
Elaine Krauss <elaine.krauss@tcg.com>
2023-01-27 09:22:35 -0500

    Adds memo unit tests

---
+++ front-end/src/app/shared/models/transaction-types/EARMARK_MEMO_CONVENTION_ACCOUNT.model.spec.ts
@@ -0,0 +1,27 @@
+import { EARMARK_MEMO_CONVENTION_ACCOUNT } from './EARMARK_MEMO_CONVENTION_ACCOUNT.model';
+import { SchATransaction, ScheduleATransactionTypes } from '../scha-transaction.model';
+
+describe('EARMARK_MEMO_CONVENTION_ACCOUNT', () => {
+  let transactionType: EARMARK_MEMO_CONVENTION_ACCOUNT;
+
+  beforeEach(() => {
+    transactionType = new EARMARK_MEMO_CONVENTION_ACCOUNT();
+  });
+
+  it('should create an instance', () => {
+    expect(transactionType).toBeTruthy();
+    expect(transactionType.scheduleId).toBe('A');
+    expect(transactionType.componentGroupId).toBe('GG');
+  });
+
+  it('#factory() should return a SchATransaction', () => {
+    const txn: SchATransaction = transactionType.getNewTransaction();
+    expect(txn.form_type).toBe('SA17');
+    expect(txn.transaction_type_identifier).toBe(ScheduleATransactionTypes.EARMARK_MEMO_CONVENTION_ACCOUNT);
+  });
+
+  it('#generatePurposeDescription() should generate a string', () => {
+    const descrip = transactionType.generatePurposeDescription();
+    expect(descrip).toBe('Total earmarked through conduit.');
+  });
+});
