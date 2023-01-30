3a5808c242a4d3e0b995de671d2ffc09f3d5a23a
Elaine Krauss <elaine.krauss@tcg.com>
2023-01-30 12:43:10 -0500

    Quick fix to prevent a compile error

--- front-end/src/app/transactions/transaction-group-d/transaction-group-d.component.ts
+++ front-end/src/app/transactions/transaction-group-d/transaction-group-d.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TransactionTypeBaseComponent } from 'app/shared/components/transaction-type-base/transaction-type-base.component';
-import { ScheduleATransactionTypeLabels, ScheduleATransactionTypes } from 'app/shared/models/scha-transaction.model';
+import { ScheduleATransactionTypes } from 'app/shared/models/scha-transaction.model';
 import { LabelUtils, PrimeOptions } from 'app/shared/utils/label.utils';
 import { ContactTypeLabels, ContactTypes } from '../../shared/models/contact.model';
 
@@ -26,7 +26,7 @@ export class TransactionGroupDComponent extends TransactionTypeBaseComponent imp
     'memo_text_input',
     'subTransaction',
   ];
-  override subTransactionOptions: { [key: string]: string | ScheduleATransactionTypes }[] = [];
+  subTransactionOptions: { [key: string]: string | ScheduleATransactionTypes }[] = [];
   override contactTypeOptions: PrimeOptions = LabelUtils.getPrimeOptions(ContactTypeLabels).filter((option) =>
     [ContactTypes.ORGANIZATION].includes(option.code as ContactTypes)
   );
