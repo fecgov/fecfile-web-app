26c3b1a36a28b21a1038711aa5af2fbeb2417453
Matt Travers <matt.travers@tcg.com>
2023-02-09 11:35:15 -0500

    Fixed transaction resolve of a dependent child payload

--- front-end/src/app/shared/resolvers/transaction.resolver.ts
+++ front-end/src/app/shared/resolvers/transaction.resolver.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
-import { map, Observable, of } from 'rxjs';
+import { map, Observable, of, mergeMap } from 'rxjs';
 import { TransactionType } from '../models/transaction-types/transaction-type.model';
 import { Transaction } from '../models/transaction.model';
 import { ContactService } from '../services/contact.service';
@@ -60,6 +60,29 @@ export class TransactionResolver implements Resolve<Transaction | undefined> {
   }
 
   resolve_existing_transaction(transactionId: string): Observable<Transaction | undefined> {
-    return this.transactionService.get(String(transactionId));
+    return this.transactionService.get(String(transactionId)).pipe(
+      mergeMap((transaction: Transaction) => {
+        if (transaction.transaction_type_identifier && transaction.contact) {
+          // Determine if we need to get the parent transaction as the
+          // transaction type requested is a dependent transaction and cannot
+          // be modified directly in a UI form. (e.g. EARMARK_MEMO)
+          if (transaction.transactionType && transaction.transactionType.isDependentChild) {
+            // Get parent transaction to ensure we have a full list of children
+            if (transaction?.parent_transaction?.id) {
+              return this.transactionService.get(transaction.parent_transaction.id);
+            } else {
+              throw new Error(
+                `Transaction ${transaction.id} (${transaction.transaction_type_identifier}) is a dependent transaction type but does not have a parent transaction.`
+              );
+            }
+          } else {
+            return of(transaction);
+          }
+        }
+        throw new Error(
+          `Transaction type resolver can't find transaction and/or contact for transaction ID ${transactionId}`
+        );
+      })
+    );
   }
 }
