1b8c2849b486d3c4f8035f2f4a36e8b9167ae8f7
David Heitzer <david.heitzer@tcg.com>
2023-02-08 23:10:36 -0500

    202 fixed unit tests

--- front-end/src/app/shared/services/fec-api.service.spec.ts
+++ front-end/src/app/shared/services/fec-api.service.spec.ts
@@ -1,10 +1,11 @@
-import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { FecApiService } from './fec-api.service';
-import { FecApiPaginatedResponse } from 'app/shared/models/fec-api.model';
+import { TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 import { CommitteeAccount } from 'app/shared/models/committee-account.model';
-import { environment } from 'environments/environment';
+import { FecApiPaginatedResponse } from 'app/shared/models/fec-api.model';
 import { FecFiling } from '../models/fec-filing.model';
+import { testMockStore } from '../utils/unit-test.utils';
+import { FecApiService } from './fec-api.service';
 
 describe('FecApiService', () => {
   let httpTestingController: HttpTestingController;
@@ -13,6 +14,7 @@ describe('FecApiService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
+      providers: [FecApiService, provideMockStore(testMockStore)],
     });
     httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(FecApiService);
@@ -41,7 +43,7 @@ describe('FecApiService', () => {
       });
 
       const req = httpTestingController.expectOne(
-        'https://api.open.fec.gov/v1/committee/C00601211/?api_key=' + environment.fecApiKey
+        `https://localhost/api/v1/mock_openfec/committee/C00601211/`
       );
 
       expect(req.request.method).toEqual('GET');
@@ -52,69 +54,32 @@ describe('FecApiService', () => {
   describe('#getCommitteeRecentFiling()', () => {
     it('should return most recent filing from realtime endpoint', () => {
       const f1Filing: FecFiling = FecFiling.fromJSON({ form_type: 'F1N', pdf_url: 'go here' });
-      const f2Filing: FecFiling = FecFiling.fromJSON({ form_type: 'F2', pdf_url: 'dont go here' });
-      const response: FecApiPaginatedResponse = {
-        api_version: '1.0',
-        pagination: {
-          page: 1,
-          per_page: 20,
-          count: 1,
-          pages: 1,
-        },
-        results: [f2Filing, f1Filing],
-      };
 
       service.getCommitteeRecentFiling('C00601211').subscribe((mostRecentFiling) => {
         expect(mostRecentFiling).toEqual(f1Filing);
       });
 
       const req = httpTestingController.expectOne(
-        `https://api.open.fec.gov/v1/efile/filings/?api_key=${environment.fecApiKey}&committee_id=C00601211&sort=-receipt_date`
+        `https://localhost/api/v1/mock_openfec/filings/C00601211/`
       );
 
       expect(req.request.method).toEqual('GET');
-      req.flush(response);
+      req.flush(f1Filing);
     });
   });
 
   it('should return most recent filing from nightly endpoint', () => {
     const f1Filing: FecFiling = FecFiling.fromJSON({ form_type: 'F1N', pdf_url: 'go here' });
     const f2Filing: FecFiling = FecFiling.fromJSON({ form_type: 'F2', pdf_url: 'dont go here' });
-    const realtimeResponse: FecApiPaginatedResponse = {
-      api_version: '1.0',
-      pagination: {
-        page: 1,
-        per_page: 20,
-        count: 1,
-        pages: 1,
-      },
-      results: [f2Filing],
-    };
-
-    const nightlyResponse: FecApiPaginatedResponse = {
-      api_version: '1.0',
-      pagination: {
-        page: 1,
-        per_page: 20,
-        count: 1,
-        pages: 1,
-      },
-      results: [f1Filing],
-    };
 
     service.getCommitteeRecentFiling('C00601211').subscribe((mostRecentFiling) => {
-      expect(mostRecentFiling).toEqual(f1Filing);
+      expect(mostRecentFiling).toEqual(f2Filing);
     });
 
     const realtimeReq = httpTestingController.expectOne(
-      `https://api.open.fec.gov/v1/efile/filings/?api_key=${environment.fecApiKey}&committee_id=C00601211&sort=-receipt_date`
+      `https://localhost/api/v1/mock_openfec/filings/C00601211/`
     );
     expect(realtimeReq.request.method).toEqual('GET');
-    realtimeReq.flush(realtimeResponse);
-    const nightlyReq = httpTestingController.expectOne(
-      `https://api.open.fec.gov/v1/filings/?api_key=${environment.fecApiKey}&sort=-receipt_date&per_page=1&page=1&committee_id=C00601211&form_type=F1`
-    );
-    expect(nightlyReq.request.method).toEqual('GET');
-    nightlyReq.flush(nightlyResponse);
+    realtimeReq.flush(f2Filing);
   });
 });
