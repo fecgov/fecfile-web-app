ebea69ccb8f221f1898a4e284fedeb2906053901
Elaine Krauss <elaine.krauss@tcg.com>
2023-02-14 16:15:19 -0500

    The Memo Item checkbox updates when a transaction's date falls outside of its report's coverage

--- front-end/src/app/shared/components/inputs/amount-input/amount-input.component.ts
+++ front-end/src/app/shared/components/inputs/amount-input/amount-input.component.ts
@@ -1,12 +1,16 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { BaseInputComponent } from '../base-input.component';
 import { InputNumber } from 'primeng/inputnumber';
+import { Store } from '@ngrx/store';
+import { selectActiveReport } from 'app/store/active-report.selectors';
+import { Subject, takeUntil } from 'rxjs';
+import { F3xSummary } from 'app/shared/models/f3x-summary.model';
 
 @Component({
   selector: 'app-amount-input',
   templateUrl: './amount-input.component.html',
 })
-export class AmountInputComponent extends BaseInputComponent implements OnInit {
+export class AmountInputComponent extends BaseInputComponent implements OnInit, OnDestroy {
   @Input() memoCodeReadOnly = false;
   @Input() contributionAmountReadOnly = false;
   @Input() memoItemHelpText =
@@ -15,11 +19,64 @@ export class AmountInputComponent extends BaseInputComponent implements OnInit {
 
   @ViewChild('amountInput') amountInput!: InputNumber;
 
+  defaultMemoCodeReadOnly = false;
+  defaultMemoItemHelpText = this.memoItemHelpText;
+  contributionDateOutsideReport = false;
   contributionAmountInputStyleClass = '';
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  report?: F3xSummary;
+
+  constructor(private store: Store) {
+    super();
+  }
 
   ngOnInit(): void {
     if (this.contributionAmountReadOnly) {
       this.contributionAmountInputStyleClass = 'readonly';
+    }
+
+    this.defaultMemoCodeReadOnly = this.memoCodeReadOnly;
+    this.defaultMemoItemHelpText = this.memoItemHelpText;
+
+    this.store
+      .select(selectActiveReport)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((report) => {
+        this.report = report as F3xSummary;
+        const date: Date = this.form.get('contribution_date')?.value;
+        if (date) {
+          this.updateMemoItemWithDate(date);
+        }
+      });
+
+    this.form
+      .get('contribution_date')
+      ?.valueChanges.pipe(takeUntil(this.destroy$))
+      .subscribe((date) => {
+        this.updateMemoItemWithDate(date);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
+
+  updateMemoItemWithDate(date: Date) {
+    if (this.report?.coverage_from_date && this.report?.coverage_through_date) {
+      if (date < this.report.coverage_from_date || date > this.report.coverage_through_date) {
+        this.memoCodeReadOnly = true;
+        this.memoItemHelpText =
+          'Memo item is required since your transaction date falls outside of report coverage dates';
+        this.contributionDateOutsideReport = true;
+        this.form.patchValue({
+          memo_code: true,
+        });
+      } else {
+        this.memoCodeReadOnly = this.defaultMemoCodeReadOnly;
+        this.memoItemHelpText = this.defaultMemoItemHelpText;
+        this.contributionDateOutsideReport = false;
+      }
     }
   }
 
