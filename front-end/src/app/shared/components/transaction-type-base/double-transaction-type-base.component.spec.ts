b13abb21e4427a75fdee28dac478d878a692156f
Matt Travers <matt.travers@tcg.com>
2023-02-14 07:27:12 -0500

    Add unit tests to double transaction template

--- front-end/src/app/shared/components/transaction-type-base/double-transaction-type-base.component.spec.ts
+++ front-end/src/app/shared/components/transaction-type-base/double-transaction-type-base.component.spec.ts
@@ -1,13 +1,19 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockStore } from '@ngrx/store/testing';
 import { TransactionService } from 'app/shared/services/transaction.service';
 import { ValidateService } from 'app/shared/services/validate.service';
-import { testMockStore } from 'app/shared/utils/unit-test.utils';
-import { MessageService } from 'primeng/api';
+import { getTestTransactionByType, testMockStore } from 'app/shared/utils/unit-test.utils';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { DoubleTransactionTypeBaseComponent } from './double-transaction-type-base.component';
+import { FecDatePipe } from 'app/shared/pipes/fec-date.pipe';
+import { DatePipe } from '@angular/common';
+import { ApiService } from 'app/shared/services/api.service';
+import { SchATransaction, ScheduleATransactionTypes } from 'app/shared/models/scha-transaction.model';
+import { transactionDateReceived } from '../../../../../cypress/support/generators/generators.spec';
 
 class TestDoubleTransactionTypeBaseComponent extends DoubleTransactionTypeBaseComponent {
   formProperties: string[] = [
@@ -57,25 +63,53 @@ class TestDoubleTransactionTypeBaseComponent extends DoubleTransactionTypeBaseCo
   ];
 }
 
-describe('TransactionTypeBaseComponent', () => {
+describe('DoubleTransactionTypeBaseComponent', () => {
   let component: TestDoubleTransactionTypeBaseComponent;
   let fixture: ComponentFixture<TestDoubleTransactionTypeBaseComponent>;
+  let testMessageService: MessageService;
+  let testRouter: Router;
+  let testTransactionService: TransactionService;
+  let testApiService: ApiService;
+  let testConfirmationService: ConfirmationService;
+  let fecDatePipe: FecDatePipe;
+  let testTransaction: SchATransaction;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TestDoubleTransactionTypeBaseComponent],
       imports: [RouterTestingModule, HttpClientTestingModule],
-      providers: [MessageService, FormBuilder, ValidateService, TransactionService, provideMockStore(testMockStore)],
+      providers: [
+        DatePipe,
+        MessageService,
+        FormBuilder,
+        ValidateService,
+        TransactionService,
+        ConfirmationService,
+        provideMockStore(testMockStore),
+        FecDatePipe,
+      ],
     }).compileComponents();
+
+    testMessageService = TestBed.inject(MessageService);
+    testRouter = TestBed.inject(Router);
+    testTransactionService = TestBed.inject(TransactionService);
+    testApiService = TestBed.inject(ApiService);
+    testConfirmationService = TestBed.inject(ConfirmationService);
+    fecDatePipe = TestBed.inject(FecDatePipe);
   });
 
   beforeEach(() => {
+    testTransaction = getTestTransactionByType(ScheduleATransactionTypes.PAC_EARMARK_RECEIPT) as SchATransaction;
+    testTransaction.children = [
+      getTestTransactionByType(ScheduleATransactionTypes.PAC_EARMARK_MEMO) as SchATransaction,
+    ];
     fixture = TestBed.createComponent(TestDoubleTransactionTypeBaseComponent);
     component = fixture.componentInstance;
+    component.transaction = testTransaction;
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
