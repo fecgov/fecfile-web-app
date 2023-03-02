13bbda320ecf41ad4cf0510926ca286c965746f4
David Heitzer <david.heitzer@tcg.com>
2023-03-02 11:02:19 -0500

    684 initial dev

---
+++ front-end/src/app/shared/models/transaction-types/OPERATING_EXPENDITURE_PAYMENT_TO_PAYROLL_MEMO.model.spec.ts
@@ -0,0 +1,27 @@
+import { OPERATING_EXPENDITURE } from './OPERATING_EXPENDITURE.model';
+import { SchBTransaction, ScheduleBTransactionTypes } from '../schb-transaction.model';
+import { TransactionType } from 'app/shared/models/transaction-type.model';
+
+describe('OPERATING_EXPENDITURE', () => {
+  let transactionType: OPERATING_EXPENDITURE;
+
+  beforeEach(() => {
+    transactionType = new OPERATING_EXPENDITURE();
+  });
+
+  it('should create an instance', () => {
+    expect(transactionType).toBeTruthy();
+    expect(transactionType.scheduleId).toBe('B');
+    expect(transactionType.componentGroupId).toBe('B');
+  });
+
+  it('#factory() should return a SchBTransaction', () => {
+    const txn: SchBTransaction = transactionType.getNewTransaction();
+    expect(txn.form_type).toBe('SB21b');
+    expect(txn.transaction_type_identifier).toBe(ScheduleBTransactionTypes.OPERATING_EXPENDITURE);
+  });
+
+  it('#generatePurposeDescription() should not be defined', () => {
+    expect((transactionType as TransactionType).generatePurposeDescription).toBe(undefined);
+  });
+});
