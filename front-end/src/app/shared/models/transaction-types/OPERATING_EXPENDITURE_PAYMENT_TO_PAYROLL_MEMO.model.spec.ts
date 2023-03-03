a5ee5f7c80cd61b9663f4d896877f2b3aa893d7e
David Heitzer <david.heitzer@tcg.com>
2023-03-02 22:44:36 -0500

    684 model

--- front-end/src/app/shared/models/transaction-types/OPERATING_EXPENDITURE_PAYMENT_TO_PAYROLL_MEMO.model.spec.ts
+++ front-end/src/app/shared/models/transaction-types/OPERATING_EXPENDITURE_PAYMENT_TO_PAYROLL_MEMO.model.spec.ts
@@ -1,12 +1,12 @@
-import { OPERATING_EXPENDITURE } from './OPERATING_EXPENDITURE.model';
-import { SchBTransaction, ScheduleBTransactionTypes } from '../schb-transaction.model';
 import { TransactionType } from 'app/shared/models/transaction-type.model';
+import { SchBTransaction, ScheduleBTransactionTypes } from '../schb-transaction.model';
+import { OPERATING_EXPENDITURE_PAYMENT_TO_PAYROLL_MEMO } from './OPERATING_EXPENDITURE_PAYMENT_TO_PAYROLL_MEMO.model';
 
-describe('OPERATING_EXPENDITURE', () => {
-  let transactionType: OPERATING_EXPENDITURE;
+describe('OPERATING_EXPENDITURE_PAYMENT_TO_PAYROLL_MEMO', () => {
+  let transactionType: OPERATING_EXPENDITURE_PAYMENT_TO_PAYROLL_MEMO;
 
   beforeEach(() => {
-    transactionType = new OPERATING_EXPENDITURE();
+    transactionType = new OPERATING_EXPENDITURE_PAYMENT_TO_PAYROLL_MEMO();
   });
 
   it('should create an instance', () => {
@@ -18,7 +18,8 @@ describe('OPERATING_EXPENDITURE', () => {
   it('#factory() should return a SchBTransaction', () => {
     const txn: SchBTransaction = transactionType.getNewTransaction();
     expect(txn.form_type).toBe('SB21b');
-    expect(txn.transaction_type_identifier).toBe(ScheduleBTransactionTypes.OPERATING_EXPENDITURE);
+    expect(txn.transaction_type_identifier).toBe(
+      ScheduleBTransactionTypes.OPERATING_EXPENDITURE_PAYMENT_TO_PAYROLL_MEMO);
   });
 
   it('#generatePurposeDescription() should not be defined', () => {
--- front-end/src/app/shared/models/transaction-types/OPERATING_EXPENDITURE_PAYMENT_TO_PAYROLL_MEMO.model.ts
+++ front-end/src/app/shared/models/transaction-types/OPERATING_EXPENDITURE_PAYMENT_TO_PAYROLL_MEMO.model.ts
@@ -1,14 +1,15 @@
 import { LabelUtils } from 'app/shared/utils/label.utils';
 import { schema } from 'fecfile-validate/fecfile_validate_js/dist/DISBURSEMENT_MEMOS';
-import { AggregationGroups } from '../transaction.model';
-import { SchBTransaction, ScheduleBTransactionTypeLabels, ScheduleBTransactionTypes } from '../schb-transaction.model';
-import { SchBTransactionType } from '../schb-transaction-type.model';
-import { STANDARD_CONTROLS, TransactionNavigationControls } from '../transaction-navigation-controls.model';
 import { ContactTypes } from '../contact.model';
+import { SchBTransactionType } from '../schb-transaction-type.model';
+import { SchBTransaction, ScheduleBTransactionTypeLabels, ScheduleBTransactionTypes } from '../schb-transaction.model';
+import { STANDARD_CONTROLS, TransactionNavigationControls } from '../transaction-navigation-controls.model';
+import { AggregationGroups } from '../transaction.model';
 
 export class OPERATING_EXPENDITURE_PAYMENT_TO_PAYROLL_MEMO extends SchBTransactionType {
-  componentGroupId = 'D';
-  title = LabelUtils.get(ScheduleBTransactionTypeLabels, ScheduleBTransactionTypes.OPERATING_EXPENDITURE);
+  componentGroupId = 'B';
+  title = LabelUtils.get(ScheduleBTransactionTypeLabels,
+    ScheduleBTransactionTypes.OPERATING_EXPENDITURE_PAYMENT_TO_PAYROLL_MEMO);
   schema = schema;
   override defaultContactTypeOption = ContactTypes.ORGANIZATION;
   override navigationControls: TransactionNavigationControls = STANDARD_CONTROLS;
@@ -16,7 +17,7 @@ export class OPERATING_EXPENDITURE_PAYMENT_TO_PAYROLL_MEMO extends SchBTransacti
   getNewTransaction() {
     return SchBTransaction.fromJSON({
       form_type: 'SB21b',
-      transaction_type_identifier: ScheduleBTransactionTypes.OPERATING_EXPENDITURE,
+      transaction_type_identifier: ScheduleBTransactionTypes.OPERATING_EXPENDITURE_PAYMENT_TO_PAYROLL_MEMO,
       aggregation_group: AggregationGroups.GENERAL_DISBURSEMENT,
     });
   }
