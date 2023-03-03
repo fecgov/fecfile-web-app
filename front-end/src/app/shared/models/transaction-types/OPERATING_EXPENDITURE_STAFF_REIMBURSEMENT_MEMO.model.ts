473932a3f7c8684e393823cdb6d792a982d4e089
Elaine Krauss <elaine.krauss@tcg.com>
2023-03-03 13:18:35 -0500

    Adds the Operating Expenditure Staff Reimbursement memo

--- front-end/src/app/shared/models/transaction-types/OPERATING_EXPENDITURE_STAFF_REIMBURSEMENT_MEMO.model.ts
+++ front-end/src/app/shared/models/transaction-types/OPERATING_EXPENDITURE_STAFF_REIMBURSEMENT_MEMO.model.ts
@@ -1,30 +1,25 @@
 import { LabelUtils } from 'app/shared/utils/label.utils';
-import { schema } from 'fecfile-validate/fecfile_validate_js/dist/DISBURSEMENT_PARENTS_STAFF';
-import { AggregationGroups, TransactionTypes } from '../transaction.model';
+import { schema } from 'fecfile-validate/fecfile_validate_js/dist/DISBURSEMENT_MEMOS';
+import { AggregationGroups } from '../transaction.model';
 import { SchBTransaction, ScheduleBTransactionTypeLabels, ScheduleBTransactionTypes } from '../schb-transaction.model';
 import { SchBTransactionType } from '../schb-transaction-type.model';
 import { STANDARD_CONTROLS, TransactionNavigationControls } from '../transaction-navigation-controls.model';
 import { ContactTypes } from '../contact.model';
 
-export class OPERATING_EXPENDITURE_STAFF_REIMBURSEMENT extends SchBTransactionType {
-  componentGroupId = 'A';
+export class OPERATING_EXPENDITURE_STAFF_REIMBURSEMENT_MEMO extends SchBTransactionType {
+  componentGroupId = 'B';
   title = LabelUtils.get(
     ScheduleBTransactionTypeLabels,
-    ScheduleBTransactionTypes.OPERATING_EXPENDITURE_STAFF_REIMBURSEMENT
+    ScheduleBTransactionTypes.OPERATING_EXPENDITURE_STAFF_REIMBURSEMENT_MEMO
   );
   schema = schema;
-  override defaultContactTypeOption = ContactTypes.INDIVIDUAL;
+  override defaultContactTypeOption = ContactTypes.ORGANIZATION;
   override navigationControls: TransactionNavigationControls = STANDARD_CONTROLS;
-  override subTransactionTypes = [ScheduleBTransactionTypes.OPERATING_EXPENDITURE_STAFF_REIMBURSEMENT_MEMO];
-
-  override generatePurposeDescription() {
-    return 'Reimbursement: See Below';
-  }
 
   getNewTransaction() {
     return SchBTransaction.fromJSON({
       form_type: 'SB21b',
-      transaction_type_identifier: ScheduleBTransactionTypes.OPERATING_EXPENDITURE_STAFF_REIMBURSEMENT,
+      transaction_type_identifier: ScheduleBTransactionTypes.OPERATING_EXPENDITURE_STAFF_REIMBURSEMENT_MEMO,
       aggregation_group: AggregationGroups.GENERAL_DISBURSEMENT,
     });
   }
