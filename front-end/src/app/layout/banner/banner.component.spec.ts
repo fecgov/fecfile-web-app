393ed3ade94f7f3fd493cfdc2d69fc68871c60c9
David Heitzer <david.heitzer@tcg.com>
2023-03-09 18:49:16 -0500

    initial rough dev

---
+++ front-end/src/app/layout/banner/banner.component.spec.ts
@@ -0,0 +1,30 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { testMockStore } from 'app/shared/utils/unit-test.utils';
+import { MenubarModule } from 'primeng/menubar';
+import { BannerComponent } from './banner.component';
+
+describe('BannerComponent', () => {
+  let component: BannerComponent;
+  let fixture: ComponentFixture<BannerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, MenubarModule, RouterTestingModule],
+      declarations: [BannerComponent],
+      providers: [BannerComponent, provideMockStore(testMockStore)],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BannerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
