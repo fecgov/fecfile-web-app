75bcde893448973815b93b9c49871846b60622e6
Elaine Krauss <elaine.krauss@tcg.com>
2023-03-09 16:55:43 -0500

    Adds a new unit test for the sidebar toggling

--- front-end/src/app/layout/layout.component.spec.ts
+++ front-end/src/app/layout/layout.component.spec.ts
@@ -9,6 +9,7 @@ import { HeaderComponent } from './header/header.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MenuReportComponent } from './sidebar/menu-report/menu-report.component';
 import { LayoutComponent } from './layout.component';
+import { NavigationEnd } from '@angular/router';
 
 describe('LayoutComponent', () => {
   let component: LayoutComponent;
@@ -28,5 +29,17 @@ describe('LayoutComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show or hide the sidebar based on the page', () => {
+    const navigationEvent = new NavigationEnd(1, '', '');
+
+    navigationEvent.url = '/reports/f3x/summary/1234-5678';
+    component.handleNavigationEvent(navigationEvent);
+    expect(component.sidebarVisible).toBe(true);
+
+    navigationEvent.url = '/contacts';
+    component.handleNavigationEvent(navigationEvent);
+    expect(component.sidebarVisible).toBe(false);
   });
 });
