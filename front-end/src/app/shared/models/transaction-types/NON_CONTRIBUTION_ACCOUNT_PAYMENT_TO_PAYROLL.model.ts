f544211e929a50560505795af54b37a0ae27956e
Elaine Krauss <elaine.krauss@tcg.com>
2023-03-31 16:26:57 -0400

    Adds the Payment to Payroll and Credit Card Payment models

--- front-end/src/app/shared/models/transaction-types/NON_CONTRIBUTION_ACCOUNT_PAYMENT_TO_PAYROLL.model.ts
+++ front-end/src/app/shared/models/transaction-types/NON_CONTRIBUTION_ACCOUNT_PAYMENT_TO_PAYROLL.model.ts
@@ -1,28 +1,30 @@
 import { LabelUtils } from 'app/shared/utils/label.utils';
-import { schema } from 'fecfile-validate/fecfile_validate_js/dist/NON_CONTRIBUTION_MEMOS';
+import { schema } from 'fecfile-validate/fecfile_validate_js/dist/NON_CONTRIBUTION_PARENTS';
 import { AggregationGroups } from '../transaction.model';
 import { SchBTransaction, ScheduleBTransactionTypeLabels, ScheduleBTransactionTypes } from '../schb-transaction.model';
 import { SchBTransactionType } from '../schb-transaction-type.model';
 import { TransactionNavigationControls, STANDARD_CONTROLS } from '../transaction-navigation-controls.model';
-import { ContactType, ContactTypes } from '../contact.model';
+import { ContactTypes } from '../contact.model';
 
-export class NON_CONTRIBUTION_ACCOUNT_PAYMENT_TO_PAYROLL_MEMO extends SchBTransactionType {
-  componentGroupId = 'C';
+export class NON_CONTRIBUTION_ACCOUNT_PAYMENT_TO_PAYROLL extends SchBTransactionType {
+  componentGroupId = 'D';
   title = LabelUtils.get(
     ScheduleBTransactionTypeLabels,
-    ScheduleBTransactionTypes.NON_CONTRIBUTION_ACCOUNT_PAYMENT_TO_PAYROLL_MEMO
+    ScheduleBTransactionTypes.NON_CONTRIBUTION_ACCOUNT_PAYMENT_TO_PAYROLL
   );
   schema = schema;
   override showAggregate = false;
-  override defaultContactTypeOption = ContactTypes.INDIVIDUAL;
-  override contactTypeOptions?: ContactType[] | undefined = [ContactTypes.INDIVIDUAL, ContactTypes.ORGANIZATION];
   override navigationControls: TransactionNavigationControls = STANDARD_CONTROLS;
+  override subTransactionTypes = [ScheduleBTransactionTypes.NON_CONTRIBUTION_ACCOUNT_PAYMENT_TO_PAYROLL_MEMO];
 
   getNewTransaction() {
     return SchBTransaction.fromJSON({
       form_type: 'SB29',
-      transaction_type_identifier: ScheduleBTransactionTypes.NON_CONTRIBUTION_ACCOUNT_PAYMENT_TO_PAYROLL_MEMO,
+      transaction_type_identifier: ScheduleBTransactionTypes.NON_CONTRIBUTION_ACCOUNT_PAYMENT_TO_PAYROLL,
       aggregation_group: AggregationGroups.GENERAL_DISBURSEMENT,
     });
+  }
+  override generatePurposeDescription(): string {
+    return 'Non-Contribution Account - Payroll: See Below';
   }
 }
