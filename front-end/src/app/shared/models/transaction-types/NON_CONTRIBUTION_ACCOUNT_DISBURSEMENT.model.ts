872fabb3092079bda73daacde53a9f02ded4c145
Elaine Krauss <elaine.krauss@tcg.com>
2023-05-01 15:57:41 -0400

    Adds the Non Contribution Account Disbursement

---
+++ front-end/src/app/shared/models/transaction-types/NON_CONTRIBUTION_ACCOUNT_DISBURSEMENT.model.ts
@@ -0,0 +1,26 @@
+import { LabelUtils } from 'app/shared/utils/label.utils';
+import { schema } from 'fecfile-validate/fecfile_validate_js/dist/NON_CONTRIBUTION_ACCOUNT_DISBURSEMENT';
+import { AggregationGroups } from '../transaction.model';
+import { SchBTransaction, ScheduleBTransactionTypeLabels, ScheduleBTransactionTypes } from '../schb-transaction.model';
+import { SchBTransactionType } from '../schb-transaction-type.model';
+import { TransactionNavigationControls, STANDARD_PARENT_CONTROLS } from '../transaction-navigation-controls.model';
+import { ContactTypes } from '../contact.model';
+
+export class NON_CONTRIBUTION_ACCOUNT_DISBURSEMENT extends SchBTransactionType {
+  componentGroupId = 'B';
+  title = LabelUtils.get(
+    ScheduleBTransactionTypeLabels,
+    ScheduleBTransactionTypes.NON_CONTRIBUTION_ACCOUNT_DISBURSEMENT
+  );
+  schema = schema;
+  override defaultContactTypeOption = ContactTypes.COMMITTEE;
+  override navigationControls: TransactionNavigationControls = STANDARD_PARENT_CONTROLS;
+
+  getNewTransaction() {
+    return SchBTransaction.fromJSON({
+      form_type: 'SB29',
+      transaction_type_identifier: ScheduleBTransactionTypes.NON_CONTRIBUTION_ACCOUNT_DISBURSEMENT,
+      aggregation_group: AggregationGroups.GENERAL_DISBURSEMENT,
+    });
+  }
+}
