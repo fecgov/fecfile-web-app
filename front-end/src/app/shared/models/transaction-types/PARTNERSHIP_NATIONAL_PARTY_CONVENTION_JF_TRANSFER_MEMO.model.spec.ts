bbd8b43b045dab0a8e054efd82cb8c1324d34a2c
David Heitzer <david.heitzer@tcg.com>
2023-05-04 18:04:32 -0400

    504 initial dev

---
+++ front-end/src/app/shared/models/transaction-types/PARTNERSHIP_NATIONAL_PARTY_CONVENTION_JF_TRANSFER_MEMO.model.spec.ts
@@ -0,0 +1,43 @@
+import { getTestTransactionByType } from 'app/shared/utils/unit-test.utils';
+import { TransactionTypeUtils } from '../../utils/transaction-type.utils';
+import { SchATransaction, ScheduleATransactionTypes } from '../scha-transaction.model';
+import { Transaction } from '../transaction.model';
+
+describe('PARTNERSHIP_NATIONAL_PARTY_CONVENTION_JF_TRANSFER_MEMO', () => {
+  let transaction: Transaction;
+
+  beforeEach(() => {
+    transaction = getTestTransactionByType(
+      ScheduleATransactionTypes.PARTNERSHIP_NATIONAL_PARTY_CONVENTION_JF_TRANSFER_MEMO,
+      ScheduleATransactionTypes.JF_TRANSFER_NATIONAL_PARTY_CONVENTION_ACCOUNT
+    ) as SchATransaction;
+    (transaction.parent_transaction as SchATransaction).contributor_organization_name = 'Test Committee';
+  });
+
+  it('should create an instance', () => {
+    expect(transaction.transactionType).toBeTruthy();
+    expect(transaction.transactionType?.scheduleId).toBe('A');
+    expect(transaction.transactionType?.componentGroupId).toBe('D');
+  });
+
+  it('#factory() should return a SchATransaction', () => {
+    expect(transaction.form_type).toBe('SA17');
+    expect(transaction.transaction_type_identifier).toBe(
+      ScheduleATransactionTypes.PARTNERSHIP_NATIONAL_PARTY_CONVENTION_JF_TRANSFER_MEMO
+    );
+  });
+
+  it('#generatePurposeDescription() should generate a string', () => {
+    const descrip = transaction.transactionType?.generatePurposeDescription?.(transaction);
+    expect(descrip).toBe('Pres. Nominating Convention Account JF Memo... (Partnership attributions do not require itemization)');
+  });
+
+  it('#generatePurposeDescription() should shrink long description', () => {
+    (transaction.parent_transaction as SchATransaction).contributor_organization_name =
+      'Super Duper Long Committee Name That Needs to Shrink';
+    const descrip = transaction.transactionType?.generatePurposeDescription?.(transaction);
+    expect(descrip).toBe(
+      'Pres. Nominating Convention Account JF Memo... (Partnership attributions do not require itemization)'
+    );
+  });
+});
