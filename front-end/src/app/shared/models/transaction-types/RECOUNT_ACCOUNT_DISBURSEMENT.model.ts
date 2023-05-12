434db91c8b60ed9dd0be8fba7344b4ba31dac3fc
Matt Travers <matt.travers@tcg.com>
2023-05-12 14:58:31 -0400

    Feature/710,711,712,713 Recount and NP Disbursements

---
+++ front-end/src/app/shared/models/transaction-types/RECOUNT_ACCOUNT_DISBURSEMENT.model.ts
@@ -0,0 +1,25 @@
+import { LabelUtils } from 'app/shared/utils/label.utils';
+import { schema } from 'fecfile-validate/fecfile_validate_js/dist/RECOUNT_AND_NP_DISBURSEMENTS';
+import { SchBTransactionType } from '../schb-transaction-type.model';
+import { SchBTransaction, ScheduleBTransactionTypeLabels, ScheduleBTransactionTypes } from '../schb-transaction.model';
+import { TransactionGroupB } from '../transaction-groups/transaction-group-b.model';
+import { STANDARD_CONTROLS, TransactionNavigationControls } from '../transaction-navigation-controls.model';
+import { AggregationGroups } from '../transaction.model';
+import { ContactTypes } from '../contact.model';
+
+export class RECOUNT_ACCOUNT_DISBURSEMENT extends SchBTransactionType {
+  transactionGroup = new TransactionGroupB();
+  title = LabelUtils.get(ScheduleBTransactionTypeLabels, ScheduleBTransactionTypes.RECOUNT_ACCOUNT_DISBURSEMENT);
+  schema = schema;
+  override defaultContactTypeOption = ContactTypes.ORGANIZATION;
+  override navigationControls: TransactionNavigationControls = STANDARD_CONTROLS;
+  override purposeDescriptionPrefix = 'Recount Account: ';
+
+  getNewTransaction() {
+    return SchBTransaction.fromJSON({
+      form_type: 'SB29',
+      transaction_type_identifier: ScheduleBTransactionTypes.RECOUNT_ACCOUNT_DISBURSEMENT,
+      aggregation_group: AggregationGroups.GENERAL_DISBURSEMENT,
+    });
+  }
+}
