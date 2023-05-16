d2c64ef493165a674defe3954240b89fbc9eb49a
toddlees <todd.lees@tcg.com>
2023-05-16 13:20:06 -0400

    disable election type and year inputs

--- front-end/src/app/shared/components/inputs/election-input/election-input.component.ts
+++ front-end/src/app/shared/components/inputs/election-input/election-input.component.ts
@@ -18,17 +18,20 @@ export class ElectionInputComponent extends BaseInputComponent implements OnInit
   ];
 
   ngOnInit(): void {
-    // Create two additional form controls that will join values to populate the composit election_code form control value
-    this.form.addControl('electionType', new FormControl('', Validators.required));
-    this.form.addControl('electionYear', new FormControl('', [Validators.required, Validators.pattern('\\d{4}')]));
-
     // Get inital values for election type and year for additional form inputs
-    const election_code = this.form.get('election_code')?.value;
-    if (election_code) {
-      this.form.patchValue({
-        electionType: election_code.slice(0, 1),
-        electionYear: election_code.slice(1, 5),
-      });
+    const election_code = this.form.get('election_code');
+    const electionType = election_code?.value?.slice(0, 1) || '';
+    const electionYear = election_code?.value?.slice(1, 5) || '';
+
+    // Create two additional form controls that will join values to populate the composit election_code form control value
+    this.form.addControl('electionType', new FormControl(electionType, Validators.required));
+    this.form.addControl(
+      'electionYear',
+      new FormControl(electionYear, [Validators.required, Validators.pattern('\\d{4}')])
+    );
+
+    if (election_code?.disabled) {
+      this.form.disable();
     }
 
     // Listen to election type and year form inputs and update election_code upon value changes
