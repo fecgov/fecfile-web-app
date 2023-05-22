f20154a9fd3efb5c526af147ae2ffa4a88eb8bff
David Heitzer <david.heitzer@tcg.com>
2023-05-22 10:57:52 -0400

    1106 address CR comments

--- front-end/src/app/shared/components/inputs/amount-input/amount-input.component.ts
+++ front-end/src/app/shared/components/inputs/amount-input/amount-input.component.ts
@@ -74,8 +74,8 @@ export class AmountInputComponent extends BaseInputComponent implements OnInit, 
   }
 
   updateMemoItemWithDate(date: Date) {
-    if (date && this.report?.coverage_from_date && this.report?.coverage_through_date) {
-      if (date < this.report.coverage_from_date || date > this.report.coverage_through_date) {
+    if (this.report?.coverage_from_date && this.report?.coverage_through_date) {
+      if (date && (date < this.report.coverage_from_date || date > this.report.coverage_through_date)) {
         this.memoControl.addValidators(Validators.requiredTrue);
         this.memoControl.markAsTouched();
         this.memoControl.updateValueAndValidity();
