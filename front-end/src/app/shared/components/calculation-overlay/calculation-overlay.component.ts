1aa19531a0572a44ea9dd42dfc0b4f4cdfd7b3dc
toddlees <todd.lees@tcg.com>
2023-05-23 14:01:12 -0400

    lint

--- front-end/src/app/shared/components/calculation-overlay/calculation-overlay.component.ts
+++ front-end/src/app/shared/components/calculation-overlay/calculation-overlay.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calculation-overlay',
   templateUrl: './calculation-overlay.component.html',
-  styleUrls: ['./calculation-overlay.component.scss']
+  styleUrls: ['./calculation-overlay.component.scss'],
 })
-export class CalculationOverlayComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CalculationOverlayComponent {
+  constructor() {}
 }
