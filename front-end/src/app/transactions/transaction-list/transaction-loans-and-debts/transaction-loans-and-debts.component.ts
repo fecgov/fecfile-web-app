a792d729b49ebba8a747d4b12490d2826aa8462c
Elaine Krauss <elaine.krauss@tcg.com>
2023-06-08 17:32:41 -0400

    WIP adding loans and debts page

---
+++ front-end/src/app/transactions/transaction-list/transaction-loans-and-debts/transaction-loans-and-debts.component.ts
@@ -0,0 +1,31 @@
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { TransactionSchAService } from 'app/shared/services/transaction-schA.service';
+import { ScheduleATransactionTypeLabels } from 'app/shared/models/scha-transaction.model';
+import { LabelList } from 'app/shared/utils/label.utils';
+import { TransactionListTableBaseComponent } from '../transaction-list-table-base.component';
+import { Store } from '@ngrx/store';
+import { ReportService } from 'app/shared/services/report.service';
+
+@Component({
+  selector: 'app-transaction-loans-and-debts',
+  templateUrl: './transaction-loans-and-debts.component.html',
+  styleUrls: ['../../transaction.scss'],
+})
+export class TransactionLoansAndDebtsComponent extends TransactionListTableBaseComponent implements OnInit {
+  scheduleTransactionTypeLabels: LabelList = ScheduleATransactionTypeLabels;
+
+  constructor(
+    protected override messageService: MessageService,
+    protected override confirmationService: ConfirmationService,
+    protected override elementRef: ElementRef,
+    protected override activatedRoute: ActivatedRoute,
+    protected override router: Router,
+    protected override itemService: TransactionSchAService,
+    protected override store: Store,
+    protected override reportService: ReportService
+  ) {
+    super(messageService, confirmationService, elementRef, activatedRoute, router, store, reportService);
+  }
+}
