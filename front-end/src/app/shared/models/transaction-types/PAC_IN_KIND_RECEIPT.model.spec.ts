3fb5eccf707ada4f5787d301479c876efea134fa
Elaine Krauss <elaine.krauss@tcg.com>
2023-06-09 15:57:51 -0400

    Adds the PAC IN KIND transactions

---
+++ front-end/src/app/shared/models/transaction-types/PAC_IN_KIND_RECEIPT.model.spec.ts
@@ -0,0 +1,22 @@
+import { SchATransaction, ScheduleATransactionTypes } from '../scha-transaction.model';
+import { getTestTransactionByType } from 'app/shared/utils/unit-test.utils';
+import { TransactionGroupEE } from '../transaction-groups/transaction-group-ee.model';
+
+describe('PAC_IN_KIND_RECEIPT', () => {
+  let transaction: SchATransaction;
+
+  beforeEach(() => {
+    transaction = getTestTransactionByType(ScheduleATransactionTypes.PAC_IN_KIND_RECEIPT) as SchATransaction;
+  });
+
+  it('should create an instance', () => {
+    expect(transaction.transactionType).toBeTruthy();
+    expect(transaction.transactionType?.scheduleId).toBe('A');
+    expect(transaction?.transactionType?.transactionGroup).toBeInstanceOf(TransactionGroupEE);
+  });
+
+  it('#factory() should return a SchATransaction', () => {
+    expect(transaction.form_type).toBe('SA11C');
+    expect(transaction.transaction_type_identifier).toBe(ScheduleATransactionTypes.PAC_IN_KIND_RECEIPT);
+  });
+});
