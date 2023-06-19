f55aac85b01bfa023eb33b7750b1dbdfde4fb434
Matt Travers <matt.travers@tcg.com>
2023-06-19 15:39:03 -0400

    Update varaible names for committee type and status codes

--- front-end/src/app/layout/committee-banner/committee-banner.component.ts
+++ front-end/src/app/layout/committee-banner/committee-banner.component.ts
@@ -4,6 +4,36 @@ import { takeUntil } from 'rxjs';
 import { selectCommitteeAccount } from 'app/store/committee-account.selectors';
 import { DestroyerComponent } from 'app/shared/components/app-destroyer.component';
 
+const committeeStatusCodes: { [key: string]: string } = {
+  T: 'Terminated',
+  A: 'Administratively terminated',
+  D: 'Active - Debt',
+  W: 'Active - Waived',
+  M: 'Active - Monthly',
+  Q: 'Active - Quarterly',
+};
+
+const activeStatusCodes = ['M', 'Q', 'W', 'D'];
+
+const committeeTypeCodes: { [key: string]: string } = {
+  C: 'communication cost',
+  D: 'delegate',
+  E: 'electioneering communication',
+  H: 'House',
+  I: 'independent expenditure filer (not a committee)',
+  N: 'PAC - nonqualified',
+  O: 'independent expenditure-only (super PACs)',
+  P: 'presidential',
+  Q: 'PAC - qualified',
+  S: 'Senate',
+  U: 'single candidate independent expenditure',
+  V: 'PAC with non-contribution account, nonqualified',
+  W: 'PAC with non-contribution account, qualified',
+  X: 'party, nonqualified',
+  Y: 'party, qualified',
+  Z: 'national party non-federal account',
+};
+
 @Component({
   selector: 'app-committee-banner',
   templateUrl: './committee-banner.component.html',
@@ -20,36 +50,6 @@ export class CommitteeBannerComponent extends DestroyerComponent implements OnIn
     super();
   }
 
-  public committee_statuses: { [key: string]: string } = {
-    T: 'Terminated',
-    A: 'Administratively terminated',
-    D: 'Active - Debt',
-    W: 'Active - Waived',
-    M: 'Active - Monthly',
-    Q: 'Active - Quarterly',
-  };
-
-  public committee_types: { [key: string]: string } = {
-    C: 'communication cost',
-    D: 'delegate',
-    E: 'electioneering communication',
-    H: 'House',
-    I: 'independent expenditure filer (not a committee)',
-    N: 'PAC - nonqualified',
-    O: 'independent expenditure-only (super PACs)',
-    P: 'presidential',
-    Q: 'PAC - qualified',
-    S: 'Senate',
-    U: 'single candidate independent expenditure',
-    V: 'PAC with non-contribution account, nonqualified',
-    W: 'PAC with non-contribution account, qualified',
-    X: 'party, nonqualified',
-    Y: 'party, qualified',
-    Z: 'national party non-federal account',
-  };
-
-  public activeCommittees = ['M', 'Q', 'W', 'D'];
-
   ngOnInit(): void {
     this.store
       .select(selectCommitteeAccount)
@@ -60,13 +60,13 @@ export class CommitteeBannerComponent extends DestroyerComponent implements OnIn
 
         const frequencyCode = committeeAccount.filing_frequency;
         if (frequencyCode) {
-          this.committeeFrequency = this.committee_statuses[frequencyCode] ?? '';
-          this.committeeStatus = this.activeCommittees.includes(frequencyCode) ? 'Active' : 'Inactive';
+          this.committeeFrequency = committeeStatusCodes[frequencyCode] ?? '';
+          this.committeeStatus = activeStatusCodes.includes(frequencyCode) ? 'Active' : 'Inactive';
         }
 
         const committeeTypeCode = committeeAccount.committee_type;
         if (committeeTypeCode) {
-          this.committeeType = this.committee_types[committeeTypeCode] ?? '';
+          this.committeeType = committeeTypeCodes[committeeTypeCode] ?? '';
         }
       });
   }
