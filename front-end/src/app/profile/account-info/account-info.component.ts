ab355f22a36c736e54bcf06dad6421be88296820
Elaine Krauss <elaine.krauss@tcg.com>
2023-06-29 18:16:45 -0400

    All fields auto-populate with the committee data

--- front-end/src/app/profile/account-info/account-info.component.ts
+++ front-end/src/app/profile/account-info/account-info.component.ts
@@ -1,31 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { CommitteeAccount } from 'app/shared/models/committee-account.model';
 import { FecFiling } from 'app/shared/models/fec-filing.model';
 import { FecApiService } from 'app/shared/services/fec-api.service';
+import { ValidateUtils } from 'app/shared/utils/validate.utils';
 import { selectCommitteeAccount } from 'app/store/committee-account.selectors';
-import { Observable, switchMap } from 'rxjs';
+import { Observable, Subject, switchMap, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './account-info.component.html',
   styleUrls: ['./account-info.component.scss'],
 })
-export class AccountInfoComponent implements OnInit {
+export class AccountInfoComponent implements OnInit, OnDestroy {
   committeeAccount$: Observable<CommitteeAccount> | undefined;
   mostRecentFilingPdfUrl: string | null | undefined = undefined;
+  destroy$: Subject<boolean> = new Subject<boolean>();
   form: FormGroup = this.fb.group({});
+  formProperties: string[] = [
+    'name',
+    'committee_id',
+    'committee_type_full',
+    'street_1',
+    'street_2',
+    'city',
+    'state',
+    'zip',
+    'email',
+    'website',
+    'treasurer_name_1',
+    'treasurer_name_2',
+    'treasurer_street_1',
+    'treasurer_street_2',
+    'treasurer_city',
+    'treasurer_state',
+    'treasurer_zip',
+    'treasurer_phone',
+    'custodian_name_full',
+  ];
 
   constructor(private store: Store, private fecApiService: FecApiService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.fb.group(ValidateUtils.getFormGroupFields(this.formProperties));
     this.committeeAccount$ = this.store.select(selectCommitteeAccount);
     this.committeeAccount$
       .pipe(switchMap((committeeAccount) => this.fecApiService.getCommitteeRecentFiling(committeeAccount.committee_id)))
       .subscribe((mostRecentFiling: FecFiling | undefined) => {
         this.mostRecentFilingPdfUrl = mostRecentFiling?.pdf_url;
       });
+
+    this.committeeAccount$.pipe(takeUntil(this.destroy$)).subscribe((committee: CommitteeAccount) => {
+      const entries = Object.entries(committee);
+      for (const [key, value] of entries) {
+        if (this.formProperties.includes(key)) {
+          this.form.get(key)?.setValue(value);
+        }
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
   }
 
   /**
