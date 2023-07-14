1860fdb15f0fc6095010d5fce4264b640d465a50
toddlees <todd.lees@tcg.com>
2023-07-14 18:20:35 -0400

    support transactions with committees but no committee id

--- front-end/src/app/shared/components/inputs/loan-info-input/loan-info-input.component.ts
+++ front-end/src/app/shared/components/inputs/loan-info-input/loan-info-input.component.ts
@@ -14,13 +14,7 @@ export class LoanInfoInputComponent extends BaseInputComponent implements OnInit
   @Input() memoItemHelpText: string | undefined;
   @Input() transaction: Transaction | undefined;
 
-  inputStyleClass = '';
-
   ngOnInit(): void {
-    if (this.readonly) {
-      this.inputStyleClass = 'readonly';
-    }
-
     // Set value to zero until ticket #1103 implemented
     this.form.get('loan_payment_to_date')?.setValue(0);
 
