bb3c5ee1e8ff5ccbfcda034bc34329996fabe606
Matt Travers <matt.travers@tcg.com>
2023-07-18 17:07:18 -0400

    Add unit tests

--- front-end/src/app/shared/components/inputs/loan-terms-input/loan-terms-input.component.spec.ts
+++ front-end/src/app/shared/components/inputs/loan-terms-input/loan-terms-input.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { provideMockStore } from '@ngrx/store/testing';
+import { testMockStore, testTemplateMap } from 'app/shared/utils/unit-test.utils';
 import { LoanTermsInputComponent } from './loan-terms-input.component';
 
 describe('LoanTermsInputComponent', () => {
@@ -8,14 +10,29 @@ describe('LoanTermsInputComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LoanTermsInputComponent]
+      declarations: [LoanTermsInputComponent],
+      providers: [provideMockStore(testMockStore)],
     });
     fixture = TestBed.createComponent(LoanTermsInputComponent);
     component = fixture.componentInstance;
+    component.templateMap = testTemplateMap;
+    component.form = new FormGroup({
+      [testTemplateMap.date]: new FormControl(''),
+      load_due_date: new FormControl(''),
+      loan_interest_rate: new FormControl(''),
+      secured: new FormControl(''),
+    });
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have an invalid INCURRED DATE input if outside the report date range', () => {
+    const control = component.form.get(component.templateMap.date);
+    expect(control?.status).toBe('VALID');
+    control?.setValue(new Date('January 1, 2015 00:00:00'));
+    expect(control?.status).toBe('INVALID');
   });
 });
--- front-end/src/app/shared/utils/unit-test.utils.ts
+++ front-end/src/app/shared/utils/unit-test.utils.ts
@@ -91,6 +91,7 @@ export const testUserLoginData: UserLoginData = {
 export const testActiveReport: F3xSummary = F3xSummary.fromJSON({
   id: '999',
   coverage_from_date: '2022-05-25',
+  coverage_through_date: '2022-06-30',
   form_type: 'F3XN',
   report_code: 'Q1',
   upload_submission: UploadSubmission.fromJSON({}),
@@ -242,5 +243,5 @@ export const testTemplateMap: TransactionTemplateMapType = {
   category_code: '',
   election_code: 'election_code',
   election_other_description: 'election_other_description',
-  candidateInputHeader: ''
+  candidateInputHeader: '',
 };
