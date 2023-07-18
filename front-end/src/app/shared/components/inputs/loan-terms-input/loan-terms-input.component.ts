4fcead7e4f21448e1ce2f85fea58522b0bc91f3f
Matt Travers <matt.travers@tcg.com>
2023-07-18 16:22:58 -0400

    Add date range validation to Date Incurred input

--- front-end/src/app/shared/components/inputs/loan-terms-input/loan-terms-input.component.ts
+++ front-end/src/app/shared/components/inputs/loan-terms-input/loan-terms-input.component.ts
@@ -1,11 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectActiveReport } from 'app/store/active-report.selectors';
+import { take } from 'rxjs';
 import { BaseInputComponent } from '../base-input.component';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { DateUtils } from 'app/shared/utils/date.utils';
+
+function dateWithinReportRange(coverage_from_date?: Date, coverage_through_date?: Date): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const date = control.value;
+
+    if (!DateUtils.isWithin(date, coverage_from_date, coverage_through_date)) {
+      const message = `This date must fall within the coverage dates of ${DateUtils.convertDateToSlashFormat(
+        coverage_from_date
+      )} - ${DateUtils.convertDateToSlashFormat(coverage_through_date)} for this report.`;
+      return { invaliddate: { msg: message } };
+    }
+
+    return null;
+  };
+}
 
 @Component({
   selector: 'app-loan-terms-input',
   templateUrl: './loan-terms-input.component.html',
 })
 export class LoanTermsInputComponent extends BaseInputComponent implements OnInit {
+  constructor(private store: Store) {
+    super();
+  }
+
   ngOnInit(): void {
     if (this.form.get('secured')?.value === null) {
       this.form.get('secured')?.setValue(false);
@@ -14,5 +38,15 @@ export class LoanTermsInputComponent extends BaseInputComponent implements OnIni
     // Set empty values until ticket #1156 implemented
     this.form.get('loan_due_date')?.setValue('-');
     this.form.get('loan_interest_rate')?.setValue('-');
+
+    // Add the date range validation check to the DATE INCURRED input
+    this.store
+      .select(selectActiveReport)
+      .pipe(take(1))
+      .subscribe((report) => {
+        this.form
+          .get(this.templateMap.date)
+          ?.addValidators(dateWithinReportRange(report.coverage_from_date, report.coverage_through_date));
+      });
   }
 }
