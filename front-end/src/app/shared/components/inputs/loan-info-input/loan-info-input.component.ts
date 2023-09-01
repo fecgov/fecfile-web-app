e503ef90909f32fb869c30594809c080d144a79f
Matt Travers <matt.travers@tcg.com>
2023-09-01 18:31:35 -0400

    Dynamically update loan balance when original loan amount edited in the form

--- front-end/src/app/shared/components/inputs/loan-info-input/loan-info-input.component.ts
+++ front-end/src/app/shared/components/inputs/loan-info-input/loan-info-input.component.ts
@@ -14,18 +14,24 @@ export class LoanInfoInputComponent extends BaseInputComponent implements OnInit
   @Input() transaction: Transaction | undefined;
 
   ngOnInit(): void {
-    // For new create transactions, the PAYMENT TO DATE is set to 0 and BALANCE
-    // OUTSTANDING is updated to match the ORIGINAL AMOUNT.
-    // These are a calculated fields so these initial values are not saved to the database.
+    // For new create transactions, the PAYMENT TO DATE is initialized to 0
+    // It is a calculated field and not saved to the database
     if (!this.transaction?.id) {
       this.form.get(this.templateMap.payment_to_date)?.setValue(0);
-
-      this.form
-        .get(this.templateMap.amount)
-        ?.valueChanges.pipe(takeUntil(this.destroy$))
-        .subscribe((value) => {
-          this.form.get(this.templateMap.balance)?.setValue(value);
-        });
     }
+
+    // balance is a calculated field and not saved to the database
+    this.form
+      .get(this.templateMap.amount)
+      ?.valueChanges.pipe(takeUntil(this.destroy$))
+      .subscribe((amount) => {
+        let balance = amount;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const payment_to_date = (<any>this.transaction)[this.templateMap.payment_to_date];
+        if (payment_to_date) {
+          balance = amount - payment_to_date;
+        }
+        this.form.get(this.templateMap.balance)?.setValue(balance);
+      });
   }
 }
