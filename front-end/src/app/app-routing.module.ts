1b8cf3a087e0daebdfb1145d3422ecae438678df
Elaine Krauss <elaine.krauss@tcg.com>
2023-09-04 10:27:24 -0400

    Sets a flag during the router module import that tells the router to scroll to the top on route changes

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -43,7 +43,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
+  imports: [
+    RouterModule.forRoot(routes, {
+      onSameUrlNavigation: 'reload',
+      scrollPositionRestoration: 'top',
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
