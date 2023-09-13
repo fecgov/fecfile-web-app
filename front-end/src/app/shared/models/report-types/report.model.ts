a1eeeda1d8bc3a500e2707ec05bb50bbe9bd2441
Elaine Krauss <elaine.krauss@tcg.com>
2023-09-13 17:20:57 -0400

    Changes fields in the report model from  to

--- front-end/src/app/shared/models/report-types/report.model.ts
+++ front-end/src/app/shared/models/report-types/report.model.ts
@@ -17,19 +17,19 @@ export abstract class Report extends BaseModel {
   @Transform(BaseModel.dateTransform) coverage_from_date?: Date;
   @Transform(BaseModel.dateTransform) coverage_through_date?: Date;
 
-  committee_name: string | undefined;
+  committee_name?: string;
 
-  street_1: string | undefined;
-  street_2: string | undefined;
-  city: string | undefined;
-  state: string | undefined;
-  zip: string | undefined;
+  street_1?: string;
+  street_2?: string;
+  city?: string;
+  state?: string;
+  zip?: string;
 
-  treasurer_last_name: string | undefined;
-  treasurer_first_name: string | undefined;
-  treasurer_middle_name: string | undefined;
-  treasurer_prefix: string | undefined;
-  treasurer_suffix: string | undefined;
+  treasurer_last_name?: string;
+  treasurer_first_name?: string;
+  treasurer_middle_name?: string;
+  treasurer_prefix?: string;
+  treasurer_suffix?: string;
 
   @Type(() => UploadSubmission)
   @Transform(UploadSubmission.transform)
