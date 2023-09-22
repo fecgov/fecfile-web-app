d279628f147fd57d7425670efec7370ed332bb86
Matt Travers <matt.travers@tcg.com>
2023-09-22 10:19:46 -0400

    Add unit test coverage

--- front-end/src/app/shared/models/transaction-types/LOAN_RECEIVED_FROM_INDIVIDUAL.model.spec.ts
+++ front-end/src/app/shared/models/transaction-types/LOAN_RECEIVED_FROM_INDIVIDUAL.model.spec.ts
@@ -17,4 +17,26 @@ describe('LOAN_RECEIVED_FROM_INDIVIDUAL', () => {
     expect(transaction.form_type).toBe('SC/10');
     expect(transaction.transaction_type_identifier).toBe(ScheduleCTransactionTypes.LOAN_RECEIVED_FROM_INDIVIDUAL);
   });
+
+  it('should change navigation control if pulled forward', () => {
+    let navControls = transaction.transactionType.navigationControls;
+    if (navControls?.continueControls) expect(navControls.continueControls[0].label).toBe('Save both transactions');
+    navControls = transaction.transactionType.getNavigationControls(transaction);
+    if (navControls?.continueControls) expect(navControls.continueControls[0].label).toBe('Save both transactions');
+    transaction.loan_id = 'test';
+    navControls = transaction.transactionType.getNavigationControls(transaction);
+    if (navControls?.continueControls) expect(navControls.continueControls[0].label).toBe('Save');
+  });
+
+  it('should change footer if pulled forward', () => {
+    const footerText =
+      'The information in this loan will automatically create a related receipt. Review the receipt; enter a purpose of receipt or note/memo text; or continue without reviewing and “Save transactions.”';
+    let footer = transaction.transactionType.footer;
+    expect(footer).toBe(footerText);
+    footer = transaction.transactionType.getFooter(transaction);
+    expect(footer).toBe(footerText);
+    transaction.loan_id = 'test';
+    footer = transaction.transactionType.getFooter(transaction);
+    expect(footer).toBeUndefined();
+  });
 });
