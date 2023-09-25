80c659982cca2a087df299e0da09dc3b492b6f67
toddlees <todd.lees@tcg.com>
2023-09-25 16:09:45 -0400

    corrects debt payment calculation on front end

--- front-end/src/app/shared/components/inputs/debt-input/debt-input.component.ts
+++ front-end/src/app/shared/components/inputs/debt-input/debt-input.component.ts
@@ -25,14 +25,12 @@ export class DebtInputComponent extends BaseInputComponent implements OnInit {
       ?.valueChanges.pipe(takeUntil(this.destroy$))
       .subscribe((amount) => {
         amount = isNaN(parseFloat(amount)) ? 0 : parseFloat(amount);
-        let balance_at_close = amount;
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const beginning_balance = parseFloat((<any>this.transaction)[this.templateMap.balance]);
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const payment_amount = parseFloat((<any>this.transaction)['payment_amount']);
-        if (payment_amount) {
-          balance_at_close = beginning_balance + (amount - payment_amount);
-        }
+        const payment_amount = parseFloat((<any>this.transaction)['payment_amount']) ?? 0;
+        const balance_at_close = beginning_balance + amount - payment_amount;
+
         this.form.get('balance_at_close')?.setValue(balance_at_close);
       });
   }
