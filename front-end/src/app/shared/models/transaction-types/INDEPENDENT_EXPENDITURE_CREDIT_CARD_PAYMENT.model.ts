052337c075d88089a85579ddbebf33bbaeb3dc97
Elaine Krauss <elaine.krauss@tcg.com>
2023-10-06 14:21:11 -0400

    Implements the Independent Expenditure CC/Payroll/Reimbursement transactions

---
+++ front-end/src/app/shared/models/transaction-types/INDEPENDENT_EXPENDITURE_CREDIT_CARD_PAYMENT.model.ts
@@ -0,0 +1,61 @@
+import { LabelUtils } from 'app/shared/utils/label.utils';
+import { schema } from 'fecfile-validate/fecfile_validate_js/dist/INDEPENDENT_EXPENDITURE_PARENTS';
+import { SchETransactionType } from '../sche-transaction-type.model';
+import { SchETransaction, ScheduleETransactionTypeLabels, ScheduleETransactionTypes } from '../sche-transaction.model';
+import { STANDARD_CONTROLS, TransactionNavigationControls } from '../transaction-navigation-controls.model';
+import {
+  ORG_FIELDS,
+  ADDRESS_FIELDS,
+  ELECTION_FIELDS,
+  COMMON_FIELDS,
+  CATEGORY_CODE,
+  SIGNATORY_1_FIELDS,
+  AGGREGATE,
+  CANDIDATE_FIELDS,
+  CANDIDATE_OFFICE_FIELDS,
+  ORGANIZATION,
+} from 'app/shared/utils/transaction-type-properties';
+import { STANDARD_AND_CANDIDATE } from '../contact.model';
+import { AggregationGroups, Transaction } from '../transaction.model';
+import { SubTransactionGroup } from '../transaction-type.model';
+
+export class INDEPENDENT_EXPENDITURE_CREDIT_CARD_PAYMENT extends SchETransactionType {
+  formFields = [
+    ...ORG_FIELDS,
+    ...ADDRESS_FIELDS,
+    ...CANDIDATE_FIELDS,
+    ...CANDIDATE_OFFICE_FIELDS,
+    ...ELECTION_FIELDS,
+    ...COMMON_FIELDS,
+    ...CATEGORY_CODE,
+    ...SIGNATORY_1_FIELDS,
+    ...AGGREGATE,
+    'date2',
+    'support_oppose_code',
+  ];
+  contactTypeOptions = ORGANIZATION;
+  override contactConfig = STANDARD_AND_CANDIDATE;
+  title = LabelUtils.get(
+    ScheduleETransactionTypeLabels,
+    ScheduleETransactionTypes.INDEPENDENT_EXPENDITURE_CREDIT_CARD_PAYMENT
+  );
+  schema = schema;
+  override navigationControls: TransactionNavigationControls = STANDARD_CONTROLS;
+  override contact2IsRequired = () => true;
+  override subTransactionConfig = new SubTransactionGroup('Independent Expenditure Credit Card Memo', [
+    ScheduleETransactionTypes.INDEPENDENT_EXPENDITURE_CREDIT_CARD_PAYMENT_MEMO,
+  ]);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  override generatePurposeDescription(transaction: Transaction): string {
+    return 'Credit Card: See Below';
+  }
+
+  getNewTransaction() {
+    return SchETransaction.fromJSON({
+      form_type: 'SE',
+      transaction_type_identifier: ScheduleETransactionTypes.INDEPENDENT_EXPENDITURE_CREDIT_CARD_PAYMENT,
+      aggregation_group: AggregationGroups.INDEPENDENT_EXPENDITURE,
+    });
+  }
+}
---
+++ front-end/src/app/shared/models/transaction-types/INDEPENDENT_EXPENDITURE_STAFF_REIMBURSEMENT.model.spec.ts
@@ -0,0 +1,26 @@
+import { TransactionType } from 'app/shared/models/transaction-type.model';
+import { SchETransaction, ScheduleETransactionTypes } from '../sche-transaction.model';
+import { INDEPENDENT_EXPENDITURE_STAFF_REIMBURSEMENT } from './INDEPENDENT_EXPENDITURE_STAFF_REIMBURSEMENT.model';
+
+describe('INDEPENDENT_EXPENDITURE_STAFF_REIMBURSEMENT', () => {
+  let transactionType: INDEPENDENT_EXPENDITURE_STAFF_REIMBURSEMENT;
+
+  beforeEach(() => {
+    transactionType = new INDEPENDENT_EXPENDITURE_STAFF_REIMBURSEMENT();
+  });
+
+  it('should create an instance', () => {
+    expect(transactionType).toBeTruthy();
+    expect(transactionType.scheduleId).toBe('E');
+  });
+
+  it('#factory() should return a SchETransaction', () => {
+    const txn: SchETransaction = transactionType.getNewTransaction();
+    expect(txn.form_type).toBe('SE');
+    expect(txn.transaction_type_identifier).toBe(ScheduleETransactionTypes.INDEPENDENT_EXPENDITURE_STAFF_REIMBURSEMENT);
+  });
+
+  it('#generatePurposeDescription() should not be defined', () => {
+    expect((transactionType as TransactionType).generatePurposeDescription).toEqual('Reimbursement: See Below');
+  });
+});
