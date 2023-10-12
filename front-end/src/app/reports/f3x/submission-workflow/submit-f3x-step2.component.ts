af15ef616e9018c229386c224abae80c1794eec0
Matt Travers <matt.travers@tcg.com>
2023-10-12 15:39:32 -0400

    Fix spelling typo

--- front-end/src/app/reports/f3x/submission-workflow/submit-f3x-step2.component.ts
+++ front-end/src/app/reports/f3x/submission-workflow/submit-f3x-step2.component.ts
@@ -58,7 +58,7 @@ export class SubmitF3xStep2Component extends DestroyerComponent implements OnIni
       .select(selectActiveReport)
       .pipe(takeUntil(this.destroy$))
       .subscribe((report) => {
-        this.report = report as ReportF3X;
+        this.report = report;
       });
     this.store
       .select(selectCommitteeAccount)
