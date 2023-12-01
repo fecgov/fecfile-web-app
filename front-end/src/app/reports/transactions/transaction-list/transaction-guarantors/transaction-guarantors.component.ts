e434a4e1c8880b44a9e8d7b692e99ea65caffc3d
toddlees <todd.lees@tcg.com>
2023-12-01 11:02:29 -0500

    tmp

--- front-end/src/app/reports/transactions/transaction-list/transaction-guarantors/transaction-guarantors.component.ts
+++ front-end/src/app/reports/transactions/transaction-list/transaction-guarantors/transaction-guarantors.component.ts
@@ -1,18 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { Transaction } from 'app/shared/models/transaction.model';
 import { TransactionChildrenComponent } from '../transaction-children-list-table-base.component';
 import { ScheduleC2TransactionTypes } from 'app/shared/models/schc2-transaction.model';
+import { TableAction } from 'app/shared/components/table-list-base/table-list-base.component';
+import { TransactionListComponent } from '../transaction-list.component';
+import { TransactionListTableBaseComponent } from '../transaction-list-table-base.component';
+import { LabelList, LineIdentifierLabels } from 'app/shared/utils/label.utils';
+import { ScheduleC2TransactionTypeLabels } from 'app/shared/models/schc2-transaction.model';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { ReportService } from 'app/shared/services/report.service';
+import { TransactionSchAService } from 'app/shared/services/transaction-schA.service';
+import { TransactionSchC2Service } from 'app/shared/services/transaction-schC2.service';
 
 @Component({
   selector: 'app-transaction-guarantors',
   templateUrl: './transaction-guarantors.component.html',
   styleUrls: ['../../transaction.scss'],
 })
-export class TransactionGuarantorsComponent extends TransactionChildrenComponent implements OnInit {
-  override tableLabel = 'Guarantors';
-  override amountHeader = 'Guaranteed financial information amount';
+export class TransactionGuarantorsComponent extends TransactionListTableBaseComponent implements OnInit {
+  scheduleTransactionTypeLabels: LabelList = ScheduleC2TransactionTypeLabels;
+  lineLabels = LineIdentifierLabels;
 
-  public override transactionFilter(transaction: Transaction): boolean {
-    return transaction.transaction_type_identifier === ScheduleC2TransactionTypes.C2_LOAN_GUARANTOR;
+  constructor(
+    protected override messageService: MessageService,
+    protected override confirmationService: ConfirmationService,
+    protected override elementRef: ElementRef,
+    protected override activatedRoute: ActivatedRoute,
+    protected override router: Router,
+    protected override itemService: TransactionSchC2Service,
+    protected override store: Store,
+    protected override reportService: ReportService
+  ) {
+    super(messageService, confirmationService, elementRef, activatedRoute, router, store, reportService);
   }
+
+  override rowActions: TableAction[] = [
+    new TableAction(
+      'View',
+      this.editItem.bind(this),
+      () => !this.reportIsEditable,
+      () => true
+    ),
+    new TableAction(
+      'Edit',
+      this.editItem.bind(this),
+      () => this.reportIsEditable,
+      () => true
+    ),
+    new TableAction(
+      'Delete',
+      this.deleteItem.bind(this),
+      () => this.reportIsEditable,
+      () => true
+    ),
+  ];
 }
---
+++ front-end/src/app/shared/services/transaction-schC2.service.ts
@@ -0,0 +1,27 @@
+import { DatePipe } from '@angular/common';
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { TransactionService } from './transaction.service';
+import { Observable } from 'rxjs';
+import { ListRestResponse } from '../models/rest-api.model';
+import { SchCTransaction } from '../models/schc-transaction.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TransactionSchC2Service extends TransactionService {
+  override tableDataEndpoint = '/transactions';
+  public transaction?: SchCTransaction;
+
+  override getTableData(
+    pageNumber?: number,
+    ordering?: string,
+    params?: { [param: string]: string | number | boolean | readonly (string | number | boolean)[] }
+  ): Observable<ListRestResponse> {
+    return super.getTableData(pageNumber, ordering, { ...params, schedules: 'A' });
+  }
+
+  constructor(override apiService: ApiService, override datePipe: DatePipe) {
+    super(apiService, datePipe);
+  }
+}
