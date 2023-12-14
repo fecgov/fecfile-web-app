40208d544150c5b0b389117b9f64ca3605a1ca34
Matt Travers <matt.travers@tcg.com>
2023-12-14 14:22:29 -0500

    Fix unit tests

--- front-end/src/app/layout/layout.component.spec.ts
+++ front-end/src/app/layout/layout.component.spec.ts
@@ -14,7 +14,8 @@ import { filter, Subject } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { setSidebarStateAction } from 'app/store/sidebar-state.actions';
 import { CommitteeBannerComponent } from './committee-banner/committee-banner.component';
-import { Event, NavigationEnd, Router } from "@angular/router";
+import { selectSidebarState } from 'app/store/sidebar-state.selectors';
+import { Event, NavigationEnd, Router } from '@angular/router';
 
 describe('LayoutComponent', () => {
   let component: LayoutComponent;
@@ -46,39 +47,23 @@ describe('LayoutComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should not set the sidebar by default', waitForAsync(() => {
-    component.sidebarState$?.subscribe((state) => expect(state).toBe(undefined));
-  }));
-
-  it('should set the sidebar to the transaction section', () => {
-    store.dispatch(setSidebarStateAction({payload: new SidebarState(ReportSidebarState.TRANSACTIONS)}));
-    component.sidebarState$
-      ?.pipe(filter((state) => !!state))
-      .subscribe((state) => expect(state?.section).toBe(ReportSidebarState.TRANSACTIONS));
+  it('should set showSidebar to true when location has a sidebar action', () => {
+    store.dispatch(setSidebarStateAction({ payload: new SidebarState(ReportSidebarState.TRANSACTIONS) }));
+    store
+      .select(selectSidebarState)
+      .pipe(filter((state) => !!state))
+      .subscribe(() => {
+        expect(component.showSidebar).toBeTruthy();
+      });
   });
 
-  it('should set isReports to false when location is base reports module', () => {
-    const event = new NavigationEnd(42, '/reports', '/');
-    (TestBed.inject(Router).events as Subject<Event>).next(event);
-    expect(component.isReports).toBeFalsy();
-  });
-
-  it('should set isReports to true when location is reports module', () => {
-    const event = new NavigationEnd(42, '/reports/transactions/report/3fa0fd7e-306e-4cb6-8c8d-9ff9306092a6/list', '/');
-    (TestBed.inject(Router).events as Subject<Event>).next(event);
-    expect(component.isReports).toBeTruthy();
-  });
-
-  it('should be visible only when showSidebar and isReports are both true', () => {
-    expect(component.isVisible).toBeFalsy();
-    component.showSidebar = true;
-    const event = new NavigationEnd(42, '/reports/transactions/report/3fa0fd7e-306e-4cb6-8c8d-9ff9306092a6/list', '/');
-    (TestBed.inject(Router).events as Subject<Event>).next(event);
-    expect(component.isReports).toBeTruthy();
-    expect(component.showSidebar).toBeTruthy();
-    expect(component.isVisible).toBeTruthy();
-
-    component.showSidebar = false;
-    expect(component.isVisible).toBeFalsy();
+  it('should set showSidebar to false when location does not have a sidebar action', () => {
+    store.dispatch(setSidebarStateAction({ payload: undefined }));
+    store
+      .select(selectSidebarState)
+      .pipe(filter((state) => !!state))
+      .subscribe(() => {
+        expect(component.showSidebar).toBeFalsy();
+      });
   });
 });
