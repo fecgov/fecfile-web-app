2cf1bf1292a00e713ca94314ac63a10bd49655c3
toddlees <todd.lees@tcg.com>
2023-12-14 12:20:40 -0500

    undo layout component

--- front-end/src/app/layout/layout.component.ts
+++ front-end/src/app/layout/layout.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { takeUntil, Observable } from 'rxjs';
+import { filter, Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
-// import { selectCommitteeAccount } from '../store/committee-account.selectors';
+import { selectCommitteeAccount } from '../store/committee-account.selectors';
 import { selectSpinnerStatus } from '../store/spinner.selectors';
-// import { CommitteeAccount } from 'app/shared/models/committee-account.model';
-import { ActivatedRoute, Router } from '@angular/router';
-// import { SidebarState } from './sidebar/sidebar.component';
-import { selectSidebarState } from 'app/store/sidebar-state.selectors';
+import { CommitteeAccount } from 'app/shared/models/committee-account.model';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { SidebarState } from './sidebar/sidebar.component';
+import { selectSidebarState, selectSidebarVisible } from 'app/store/sidebar-state.selectors';
 import { DestroyerComponent } from 'app/shared/components/app-destroyer.component';
 
 @Component({
@@ -15,45 +15,32 @@ import { DestroyerComponent } from 'app/shared/components/app-destroyer.componen
   styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent extends DestroyerComponent implements OnInit {
-  // committeeAccount$: Observable<CommitteeAccount> | undefined;
+  committeeAccount$: Observable<CommitteeAccount> | undefined;
   progressBarVisible$: Observable<{ spinnerOn: boolean }> | undefined;
-  // sidebarState$?: Observable<SidebarState | undefined>;
-  showSidebar = false;
-  // isReports = false;
-  // private window = window;
+  sidebarState$?: Observable<SidebarState | undefined>;
+  showSidebar = true;
+  isReports = false;
+  private window = window;
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute, private store: Store) {
+  constructor(private router: Router, private route: ActivatedRoute, private store: Store) {
     super();
 
-    // this.isReports =
-    //   this.window.location.href.includes('reports') &&
-    //   this.window.location.href.endsWith('reports') &&
-    //   this.window.location.href.endsWith('f3x/create/step1');
-    // store.select(selectSidebarVisible).subscribe((res) => {
-    //   this.showSidebar = res;
-    // });
-    // router.events.pipe(filter((e) => e instanceof NavigationEnd)).subscribe((e) => {
-    //   this.isReports =
-    //     (e as NavigationEnd).url.includes('reports') &&
-    //     !(e as NavigationEnd).url.endsWith('reports') &&
-    //     !(e as NavigationEnd).url.endsWith('f3x/create/step1');
-    // });
+    this.isReports = this.window.location.href.includes('reports');
+    store.select(selectSidebarVisible).subscribe((res) => {
+      this.showSidebar = res;
+    });
+    router.events.pipe(filter((e) => e instanceof NavigationEnd)).subscribe((e) => {
+      this.isReports = (e as NavigationEnd).url.includes('reports') && !(e as NavigationEnd).url.endsWith('reports');
+    });
   }
 
   ngOnInit(): void {
+    this.committeeAccount$ = this.store.select(selectCommitteeAccount);
     this.progressBarVisible$ = this.store.select(selectSpinnerStatus);
-
-    this.store
-      .select(selectSidebarState)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((state) => {
-        // Show sidebar if there is an associated "sidebar state" declared in a routing module.
-        debugger;
-        this.showSidebar = !!state;
-      });
+    this.sidebarState$ = this.store.select(selectSidebarState);
   }
 
-  // get isVisible(): boolean {
-  //   return this.showSidebar && this.isReports;
-  // }
+  get isVisible(): boolean {
+    return this.showSidebar && this.isReports;
+  }
 }
