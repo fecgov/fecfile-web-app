389388f0b796dc86cc75c4a780748e667ffab93e
Matt Travers <matt.travers@tcg.com>
2023-12-18 15:48:11 -0500

    Fix f99 unit tests

--- front-end/src/app/layout/sidebar/menus/f99/f99-menu.component.spec.ts
+++ front-end/src/app/layout/sidebar/menus/f99/f99-menu.component.spec.ts
@@ -1,29 +1,33 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { provideMockStore } from '@ngrx/store/testing';
-import { testMockStore } from 'app/shared/utils/unit-test.utils';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SharedModule } from '../../../../shared/shared.module';
 import { PanelMenuModule } from 'primeng/panelmenu';
 import { F99MenuComponent } from './f99-menu.component';
 import { ReportSidebarState, SidebarState } from '../../sidebar.component';
-import { combineLatest, filter } from 'rxjs';
 import { Store } from '@ngrx/store';
-import { setSidebarStateAction } from 'app/store/sidebar-state.actions';
+import { initialState as initSidebarState } from 'app/store/sidebar-state.reducer';
 import { selectSidebarState } from 'app/store/sidebar-state.selectors';
 
 describe('F99MenuComponent', () => {
   let component: F99MenuComponent;
   let fixture: ComponentFixture<F99MenuComponent>;
   let router: Router;
-  let store: Store;
+
+  const mockStore = {
+    initialState: {
+      fecfile_online_sidebarState: initSidebarState,
+    },
+    selectors: [{ selector: selectSidebarState, value: new SidebarState(ReportSidebarState.REVIEW, '/url') }],
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [F99MenuComponent],
-      providers: [provideMockStore(testMockStore)],
+      providers: [provideMockStore(mockStore)],
       imports: [
         SharedModule,
         PanelMenuModule,
@@ -43,7 +47,6 @@ describe('F99MenuComponent', () => {
     router = TestBed.inject(Router);
     fixture = TestBed.createComponent(F99MenuComponent);
     component = fixture.componentInstance;
-    store = TestBed.inject(Store);
     fixture.detectChanges();
   });
 
@@ -51,37 +54,16 @@ describe('F99MenuComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should set the sidebar state to TRANSACTIONS', waitForAsync(() => {
-    store.dispatch(setSidebarStateAction({ payload: new SidebarState(ReportSidebarState.TRANSACTIONS, '/url') }));
-    const waitUntilSidebarState = store.select(selectSidebarState).pipe(filter((state) => !!state));
-    combineLatest([component.items$, waitUntilSidebarState]).subscribe(([items, _]) => {
-      expect(_.section).toBe(ReportSidebarState.TRANSACTIONS);
-      expect(items[0].visible).toBeTrue();
+  it('should set the sidebar state to REVIEW A REPORT', () => {
+    component.items$.subscribe((items) => {
+      expect(items[1].expanded).toBeTrue();
     });
-  }));
+  });
 
-  it('should set the sidebar state to REVIEW', waitForAsync(() => {
-    store.dispatch(setSidebarStateAction({ payload: new SidebarState(ReportSidebarState.REVIEW, '/url') }));
-    const waitUntilSidebarState = store.select(selectSidebarState).pipe(filter((state) => !!state));
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    combineLatest([component.items$, waitUntilSidebarState]).subscribe(([items, _]) => {
-      expect(items[1].visible).toBeTrue();
-    });
-  }));
-
-  it('should set the sidebar state to SUBMISSION', waitForAsync(() => {
-    store.dispatch(setSidebarStateAction({ payload: new SidebarState(ReportSidebarState.REVIEW, '/url') }));
-    const waitUntilSidebarState = store.select(selectSidebarState).pipe(filter((state) => !!state));
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    combineLatest([component.items$, waitUntilSidebarState]).subscribe(([items, _]) => {
-      expect(items[2].visible).toBeTrue();
-    });
-  }));
-
-  it('should get report from url', waitForAsync(() => {
+  it('should get report from url', () => {
     router.navigateByUrl('/reports/transactions/report/999/list');
     component.activeReport$?.subscribe((report) => {
       expect(report?.id).toBe('999');
     });
-  }));
+  });
 });
