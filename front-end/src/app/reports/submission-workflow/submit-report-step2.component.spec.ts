3dc09b686107b18b436a308e9c2f549351d62009
toddlees <todd.lees@tcg.com>
2023-12-22 12:50:19 -0500

    lint

--- front-end/src/app/reports/submission-workflow/submit-report-step2.component.spec.ts
+++ front-end/src/app/reports/submission-workflow/submit-report-step2.component.spec.ts
@@ -1,5 +1,5 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
