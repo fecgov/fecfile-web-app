1cf4fbc8fe6875697efb0dbb06670967167ceb53
toddlees <todd.lees@tcg.com>
2023-12-22 10:11:06 -0500

    style and test foundation

---
+++ front-end/src/app/reports/submission-workflow/submit-report-step2.component.spec.ts
@@ -0,0 +1,78 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { Form3X } from 'app/shared/models/form-3x.model';
+import { testMockStore } from 'app/shared/utils/unit-test.utils';
+import { ConfirmationService, MessageService, SharedModule } from 'primeng/api';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DividerModule } from 'primeng/divider';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { of } from 'rxjs';
+import { SubmitReportStep2Component } from './submit-report-step2.component';
+import { ReportService } from 'app/shared/services/report.service';
+import { ApiService } from 'app/shared/services/api.service';
+import { ReportsModule } from '../reports.module';
+
+describe('SubmitReportStep2Component', () => {
+  let component: SubmitReportStep2Component;
+  let fixture: ComponentFixture<SubmitReportStep2Component>;
+  let router: Router;
+  let reportService: ReportService;
+  let apiService: ApiService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule.withRoutes([]),
+        HttpClientTestingModule,
+        DividerModule,
+        CheckboxModule,
+        RadioButtonModule,
+        SharedModule,
+        ReportsModule,
+      ],
+      declarations: [SubmitReportStep2Component],
+      providers: [
+        FormBuilder,
+        ReportService,
+        MessageService,
+        ConfirmationService,
+        ReportService,
+        ApiService,
+        provideMockStore(testMockStore),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                report: Form3X.fromJSON({
+                  report_code: 'Q1',
+                  id: '999',
+                }),
+              },
+            },
+          },
+        },
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    router = TestBed.inject(Router);
+    reportService = TestBed.inject(ReportService);
+    fixture = TestBed.createComponent(SubmitReportStep2Component);
+    apiService = TestBed.inject(ApiService);
+    component = fixture.componentInstance;
+    spyOn(reportService, 'get').and.returnValue(of(Form3X.fromJSON({ id: '999' })));
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
