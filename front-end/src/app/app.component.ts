9c25bd6ab8903bf34bf73d7b66a9b126830f9fab
Sasha Dresden <sasha.dresden@tcg.com>
2024-01-05 16:50:05 -0500

    Fix issue with login page getting header information it shouldn't have yet and make sidebar stretch to bottom

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { takeUntil } from "rxjs";
-import { Store } from "@ngrx/store";
-import { ActivatedRoute } from "@angular/router";
-import { selectSidebarState } from "./store/sidebar-state.selectors";
+import { Component } from '@angular/core';
 import { DestroyerComponent } from "./shared/components/app-destroyer.component";
 
 @Component({
@@ -10,21 +6,7 @@ import { DestroyerComponent } from "./shared/components/app-destroyer.component"
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent extends DestroyerComponent implements OnInit {
-  showSidebar = false;
+export class AppComponent extends DestroyerComponent {
 
-  constructor(private store: Store, private activatedRoute: ActivatedRoute) {
-    super();
-  }
-
-  ngOnInit(): void {
-    this.store
-      .select(selectSidebarState)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((state) => {
-        // Show sidebar if there is an associated "sidebar state" declared in a routing module.
-        this.showSidebar = !!state;
-      });
-  }
 }
 
--- front-end/src/app/layout/layout.component.ts
+++ front-end/src/app/layout/layout.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { DestroyerComponent } from 'app/shared/components/app-destroyer.component';
+import { Store } from "@ngrx/store";
+import { ActivatedRoute } from "@angular/router";
+import { selectSidebarState } from "../store/sidebar-state.selectors";
+import { takeUntil } from "rxjs";
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent extends DestroyerComponent {
+export class LayoutComponent extends DestroyerComponent implements OnInit {
   showSidebar = false;
 
-  constructor() {
+  constructor(private store: Store, private activatedRoute: ActivatedRoute) {
     super();
+  }
+
+  ngOnInit(): void {
+    this.store
+      .select(selectSidebarState)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((state) => {
+        // Show sidebar if there is an associated "sidebar state" declared in a routing module.
+        this.showSidebar = !!state;
+      });
   }
 
 }
