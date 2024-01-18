b54fbe15f7e02c33aec51277f4cdf799908ea4a3
Sasha Dresden <sasha.dresden@tcg.com>
2024-01-18 16:08:37 -0500

    Add tests for SelectReportDialog

--- front-end/src/app/reports/transactions/transaction-list/select-report-dialog/select-report-dialog.component.spec.ts
+++ front-end/src/app/reports/transactions/transaction-list/select-report-dialog/select-report-dialog.component.spec.ts
@@ -6,6 +6,9 @@ import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { provideMockStore } from "@ngrx/store/testing";
 import { testMockStore } from "../../../../shared/utils/unit-test.utils";
+import { of } from "rxjs";
+import { F3xFormTypes, Form3X } from "../../../../shared/models/form-3x.model";
+import { ListRestResponse } from "../../../../shared/models/rest-api.model";
 
 describe('SelectReportDialogComponent', () => {
   let component: SelectReportDialogComponent;
@@ -27,4 +30,25 @@ describe('SelectReportDialogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get a list of available reports', async () => {
+    spyOn(reportService, 'getTableData').and.callFake(() => {
+      const data = {
+        id: '999',
+        form_type: F3xFormTypes.F3XT,
+        committee_name: 'foo',
+      };
+      const response: ListRestResponse = {
+        next: "",
+        pageNumber: 0,
+        previous: "",
+        results: [Form3X.fromJSON(data)],
+        count: 1
+      };
+      return of(response);
+    })
+    spyOn(reportService, 'isEditable').and.callThrough();
+    await component.ngOnInit();
+    expect(component.availableReports.length).toBe(1);
+  })
 });
