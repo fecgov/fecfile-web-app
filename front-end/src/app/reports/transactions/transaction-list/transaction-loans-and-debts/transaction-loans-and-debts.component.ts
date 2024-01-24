d472b1ec177eafe7ff75375880ba37840ff81948
David Heitzer <david.heitzer@tcg.com>
2024-01-24 10:15:23 -0500

    1230 update field name

--- front-end/src/app/reports/transactions/transaction-list/transaction-loans-and-debts/transaction-loans-and-debts.component.ts
+++ front-end/src/app/reports/transactions/transaction-list/transaction-loans-and-debts/transaction-loans-and-debts.component.ts
@@ -29,7 +29,7 @@ export class TransactionLoansAndDebtsComponent extends TransactionListTableBaseC
     {field: "transaction_type_identifier", label: "Type"},
     {field: "name", label: "Name"},
     {field: "date", label: "Date incurred"},
-    {field: "transaction_table_amount", label: "Amount"},
+    {field: "loans_debts_transaction_table_amount", label: "Amount"},
     {field: "balance", label: "Balance"}
   ];
 
