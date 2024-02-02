6f28d8bdc01797586464037c892e343e65f885ca
Elaine Krauss <elaine.krauss@tcg.com>
2024-02-02 16:21:34 -0500

    Cleans up unit tests

--- front-end/src/app/shared/services/login.service.spec.ts
+++ front-end/src/app/shared/services/login.service.spec.ts
@@ -102,32 +102,32 @@ describe('LoginService', () => {
 
     it('current date is valid', () => {
       const testDate = DateUtils.convertDateToFecFormat(new Date()) as string;
-      ((service.userLoginData as UserLoginData).security_consent_date = testDate),
-        expect(service.userHasRecentSecurityConsentDate()).toBeTrue();
+      (service.userLoginData as UserLoginData).security_consent_date = testDate;
+      expect(service.userHasRecentSecurityConsentDate()).toBeTrue();
     });
 
     it('recent date is valid', () => {
       const recentDate = new Date();
       recentDate.setMonth(recentDate.getMonth() - 6);
       const testDate = DateUtils.convertDateToFecFormat(recentDate) as string;
-      ((service.userLoginData as UserLoginData).security_consent_date = testDate),
-        expect(service.userHasRecentSecurityConsentDate()).toBeTrue();
+      (service.userLoginData as UserLoginData).security_consent_date = testDate;
+      expect(service.userHasRecentSecurityConsentDate()).toBeTrue();
     });
 
     it('364 days ago is valid', () => {
       const recentDate = new Date();
       recentDate.setDate(recentDate.getDate() - 364);
       const testDate = DateUtils.convertDateToFecFormat(recentDate) as string;
-      ((service.userLoginData as UserLoginData).security_consent_date = testDate),
-        expect(service.userHasRecentSecurityConsentDate()).toBeTrue();
+      (service.userLoginData as UserLoginData).security_consent_date = testDate;
+      expect(service.userHasRecentSecurityConsentDate()).toBeTrue();
     });
 
     it('one year ago is invalid', () => {
       const recentDate = new Date();
       recentDate.setFullYear(recentDate.getFullYear() - 1);
       const testDate = DateUtils.convertDateToFecFormat(recentDate) as string;
-      ((service.userLoginData as UserLoginData).security_consent_date = testDate),
-        expect(service.userHasRecentSecurityConsentDate()).toBeFalse();
+      (service.userLoginData as UserLoginData).security_consent_date = testDate;
+      expect(service.userHasRecentSecurityConsentDate()).toBeFalse();
     });
   });
 
