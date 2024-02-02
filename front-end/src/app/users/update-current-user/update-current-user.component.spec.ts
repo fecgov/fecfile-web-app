3b1fac91ecb58f868870d7fe229ab4c3f08f9d94
David Heitzer <david.heitzer@tcg.com>
2024-02-01 23:32:26 -0500

    92 add unit tests

--- front-end/src/app/users/update-current-user/update-current-user.component.spec.ts
+++ front-end/src/app/users/update-current-user/update-current-user.component.spec.ts
@@ -1,7 +1,10 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { provideMockStore } from '@ngrx/store/testing';
+import { UsersService } from 'app/shared/services/users.service';
 import { testMockStore } from 'app/shared/utils/unit-test.utils';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
@@ -15,6 +18,8 @@ import { UpdateCurrentUserComponent } from './update-current-user.component';
 describe('UpdateCurrentUserComponent', () => {
   let component: UpdateCurrentUserComponent;
   let fixture: ComponentFixture<UpdateCurrentUserComponent>;
+  let router: Router;
+  let store: Store;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -28,17 +33,31 @@ describe('UpdateCurrentUserComponent', () => {
         ConfirmDialogModule,
       ],
       declarations: [UpdateCurrentUserComponent],
-      providers: [ConfirmationService, MessageService, FormBuilder, provideMockStore(testMockStore)],
+      providers: [ConfirmationService, MessageService,
+        FormBuilder, provideMockStore(testMockStore), UsersService],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UpdateCurrentUserComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
+    store = TestBed.inject(Store);
     fixture.detectChanges();
+    component.ngOnInit();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create', fakeAsync(() => {
+    component.form.get('last_name')?.setValue('testLastName');
+    component.form.get('first_name')?.setValue('testFirstName');
+    component.form.get('email')?.setValue('testEmail');
+
+    component.continue();
+    expect(component.form.valid).toBeTrue();
+    expect(component.formSubmitted).toBeTrue();
+  }));
 });
