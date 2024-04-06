b4735f50b9a2e62f62561fd6076a7cfe85ef6edc
David Heitzer <david.heitzer@tcg.com>
2024-04-06 11:10:09 -0400

    1755 fix lint

--- front-end/src/app/layout/feedback-overlay/feedback-overlay.component.spec.ts
+++ front-end/src/app/layout/feedback-overlay/feedback-overlay.component.spec.ts
@@ -4,7 +4,6 @@ import { provideMockStore } from '@ngrx/store/testing';
 import { testMockStore } from 'app/shared/utils/unit-test.utils';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { DialogModule } from 'primeng/dialog';
 import { OverlayPanelModule } from 'primeng/overlaypanel';
 import { TableModule } from 'primeng/table';
 import { ToastModule } from 'primeng/toast';
