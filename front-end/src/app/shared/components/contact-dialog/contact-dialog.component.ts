1b75ff258c2606b033faf4bdf98f2f26d6e7522c
Elaine Krauss <elaine.krauss@tcg.com>
2024-04-12 12:15:20 -0400

    When reseting the contact dialog, ensure that the country and state are set appropriately

--- front-end/src/app/shared/components/contact-dialog/contact-dialog.component.ts
+++ front-end/src/app/shared/components/contact-dialog/contact-dialog.component.ts
@@ -278,6 +278,8 @@ export class ContactDialogComponent extends DestroyerComponent implements OnInit
 
   private resetForm() {
     this.form.reset();
+    this.form.get('country')?.setValue(this.countryOptions[0]['value']);
+    this.form.get('state')?.setValue(this.stateOptions[0]['value']);
     this.isNewItem = true;
     this.contactLookup.contactTypeFormControl.enable();
     this.contactLookup.contactTypeFormControl.setValue(this.contactType);
