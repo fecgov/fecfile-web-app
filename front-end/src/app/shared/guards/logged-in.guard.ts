58475e67189f3c68e473559a460f18ad24940855
toddlees <todd.lees@tcg.com>
2024-04-19 08:20:35 -0400

    lint

--- front-end/src/app/shared/guards/logged-in.guard.ts
+++ front-end/src/app/shared/guards/logged-in.guard.ts
@@ -1,10 +1,10 @@
 import { CanActivateFn } from '@angular/router';
-import { Router, UrlTree } from '@angular/router';
+import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { LoginService } from '../services/login.service';
 import { inject } from '@angular/core';
 
-export const loggedInGuard: CanActivateFn = (route, state) => {
+export const loggedInGuard: CanActivateFn = () => {
   const router = inject(Router);
   const loginService = inject(LoginService);
   const cookieService = inject(CookieService);
