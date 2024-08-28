fd55d255b2963f318817be0b4247d830fceddd59
Elaine Krauss <elaine.krauss@tcg.com>
2024-08-28 11:31:12 -0400

    WIP, submitting

--- front-end/src/app/committee/register-committee/register-committee.component.ts
+++ front-end/src/app/committee/register-committee/register-committee.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
 import { DestroyerComponent } from 'app/shared/components/app-destroyer.component';
 import { CommitteeAccount } from 'app/shared/models/committee-account.model';
 import { FecFiling } from 'app/shared/models/fec-filing.model';
 import { CommitteeAccountService } from 'app/shared/services/committee-account.service';
 import { FecApiService } from 'app/shared/services/fec-api.service';
-import { partialCommitteeIdValidator } from 'app/shared/utils/validators.utils';
+import { committeeIdValidator } from 'app/shared/utils/validators.utils';
 import { ConfirmationService, MessageService } from 'primeng/api';
 
 @Component({
@@ -19,9 +19,7 @@ export class RegisterCommitteeComponent extends DestroyerComponent {
   selectedCommittee?: CommitteeAccount;
   explanationVisible = false;
   unableToCreateAccount = false;
-  form: FormGroup = new FormGroup({
-    'committee-search': new FormControl('', partialCommitteeIdValidator),
-  });
+  form: FormGroup = new FormGroup({ 'committee-id': new FormControl('', committeeIdValidator) }, { updateOn: 'blur' });
 
   constructor(
     protected fecApiService: FecApiService,
@@ -64,6 +62,30 @@ export class RegisterCommitteeComponent extends DestroyerComponent {
         this.selectedCommittee = undefined;
       },
     );
+  }
+  registerMembership() {
+    const committeeIdField = this.form.get('committee-id') as AbstractControl;
+    committeeIdField.updateValueAndValidity();
+
+    const committeeId = committeeIdField.value;
+    this.unableToCreateAccount = false;
+
+    if (this.form.get('committee-id')?.valid) {
+      this.committeeAccountService.registerCommitteeAccount(committeeId).then(
+        (committeeAccount) => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Successful',
+            detail: `Committee Account ${committeeAccount.committee_id} Created`,
+            life: 3000,
+          });
+        },
+        () => {
+          this.unableToCreateAccount = true;
+          this.selectedCommittee = undefined;
+        },
+      );
+    }
   }
   showExplanation() {
     this.explanationVisible = true;
