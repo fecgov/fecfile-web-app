f0c3dd5292140beaebe47cb05393f3fbf179fe53
Elaine Krauss <elaine.krauss@tcg.com>
2024-10-14 20:03:16 -0400

    Missed a spot...

--- front-end/src/app/shared/services/fec-api.service.ts
+++ front-end/src/app/shared/services/fec-api.service.ts
@@ -1,11 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
-import { firstValueFrom, map, Observable } from 'rxjs';
-=======
 import { map, Observable } from 'rxjs';
-import { Candidate } from '../models/candidate.model';
->>>>>>> develop
 import { CommitteeAccount } from '../models/committee-account.model';
 import { FecApiPaginatedResponse } from '../models/fec-api.model';
 import { FecFiling } from '../models/fec-filing.model';
