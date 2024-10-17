5b8febf6c6a3895384e9ef98be6dab181b82084c
Sasha Dresden <sasha.dresden@proton.me>
2024-10-17 14:23:30 -0400

    Add Security Notification page

--- front-end/src/app/notifications/notifications-routing.module.ts
+++ front-end/src/app/notifications/notifications-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { loggedInGuard } from 'app/shared/guards/logged-in.guard';
+import { nameGuard } from 'app/shared/guards/name.guard';
+import { SecurityNotificationComponent } from './security-notification/security-notification.component';
 
-const routes: Routes = [{ path: '**', redirectTo: '' }];
+const routes: Routes = [
+  {
+    path: 'security',
+    component: SecurityNotificationComponent,
+    title: 'Security Notice',
+    canActivate: [loggedInGuard, nameGuard],
+    data: {
+      showCommitteeBanner: false,
+    },
+  },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
---
+++ front-end/src/app/notifications/security-notification/security-notification.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-security-notification',
+  templateUrl: './security-notification.component.html',
+  styleUrl: './security-notification.component.scss',
+})
+export class SecurityNotificationComponent {}
