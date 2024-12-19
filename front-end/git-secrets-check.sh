d1313ad9adc478b82570558525a341226c327ec9
Elaine Krauss <elaine.krauss@tcg.com>
2024-12-19 11:40:08 -0500

    Adds support for an  env variable

--- front-end/git-secrets-check.sh
+++ front-end/git-secrets-check.sh
@@ -15,12 +15,17 @@ break_installation() {
     exit 1
 }
 
+# If the environment variable is present, then exit with a success
+if [[ $IGNORE_GIT_SECRETS_CHECK ]]; then exit 0; fi
+
+# If the git secrets command doesn't result in a man page, it isn't installed
 if ! git secrets | grep -q "usage"
 then
     echo "Git secrets is not installed";
     break_installation;
 fi
 
+# If the --list flag doesn't include any patterns, our pattern install script hasn't been run
 if ! git secrets --list | grep -q "patterns"
 then
     echo "Git secrets has no installed patterns.  Have you run the API repo's install script?";
