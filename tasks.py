dcffbdfdd203111901face6dc3a38efa8424402e
Elaine Krauss <elaine.krauss@tcg.com>
2025-02-19 14:48:35 -0500

    Reverts deploy logic

--- tasks.py
+++ tasks.py
@@ -47,7 +47,7 @@ def _detect_branch(repo):
 DEPLOY_RULES = (
     ("test", lambda _, branch: branch == "main"),
     ("stage", lambda _, branch: branch.startswith("release")),
-    ("dev", lambda _, branch: branch == "feature/2004-acao-header"),
+    ("dev", lambda _, branch: branch == "develop"),
 )
 
 
