ff3ceb4ebc400c7003c50364550e973585858de1
toddlees <todd.lees@tcg.com>
2025-02-20 00:57:24 -0500

    unused app.config

--- front-end/src/app/app.config.ts
+++
@@ -1,7 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideAnimationsAsync(), providePrimeNG({})],
-};
