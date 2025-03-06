11f1ef92dcdaa871fbc283b7951e45f4362b1ed1
toddlees <todd.lees@tcg.com>
2025-03-06 18:35:04 -0500

    emit initial date value on existing transction load

--- front-end/src/app/shared/components/transaction-type-base/transaction-form.utils.ts
+++ front-end/src/app/shared/components/transaction-type-base/transaction-form.utils.ts
@@ -381,6 +381,7 @@ export class TransactionFormUtils {
         },
         component.destroy$,
         [
+          startWith<Date>(form.get(templateMap.date)?.value),
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
           combineLatestWith(form.get(templateMap.amount)?.valueChanges as Observable<any>, contactId$),
           startWith<[Date, number, string]>([
