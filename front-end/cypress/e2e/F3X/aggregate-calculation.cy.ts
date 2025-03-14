05843ad6c5466f3db7d5b47c39a18ae8449bc407
Elaine Krauss <elaine.krauss@tcg.com>
2025-03-14 17:11:36 -0400

    Adds another E2E test for leapfrogging while changing contacts

--- front-end/cypress/e2e/F3X/aggregate-calculation.cy.ts
+++ front-end/cypress/e2e/F3X/aggregate-calculation.cy.ts
@@ -12,7 +12,7 @@ describe('Tests transaction form aggregate calculation', () => {
     Initialize();
   });
 
-  xit('new transaction aggregate', () => {
+  it('new transaction aggregate', () => {
     F3XSetup();
 
     // Create the first Individual Receipt
@@ -100,9 +100,149 @@ describe('Tests transaction form aggregate calculation', () => {
     cy.get('h1').click(); // clicking outside of fields to ensure that the amount field loses focus and updates
 
     cy.get('[id=aggregate]').should('have.value', '$240.01');
+    PageUtils.clickButton('Save');
+
+    cy.contains('Transactions in this report').should('exist');
+    cy.get('.p-datatable-tbody > :nth-child(1) > :nth-child(7)').should('contain', '$200.01');
+    cy.get('.p-datatable-tbody > :nth-child(2) > :nth-child(7)').should('contain', '$240.01');
   });
 
-  // Disabled pending the fix that should come with #2087
+  it('existing transaction change contact', () => {
+    F3XSetup();
+
+    // Create the first Individual Receipt
+    StartTransaction.Receipts().Individual().IndividualReceipt();
+
+    PageUtils.clickLink('Create a new contact');
+    const individualFormContactData = {
+      ...defaultContactFormData,
+      ...{ contact_type: 'Individual', last_name: 'A', first_name: 'A' },
+    };
+    ContactListPage.enterFormData(individualFormContactData, true);
+    PageUtils.clickButton('Save & continue');
+
+    const transactionOneData = {
+      ...formTransactionDataForSchedule,
+      ...{
+        purpose_description: '',
+        category_code: '',
+        date_received: new Date(currentYear, 3, 27),
+      },
+    };
+    TransactionDetailPage.enterScheduleFormData(transactionOneData, false, '', true, 'contribution_date');
+    PageUtils.clickButton('Save');
+    cy.contains('Confirm').should('exist');
+    PageUtils.clickButton('Continue', '', true);
+
+    cy.contains('Transactions in this report').should('exist');
+
+    // Create the second Individual Receipt
+    StartTransaction.Receipts().Individual().IndividualReceipt();
+    PageUtils.clickLink('Create a new contact');
+    const secondIndividualFormContactData = {
+      ...defaultContactFormData,
+      ...{ contact_type: 'Individual', last_name: 'Z', first_name: 'Z' },
+    };
+    ContactListPage.enterFormData(secondIndividualFormContactData, true);
+    PageUtils.clickButton('Save & continue');
+
+    const transactionTwoData = {
+      ...formTransactionDataForSchedule,
+      ...{
+        purpose_description: '',
+        category_code: '',
+        date_received: new Date(currentYear, 3, 28),
+        amount: 25,
+      },
+    };
+    TransactionDetailPage.enterScheduleFormData(transactionTwoData, false, '', true, 'contribution_date');
+    PageUtils.clickButton('Save');
+    cy.contains('Confirm').should('exist');
+    PageUtils.clickButton('Continue', '', true);
+
+    cy.contains('Transactions in this report').should('exist');
+    cy.get('.p-datatable-tbody > :nth-child(2) > :nth-child(2) > a').click();
+
+    // Tests changing the second transaction's contact
+    cy.get('[id=aggregate]').should('have.value', '$25.00');
+    cy.get('[id="searchBox"]').type('A');
+    cy.contains('A, A').should('exist');
+    cy.contains('A, A').click({ force: true });
+    cy.get('h1').click(); // clicking outside of fields to ensure that the amount field loses focus and updates
+
+    cy.get('[id=aggregate]').should('have.value', '$225.01');
+    PageUtils.clickButton('Save');
+
+    cy.contains('Transactions in this report').should('exist');
+    cy.get('.p-datatable-tbody > :nth-child(1) > :nth-child(7)').should('contain', '$200.01');
+    cy.get('.p-datatable-tbody > :nth-child(2) > :nth-child(7)').should('contain', '$225.01');
+  });
+
+  it('existing transaction change amount', () => {
+    F3XSetup();
+
+    // Create the first Individual Receipt
+    StartTransaction.Receipts().Individual().IndividualReceipt();
+
+    PageUtils.clickLink('Create a new contact');
+    const individualFormContactData = {
+      ...defaultContactFormData,
+      ...{ contact_type: 'Individual' },
+    };
+    ContactListPage.enterFormData(individualFormContactData, true);
+    PageUtils.clickButton('Save & continue');
+
+    const transactionOneData = {
+      ...formTransactionDataForSchedule,
+      ...{
+        purpose_description: '',
+        category_code: '',
+        date_received: new Date(currentYear, 3, 27),
+      },
+    };
+    TransactionDetailPage.enterScheduleFormData(transactionOneData, false, '', true, 'contribution_date');
+    PageUtils.clickButton('Save');
+    cy.contains('Confirm').should('exist');
+    PageUtils.clickButton('Continue', '', true);
+
+    cy.contains('Transactions in this report').should('exist');
+
+    // Create the second Individual Receipt
+    StartTransaction.Receipts().Individual().IndividualReceipt();
+    cy.get('[id="searchBox"]').type(defaultContactFormData['last_name'].slice(0, 1));
+    cy.contains(defaultContactFormData['last_name']).should('exist');
+    cy.contains(defaultContactFormData['last_name']).click({ force: true });
+
+    const transactionTwoData = {
+      ...formTransactionDataForSchedule,
+      ...{
+        purpose_description: '',
+        category_code: '',
+        date_received: new Date(currentYear, 3, 28),
+        amount: 25,
+      },
+    };
+    TransactionDetailPage.enterScheduleFormData(transactionTwoData, false, '', true, 'contribution_date');
+    PageUtils.clickButton('Save');
+    cy.contains('Confirm').should('exist');
+    PageUtils.clickButton('Continue', '', true);
+
+    cy.contains('Transactions in this report').should('exist');
+    cy.get('.p-datatable-tbody > :nth-child(2) > :nth-child(2) > a').click();
+
+    // Tests changing the amount
+    cy.get('[id=aggregate]').should('have.value', '$225.01');
+    cy.get('[id="amount"]').clear().safeType('40');
+    cy.get('h1').click(); // clicking outside of fields to ensure that the amount field loses focus and updates
+
+    cy.get('[id=aggregate]').should('have.value', '$240.01');
+    PageUtils.clickButton('Save');
+
+    cy.contains('Transactions in this report').should('exist');
+    cy.get('.p-datatable-tbody > :nth-child(1) > :nth-child(7)').should('contain', '$200.01');
+    cy.get('.p-datatable-tbody > :nth-child(2) > :nth-child(7)').should('contain', '$240.01');
+  });
+
   it('existing transaction date leapfrogging', () => {
     F3XSetup();
 
@@ -167,73 +307,7 @@ describe('Tests transaction form aggregate calculation', () => {
     cy.get('.p-datatable-tbody > :nth-child(2) > :nth-child(7)').should('contain', '$25.00');
   });
 
-  xit('existing transaction change contact', () => {
-    F3XSetup();
-
-    // Create the first Individual Receipt
-    StartTransaction.Receipts().Individual().IndividualReceipt();
-
-    PageUtils.clickLink('Create a new contact');
-    const individualFormContactData = {
-      ...defaultContactFormData,
-      ...{ contact_type: 'Individual', last_name: 'A', first_name: 'A' },
-    };
-    ContactListPage.enterFormData(individualFormContactData, true);
-    PageUtils.clickButton('Save & continue');
-
-    const transactionOneData = {
-      ...formTransactionDataForSchedule,
-      ...{
-        purpose_description: '',
-        category_code: '',
-        date_received: new Date(currentYear, 3, 27),
-      },
-    };
-    TransactionDetailPage.enterScheduleFormData(transactionOneData, false, '', true, 'contribution_date');
-    PageUtils.clickButton('Save');
-    cy.contains('Confirm').should('exist');
-    PageUtils.clickButton('Continue', '', true);
-
-    cy.contains('Transactions in this report').should('exist');
-
-    // Create the second Individual Receipt
-    StartTransaction.Receipts().Individual().IndividualReceipt();
-    PageUtils.clickLink('Create a new contact');
-    const secondIndividualFormContactData = {
-      ...defaultContactFormData,
-      ...{ contact_type: 'Individual', last_name: 'Z', first_name: 'Z' },
-    };
-    ContactListPage.enterFormData(secondIndividualFormContactData, true);
-    PageUtils.clickButton('Save & continue');
-
-    const transactionTwoData = {
-      ...formTransactionDataForSchedule,
-      ...{
-        purpose_description: '',
-        category_code: '',
-        date_received: new Date(currentYear, 3, 28),
-        amount: 25,
-      },
-    };
-    TransactionDetailPage.enterScheduleFormData(transactionTwoData, false, '', true, 'contribution_date');
-    PageUtils.clickButton('Save');
-    cy.contains('Confirm').should('exist');
-    PageUtils.clickButton('Continue', '', true);
-
-    cy.contains('Transactions in this report').should('exist');
-    cy.get('.p-datatable-tbody > :nth-child(2) > :nth-child(2) > a').click();
-
-    // Tests changing the second transaction's contact
-    cy.get('[id=aggregate]').should('have.value', '$25.00');
-    cy.get('[id="searchBox"]').type('A');
-    cy.contains('A, A').should('exist');
-    cy.contains('A, A').click({ force: true });
-    cy.get('h1').click(); // clicking outside of fields to ensure that the amount field loses focus and updates
-
-    cy.get('[id=aggregate]').should('have.value', '$225.01');
-  });
-
-  xit('existing transaction change amount', () => {
+  it('leapfrog and contact change', () => {
     F3XSetup();
 
     // Create the first Individual Receipt
@@ -282,14 +356,49 @@ describe('Tests transaction form aggregate calculation', () => {
     cy.contains('Confirm').should('exist');
     PageUtils.clickButton('Continue', '', true);
 
-    cy.contains('Transactions in this report').should('exist');
-    cy.get('.p-datatable-tbody > :nth-child(2) > :nth-child(2) > a').click();
+    // Create a third Individual Receipt
+    StartTransaction.Receipts().Individual().IndividualReceipt();
+    cy.get('[id="searchBox"]').type(defaultContactFormData['last_name'].slice(0, 1));
+    cy.contains(defaultContactFormData['last_name']).should('exist');
+    cy.contains(defaultContactFormData['last_name']).click({ force: true });
 
-    // Tests changing the amount
-    cy.get('[id=aggregate]').should('have.value', '$225.01');
-    cy.get('[id="amount"]').clear().safeType('40');
+    const transactionThreeData = {
+      ...formTransactionDataForSchedule,
+      ...{
+        purpose_description: '',
+        category_code: '',
+        date_received: new Date(currentYear, 3, 30),
+        amount: 40,
+      },
+    };
+    TransactionDetailPage.enterScheduleFormData(transactionThreeData, false, '', true, 'contribution_date');
+    PageUtils.clickButton('Save');
+    cy.contains('Confirm').should('exist');
+    PageUtils.clickButton('Continue', '', true);
+
+    cy.contains('Transactions in this report').should('exist');
+    cy.get('.p-datatable-tbody > :nth-child(1) > :nth-child(2) > a').click();
+    PageUtils.clickLink('Create a new contact');
+
+    const secondIndividualFormContactData = {
+      ...defaultContactFormData,
+      ...{ contact_type: 'Individual', last_name: 'Z', first_name: 'Z' },
+    };
+    ContactListPage.enterFormData(secondIndividualFormContactData, true);
+    PageUtils.clickButton('Save & continue');
+
+    // Tests moving the first transaction's date to be later than the second
+    TransactionDetailPage.enterDate('[data-cy="contribution_date"]', new Date(currentYear, 3, 29), '');
     cy.get('h1').click(); // clicking outside of fields to ensure that the amount field loses focus and updates
 
-    cy.get('[id=aggregate]').should('have.value', '$240.01');
+    cy.get('[id=aggregate]').should('have.value', '$200.01');
+    PageUtils.clickButton('Save');
+    cy.contains('Confirm').should('exist');
+    PageUtils.clickButton('Continue', '', true);
+
+    cy.contains('Transactions in this report').should('exist');
+    cy.get('.p-datatable-tbody > :nth-child(1) > :nth-child(7)').should('contain', '$200.01');
+    cy.get('.p-datatable-tbody > :nth-child(2) > :nth-child(7)').should('contain', '$25.00');
+    cy.get('.p-datatable-tbody > :nth-child(3) > :nth-child(7)').should('contain', '$65.00');
   });
 });
