770568853245641fcff870f568413f3b7b672c4b
David Heitzer <david.heitzer@tcg.com>
2025-03-31 15:47:41 -0400

    2063 add COORDINATED_PARTY_EXPENDITURE_VOID

--- front-end/src/app/shared/models/transaction-types/COORDINATED_PARTY_EXPENDITURE_VOID.model.spec.ts
+++ front-end/src/app/shared/models/transaction-types/COORDINATED_PARTY_EXPENDITURE_VOID.model.spec.ts
@@ -1,12 +1,12 @@
 import { TransactionType } from 'app/shared/models/transaction-type.model';
 import { SchFTransaction, ScheduleFTransactionTypes } from '../schf-transaction.model';
-import { COORDINATED_PARTY_EXPENDITURE } from './COORDINATED_PARTY_EXPENDITURE.model';
+import { COORDINATED_PARTY_EXPENDITURE_VOID } from './COORDINATED_PARTY_EXPENDITURE_VOID.model';
 
-describe('COORDINATED_PARTY_EXPENDITURE', () => {
-  let transactionType: COORDINATED_PARTY_EXPENDITURE;
+describe('COORDINATED_PARTY_EXPENDITURE_VOID', () => {
+  let transactionType: COORDINATED_PARTY_EXPENDITURE_VOID;
 
   beforeEach(() => {
-    transactionType = new COORDINATED_PARTY_EXPENDITURE();
+    transactionType = new COORDINATED_PARTY_EXPENDITURE_VOID();
   });
 
   it('should create an instance', () => {
@@ -17,7 +17,7 @@ describe('COORDINATED_PARTY_EXPENDITURE', () => {
   it('#factory() should return a SchFTransaction', () => {
     const txn: SchFTransaction = transactionType.getNewTransaction();
     expect(txn.form_type).toBe('SF');
-    expect(txn.transaction_type_identifier).toBe(ScheduleFTransactionTypes.COORDINATED_PARTY_EXPENDITURE);
+    expect(txn.transaction_type_identifier).toBe(ScheduleFTransactionTypes.COORDINATED_PARTY_EXPENDITURE_VOID);
   });
 
   it('#generatePurposeDescription() should not be defined', () => {
