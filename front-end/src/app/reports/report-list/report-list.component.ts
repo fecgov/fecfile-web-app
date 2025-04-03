1351cc787230a767e02ac16c7a9085d0a0ecf461
toddlees <todd.lees@tcg.com>
2025-04-03 08:53:49 -0400

    simplify switch

--- front-end/src/app/reports/report-list/report-list.component.ts
+++ front-end/src/app/reports/report-list/report-list.component.ts
@@ -86,13 +86,11 @@ export class ReportListComponent extends TableListBaseComponent<Report> implemen
 
     switch (item.report_type) {
       case ReportTypes.F3:
-        return this.router.navigateByUrl(`/reports/transactions/report/${item.id}/list`);
       case ReportTypes.F3X:
+      case ReportTypes.F24:
         return this.router.navigateByUrl(`/reports/transactions/report/${item.id}/list`);
       case ReportTypes.F99:
         return this.router.navigateByUrl(`/reports/f99/edit/${item.id}`);
-      case ReportTypes.F24:
-        return this.router.navigateByUrl(`/reports/transactions/report/${item.id}/list`);
       case ReportTypes.F1M:
         return this.router.navigateByUrl(`/reports/f1m/edit/${item.id}`);
     }
