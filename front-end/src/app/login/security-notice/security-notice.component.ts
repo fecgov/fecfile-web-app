32cb41886bdb43ae665bc3117c92629d9cc869de
Sasha Dresden <sasha.dresden@tcg.com>
2025-04-14 17:57:01 -0400

    Update security notice to use signals

--- front-end/src/app/login/security-notice/security-notice.component.ts
+++ front-end/src/app/login/security-notice/security-notice.component.ts
@@ -1,9 +1,7 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Data, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { DestroyerComponent } from 'app/shared/components/app-destroyer.component';
-import { UserLoginData } from 'app/shared/models/user.model';
 import { LoginService } from 'app/shared/services/login.service';
 import { UsersService } from 'app/shared/services/users.service';
 import { blurActiveInput } from 'app/shared/utils/form.utils';
@@ -11,9 +9,9 @@ import { SubscriptionFormControl } from 'app/shared/utils/subscription-form-cont
 import { singleClickEnableAction } from 'app/store/single-click.actions';
 import { userLoginDataUpdatedAction } from 'app/store/user-login-data.actions';
 import { selectUserLoginData } from 'app/store/user-login-data.selectors';
-import { takeUntil } from 'rxjs';
 import { Checkbox } from 'primeng/checkbox';
 import { ButtonDirective } from 'primeng/button';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-security-notice',
@@ -21,15 +19,16 @@ import { ButtonDirective } from 'primeng/button';
   styleUrls: ['./security-notice.component.scss'],
   imports: [ReactiveFormsModule, Checkbox, ButtonDirective],
 })
-export class SecurityNoticeComponent extends DestroyerComponent implements OnInit {
+export class SecurityNoticeComponent {
   private readonly store = inject(Store);
   private readonly router = inject(Router);
   public readonly loginService = inject(LoginService);
   private readonly usersService = inject(UsersService);
   private readonly activatedRoute = inject(ActivatedRoute);
-  formSubmitted = false;
-  showForm = true;
-  userLoginData?: UserLoginData;
+
+  private readonly data = toSignal(this.activatedRoute.data, { initialValue: {} as Data });
+  readonly showForm = computed(() => !!this.data()['backgroundStyle']);
+  readonly userLoginData = this.store.selectSignal(selectUserLoginData);
 
   form = new FormGroup(
     {
@@ -38,33 +37,13 @@ export class SecurityNoticeComponent extends DestroyerComponent implements OnIni
     { updateOn: 'blur' },
   );
 
-  constructor() {
-    super();
-    this.activatedRoute.data.subscribe((d) => {
-      this.showForm = !!d['backgroundStyle'];
-    });
-  }
-
-  ngOnInit() {
-    this.formSubmitted = false;
-
-    this.store
-      .select(selectUserLoginData)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((userLoginData: UserLoginData) => {
-        this.formSubmitted = false;
-        this.userLoginData = userLoginData;
-      });
-  }
-
   async signConsentForm() {
-    this.formSubmitted = true;
     blurActiveInput(this.form);
     if (this.form.invalid || !this.userLoginData) {
       this.store.dispatch(singleClickEnableAction());
       return;
     }
-    const updatedUserLoginData = { ...this.userLoginData };
+    const updatedUserLoginData = { ...this.userLoginData() };
     if (this.form.get('security-consent-annual')?.value) {
       updatedUserLoginData.consent_for_one_year = true;
     }
