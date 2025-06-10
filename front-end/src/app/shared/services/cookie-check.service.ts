66babf60be383527da67883beccd8471a2f96307
Dan Fowlkes <dfowlkes@precision-solutions.com>
2025-06-10 16:59:18 -0400

    FECFILE-478: Probably overkill, but make sure the browser knows cookies are enabled too.

--- front-end/src/app/shared/services/cookie-check.service.ts
+++ front-end/src/app/shared/services/cookie-check.service.ts
@@ -14,7 +14,7 @@ export class CookieCheckService {
       document.cookie = cookietest + '1';
       const cookiesEnabled = document.cookie.indexOf(cookietest) !== -1;
       document.cookie = cookietest + '1; expires=Thu, 01-Jan-1970 00:00:01 GMT';
-      return cookiesEnabled;
+      return cookiesEnabled && window.navigator.cookieEnabled;
     } catch (e) {
       console.log(e);
       return false;
