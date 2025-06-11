5c1d297c52a5bd2ccce655b26f28495cc0179893
Dan Fowlkes <dfowlkes@precision-solutions.com>
2025-06-11 11:33:45 -0400

    FECFILE-478: Dev Notes say to check localstorage as well.

--- front-end/src/app/shared/services/cookie-check.service.ts
+++ front-end/src/app/shared/services/cookie-check.service.ts
@@ -5,11 +5,16 @@ import { Injectable } from '@angular/core';
 })
 export class CookieCheckService {
   areCookiesEnabled(): boolean {
+    const cookietest = 'cookietest=';
+    const localtest = 'localtest';
     try {
-      document.cookie = 'cookietest=1';
-      const cookiesEnabled = document.cookie.indexOf('cookietest=') !== -1;
-      document.cookie = 'cookietest=1; expires=Thu, 01-Jan-1970 00:00:01 GMT';
-      return cookiesEnabled;
+      localStorage.setItem(localtest, localtest);
+      localStorage.removeItem(localtest);
+
+      document.cookie = cookietest + '1';
+      const cookiesEnabled = document.cookie.indexOf(cookietest) !== -1;
+      document.cookie = cookietest + '1; expires=Thu, 01-Jan-1970 00:00:01 GMT';
+      return cookiesEnabled && window.navigator.cookieEnabled;
     } catch (e) {
       console.log(e);
       return false;
