bbf02ce2b4add573be9ce7f69698485d71f29a09
Elaine Krauss <elaine.krauss@tcg.com>
2025-06-18 17:57:36 -0400

    Removes a temporary console.log() call

--- front-end/src/app/shared/components/inputs/committee-input/committee-input.component.ts
+++ front-end/src/app/shared/components/inputs/committee-input/committee-input.component.ts
@@ -18,7 +18,6 @@ export class CommitteeInputComponent extends BaseInputComponent implements OnIni
   @Input() tertiaryContact = false;
 
   ngOnInit(): void {
-    console.log('INTI');
     if (this.transaction?.transactionType?.synchronizeOrgComNameValues) {
       this.form
         .get(this.templateMap.organization_name)
