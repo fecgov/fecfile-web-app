0f21efcb10054d132c0ea0cda3778cb28ce10f28
Sasha Dresden <sasha.dresden@proton.me>
2025-07-14 11:48:13 -0400

    Update redesignations to work with new F3XSetup

--- front-end/cypress/e2e/F3X/redesignations.cy.ts
+++ front-end/cypress/e2e/F3X/redesignations.cy.ts
@@ -1,15 +1,13 @@
 import { Initialize } from '../pages/loginPage';
 import { currentYear, PageUtils } from '../pages/pageUtils';
 import { TransactionDetailPage } from '../pages/transactionDetailPage';
-import { f3ReportId$, F3XSetup } from './f3x-setup';
+import { F3XSetup } from './f3x-setup';
 import {
   ContributionFormData,
   defaultScheduleFormData as defaultTransactionFormData,
 } from '../models/TransactionFormModel';
 import { Contributions } from './utils/start-transaction/disbursements';
 import { F3X_Q1, F3X_Q2 } from '../requests/library/reports';
-import { Candidate_House_A$, Committee_A$ } from '../requests/library/contacts';
-import { combineLatest, filter, first } from 'rxjs';
 import { buildContributionToCandidate } from '../requests/library/transactions';
 import { makeRequestToAPI } from '../requests/methods';
 
@@ -55,24 +53,17 @@ describe('Redesignations', () => {
       'http://localhost:8080/api/v1/reports/form-3x/?fields_to_validate=filing_frequency',
       F3X_Q2,
     );
-    F3XSetup({ committee: true, candidate: true, report: F3X_Q1 });
-    combineLatest([
-      Committee_A$.pipe(filter((value) => value !== undefined)),
-      Candidate_House_A$.pipe(filter((value) => value !== undefined)),
-      f3ReportId$.pipe(filter((value) => value !== '')),
-    ])
-      .pipe(first())
-      .subscribe(([committeeA, candidateA, reportId]) => {
-        const transaction = buildContributionToCandidate(
-          100.55,
-          `${currentYear}-03-27`,
-          [committeeA, candidateA],
-          reportId,
-          { election_code: 'P2020', support_oppose_code: 'S', date_signed: `${currentYear}-07-09` },
-        );
-        makeRequestToAPI('POST', 'http://localhost:8080/api/v1/transactions/', transaction);
-        cy.visit(`/reports/transactions/report/${reportId}/list`);
-        Redesignate();
-      });
+    cy.wrap(F3XSetup({ committee: true, candidate: true, report: F3X_Q1 })).then((result: any) => {
+      const transaction = buildContributionToCandidate(
+        100.55,
+        `${currentYear}-03-27`,
+        [result.committee, result.candidate],
+        result.report,
+        { election_code: 'P2020', support_oppose_code: 'S', date_signed: `${currentYear}-07-09` },
+      );
+      makeRequestToAPI('POST', 'http://localhost:8080/api/v1/transactions/', transaction);
+      cy.visit(`/reports/transactions/report/${result.report}/list`);
+      Redesignate();
+    });
   });
 });
