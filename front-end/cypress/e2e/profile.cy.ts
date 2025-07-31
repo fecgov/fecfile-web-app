1b40e534958b0c5274a74b4e0dd16d18e577167e
Sasha Dresden <sasha.dresden@tcg.com>
2025-07-31 09:43:38 -0400

    Added 2 e2e tests to test navbar reaching committee members page and profile account page.

--- front-end/cypress/e2e/profile.cy.ts
+++ front-end/cypress/e2e/profile.cy.ts
@@ -1,4 +1,5 @@
 import { LoginPage } from './pages/loginPage';
+import { PageUtils } from './pages/pageUtils';
 import { ProfileAccountPage } from './pages/profileAccountPage';
 import { ProfileUserListPage } from './pages/profileUserListPage';
 
@@ -15,5 +16,23 @@ describe('Manage profile', () => {
   it('Can view the Users table', () => {
     ProfileUserListPage.goToPage();
     cy.contains('Testson, Test').should('exist');
+  });
+
+  it('Can navigate to the Committee members via the navbar', () => {
+    const alias = PageUtils.getAlias('');
+    cy.visit('/reports');
+    cy.get('#navbarProfileDropdownMenuLink').click();
+    cy.get(alias).find('.p-popover').contains('Users').click();
+    cy.location('pathname').should('include', '/members');
+  });
+
+  it('Can navigate to the Profile Account page via the navbar', () => {
+    const alias = PageUtils.getAlias('');
+    cy.intercept('/profile').as('account');
+    cy.visit('/reports');
+
+    cy.get('#navbarProfileDropdownMenuLink').click();
+    cy.get(alias).find('.p-popover').contains('Account').click();
+    cy.location('pathname').should('include', '/committee');
   });
 });
