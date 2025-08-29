2df0008b57d4dbc32dc4fb586e4041e61fc1764c
Joshua Coombs <joshua.coombs@tcg.com>
2025-08-29 13:17:57 -0400

    Revert "linting fix"
    This reverts commit ce24e0d87c2df9222f33c9be08f070c2b2c27964.

--- front-end/cypress/support/component.ts
+++ front-end/cypress/support/component.ts
@@ -14,9 +14,9 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands';
+import './commands'
 
-import { mount } from 'cypress/angular';
+import { mount } from 'cypress/angular'
 
 // Augment the Cypress namespace to include type definitions for
 // your custom command.
@@ -25,13 +25,13 @@ import { mount } from 'cypress/angular';
 declare global {
   namespace Cypress {
     interface Chainable {
-      mount: typeof mount;
+      mount: typeof mount
     }
   }
 }
 
-Cypress.Commands.add('mount', mount);
+Cypress.Commands.add('mount', mount)
 
 // Example use:
 // cy.mount(MyComponent)
-import 'browserstack-cypress-cli/bin/testObservability/cypress';
+import 'browserstack-cypress-cli/bin/testObservability/cypress'
