b4aaac789815c9e025f63528d3accf9a246801f2
Sasha Dresden <sasha.dresden@tcg.com>
2025-10-31 12:12:33 -0400

    Update a few more packages

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -19,7 +19,6 @@ import { CustomRouteReuseStrategy } from './app/custom-route-reuse-strategy';
 import { LoginService } from './app/shared/services/login.service';
 import { SchedulerAction, asyncScheduler } from 'rxjs';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 import { StoreModule, MetaReducer, ActionReducer, Action } from '@ngrx/store';
@@ -153,7 +152,6 @@ bootstrapApplication(AppComponent, {
       const initializerFn = initializeAppFactory(inject(LoginService), inject(Router), inject(CookieCheckService));
       return initializerFn();
     }),
-    provideAnimations(),
     provideHttpClient(withInterceptorsFromDi()),
   ],
 }).catch((err) => console.log(err));
