b8c49f6c56e17d9fec933b449fa6456a8c146a02
David Heitzer <david.heitzer@tcg.com>
2025-11-17 12:40:40 -0500

    2590 make report titles clickable for edit feature

--- front-end/src/app/reports/report-list/shared-templates.component.ts
+++ front-end/src/app/reports/report-list/shared-templates.component.ts
@@ -1,12 +1,16 @@
-import { Component, TemplateRef, viewChild } from '@angular/core';
+import { Component, inject, TemplateRef, viewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { TableActionsButtonComponent } from 'app/shared/components/table-actions-button/table-actions-button.component';
-import { FecDatePipe } from 'app/shared/pipes/fec-date.pipe';
 import { TableBodyContext } from 'app/shared/components/table/table.component';
-import { Report } from 'app/shared/models';
+import { Report, ReportStatus } from 'app/shared/models';
+import { FecDatePipe } from 'app/shared/pipes/fec-date.pipe';
 
 @Component({
   selector: 'app-shared-templates',
   template: `
+    <ng-template #reportNameBody let-item>
+      <a (click)="this.editItem(item)">{{ item.formSubLabel }}</a>
+    </ng-template>
     <ng-template #actionsBody let-item let-actions="rowActions">
       <app-table-actions-button
         (tableActionClick)="$event.action.action($event.actionItem)"
@@ -23,6 +27,16 @@ import { Report } from 'app/shared/models';
   imports: [TableActionsButtonComponent, FecDatePipe],
 })
 export class SharedTemplatesComponent<T extends Report> {
+  protected readonly router = inject(Router);
+
+  readonly reportNameBodyTpl = viewChild.required<TemplateRef<TableBodyContext<T>>>('reportNameBody');
   readonly actionsBodyTpl = viewChild.required<TemplateRef<TableBodyContext<T>>>('actionsBody');
   readonly submissionBodyTpl = viewChild.required<TemplateRef<TableBodyContext<T>>>('submissionBody');
+
+  async editItem(item: T): Promise<boolean> {
+    if (item.report_status && item.report_status !== ReportStatus.IN_PROGRESS) {
+      return this.router.navigateByUrl(`/reports/${item.report_type.toLocaleLowerCase()}/submit/status/${item.id}`);
+    }
+    return this.router.navigateByUrl(`/reports/transactions/report/${item.id}/list`);
+  }
 }
