fe390922706bd1e0a1faf4fa2ebdb21bd3eb3978
David Heitzer <david.heitzer@tcg.com>
2025-11-20 19:23:16 -0500

    2663 local testing

---
+++ front-end/src/app/shared/models/transaction-list-record.model.ts
@@ -0,0 +1,25 @@
+import { plainToClass, Transform } from 'class-transformer';
+import { BaseModel } from './base.model';
+
+export class TransactionListRecord {
+  id: string | undefined;
+  transaction_type_identifier: string | undefined;
+  back_reference_tran_id_number: string | undefined;
+  form_type: string | undefined;
+  transaction_id: string | undefined;
+  line_label: string | undefined;
+  itemized: boolean | undefined;
+  force_unaggregated: boolean | undefined;
+  name: string | undefined;
+  @Transform(BaseModel.dateTransform) date: Date | undefined;
+  memo_code: boolean | undefined;
+  amount: number | undefined;
+  aggregate: number | undefined;
+  report_code_label: string | undefined;
+
+
+  // prettier-ignore
+  static fromJSON(json: any): TransactionListRecord { // eslint-disable-line @typescript-eslint/no-explicit-any
+    return plainToClass(TransactionListRecord, json);
+  }
+}
