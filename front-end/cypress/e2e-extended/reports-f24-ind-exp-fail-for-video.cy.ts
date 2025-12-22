aea75b4ddaac30dc306ad8b8349558a451e5fff3
sVmsepi0l <joshua.coombs@tcg.com>
2025-12-22 11:15:47 -0500

    same intentionally failing test added to both test suites, without video

---
+++ front-end/cypress/e2e-extended/reports-f24-ind-exp-fail-for-video.cy.ts
@@ -0,0 +1,57 @@
+import { Initialize } from '../e2e-smoke/pages/loginPage';
+import { currentYear, PageUtils } from '../e2e-smoke/pages/pageUtils';
+import { TransactionDetailPage } from '../e2e-smoke/pages/transactionDetailPage';
+import {
+  defaultScheduleFormData as defaultTransactionFormData,
+  DisbursementFormData,
+} from '../e2e-smoke//models/TransactionFormModel';
+import { DataSetup } from '../e2e-smoke//F3X/setup';
+import { StartTransaction } from '../e2e-smoke/F3X/utils/start-transaction/start-transaction';
+import { faker } from '@faker-js/faker';
+import { ReportListPage } from '../e2e-smoke/pages/reportListPage';
+import { ContactLookup } from '../e2e-smoke/pages/contactLookup';
+
+const independentExpenditureData: DisbursementFormData = {
+  ...defaultTransactionFormData,
+  date2: new Date(currentYear, 4 - 1, 27),
+  supportOpposeCode: 'SUPPORT',
+  signatoryDateSigned: new Date(currentYear, 4 - 1, 27),
+  signatoryFirstName: faker.person.firstName(),
+  signatoryLastName: faker.person.lastName(),
+};
+
+describe('Form 24 Independent Expenditures', () => {
+  beforeEach(() => {
+    Initialize();
+  });
+
+  it('Independent Expenditures created on a Form 24 should be linked to a Form 3X', () => {
+    cy.wrap(DataSetup({ individual: true, candidate: true, f24: true })).then((result: any) => {
+      ReportListPage.goToReportList(result.f24, false, true, false);
+      StartTransaction.IndependentExpenditures().IndependentExpenditure();
+      ContactLookup.getContact(result.individual.last_name, '', 'Individual');
+
+      TransactionDetailPage.enterSheduleFormDataForVoidExpenditure(
+        independentExpenditureData,
+        result.candidate,
+        false,
+        '',
+        'date_signed',
+      );
+
+      PageUtils.clickButton('Save');
+      PageUtils.clickLink('Independent Expenditure');
+      cy.contains('Address').should('exist');
+      cy.get('#first_name').should('have.value', result.individual.first_name);
+      cy.get('#last_name').should('have.value', result.individual.last_name);
+
+      ReportListPage.goToReportList(result.report);
+      PageUtils.clickSidebarItem('Manage your transactions');
+      PageUtils.clickLink('Independent Expenditure');
+      cy.contains('Address').should('exist');
+      cy.get('#first_name').should('have.value', result.individual.first_name);
+      // Making it fail on the last step
+      cy.get('#last_name').should('not.have.value', result.individual.last_name);
+    });
+  });
+});
