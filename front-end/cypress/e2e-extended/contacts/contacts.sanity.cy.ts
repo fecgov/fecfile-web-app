833f6a799ead597ffd0072a2cb7da4a0ef28de96
sVmsepi0l <joshua.coombs@tcg.com>
2025-12-24 12:27:46 -0500

    part1: three contacts, two soft deleted, one restored; part2: hard-delete of contacts is successful

--- front-end/cypress/e2e-extended/contacts/contacts.sanity.cy.ts
+++ front-end/cypress/e2e-extended/contacts/contacts.sanity.cy.ts
@@ -3,6 +3,29 @@ import { ContactListPage } from '../../e2e-smoke/pages/contactListPage';
 import { makeContact } from '../../e2e-smoke/requests/methods';
 import { Individual_A_A, MockContact } from '../../e2e-smoke/requests/library/contacts';
 import { PageUtils } from '../../e2e-smoke/pages/pageUtils';
+import { ContactsHelpers } from './contacts.helpers';
+
+function deleteContact(contactName: string) {
+  cy.contains('tbody tr', contactName, { timeout: 15000 }).should('be.visible');
+  PageUtils.clickKababItem(contactName, 'Delete');
+  PageUtils.clickButton('Confirm');
+  cy.wait('@deleteContact');
+  cy.contains('tbody tr', contactName, { timeout: 15000 }).should('not.exist');
+  cy.wait('@contactsGone');
+}
+
+function restoreContact(contactName: string) {
+  cy.intercept('POST', '**api/v1/contacts-deleted/restore/**').as('restoreContact');
+  cy.intercept('GET', '**/api/v1/contacts-deleted/?page=**').as('getDeletedContacts');
+  cy.contains('tbody tr', contactName, { timeout: 15000 })
+    .should('be.visible')
+    .find('.p-checkbox-input')
+    .check({ force: true });
+  cy.contains('button,a', 'Restore selected').click();
+  cy.wait('@restoreContact');
+  cy.wait('@getDeletedContacts');
+  cy.contains('tbody tr', contactName, { timeout: 15000 }).should('not.exist');
+}
 
 describe('Contacts Soft and Hard Delete (/contacts)', () => {
   beforeEach(() => {
@@ -11,48 +34,59 @@ describe('Contacts Soft and Hard Delete (/contacts)', () => {
     cy.intercept('GET', `**/api/v1/contacts/**page_size=**`).as(`getContactsList`);
   });
 
-  it('part1: creates two contacts, soft deletes one, doubly verifies the deletion, then verifies Restore deleted contacts button', () => {
-    const contacts: MockContact[] = [Individual_A_A, Individual_A_A];
-    cy.wait('@getContactsList');
+  /**
+   * creates three contacts, soft deletes two, verifies Restore deleted contacts button,
+   * doubly verifies the deletion of two contacts, restores one deleted contact
+   * returns to Contact List, verifies un-deleted contact and restored contact are in list
+   * verifies Restore deleted contacts button
+   */
+  it('part1', () => {
+    const contacts: MockContact[] = [Individual_A_A, Individual_A_A, Individual_A_A];
     cy.contains('button,a', 'Add contact').should('be.visible');
     cy.contains('button,a', 'Restore deleted contacts')
-      .should('not.exist').then(() => {
-        for (const [i, contact] of contacts.entries()) {
-          makeContact({
-            ...contact,
-            last_name: `${contact.last_name}${i}`,
-            first_name: `${contact.first_name}${i}`,
-          });
-        }
-    });
+      .should('not.exist');
+    for (const [i, contact] of contacts.entries()) {
+      makeContact({
+        ...contact,
+        last_name: `${contact.last_name}${i}`,
+        first_name: `${contact.first_name}${i}`,
+      });
+    }
+    ContactListPage.goToPage();
+    ContactsHelpers.assertColumnHeaders(ContactsHelpers.CONTACTS_HEADERS);
     cy.wait('@getContactsList');  
     const contactDisplayNames = contacts.map(
       (contact, i) => `${contact.last_name}${i}, ${contact.first_name}${i}`,
     );
     const contactToDelete = contactDisplayNames[0];
-    const contactToPreserve = contactDisplayNames[1];
+    const contactToRestore = contactDisplayNames[1];
+    const contactToPreserve = contactDisplayNames[2];
     cy.intercept('DELETE', '**/api/v1/contacts/**').as('deleteContact');
-    cy.contains('tbody tr', contactToDelete, { timeout: 15000 }).should('be.visible');
-    PageUtils.clickKababItem(contactToDelete, 'Delete');
-    PageUtils.clickButton('Confirm');
-    cy.wait('@deleteContact');
-    cy.wait('@getContactsList');
-    cy.contains('tbody tr', contactToDelete, { timeout: 15000 }).should('not.exist');
-    cy.intercept('GET', '**/api/v1/contacts-deleted/**').as('getDeletedContacts');
+    cy.intercept('GET', '**/api/v1/contacts-deleted/?page=1&ordering=name**').as('contactsGone');
+    deleteContact(contactToDelete);
+    deleteContact(contactToRestore);
+    cy.intercept('GET', '**/api/v1/contacts-deleted/?page=1&ordering=sort_name**').as('getDeletedContacts');
     cy.contains('button,a', 'Restore deleted contacts')
       .should('be.visible')
       .click();
     cy.wait('@getDeletedContacts');
     cy.contains('h1', 'Restore deleted contacts').should('be.visible');
     cy.contains('tbody tr', contactToDelete, { timeout: 15000 }).should('be.visible');
+    cy.contains('tbody tr', contactToRestore, { timeout: 15000 }).should('be.visible');
+    cy.contains('tbody tr', contactToPreserve, { timeout: 15000 }).should('not.exist');
+    restoreContact(contactToRestore);
+    cy.contains('tbody tr', contactToRestore, { timeout: 15000 }).should('not.exist');
     ContactListPage.goToPage();
     cy.wait('@getContactsList');
     cy.contains('tbody tr', contactToPreserve, { timeout: 15000 }).should('be.visible');
     cy.contains('button,a', 'Restore deleted contacts').should('be.visible');
   });
 
-  it('part2: triggers beforeEach--> Initialize--> deleteAllContacts(), verify Restore deleted contacts is not visible, contacts table is empty', () => {
-    cy.wait('@getContactsList');
+  /**
+   * triggers beforeEach--> Initialize--> deleteAllContacts()
+   * verifies Restore deleted contacts button is not visible and contacts table is empty
+   */
+  it('part2', () => {
     cy.contains('button,a', 'Restore deleted contacts')
       .should('not.exist');
     cy.contains('.empty-message', 'No data available in table').should('exist');
