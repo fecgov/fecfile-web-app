b5258c27bab77e2e5301dd5be754a419ac62535e
Sasha Dresden <sasha.dresden@tcg.com>
2026-01-08 15:18:21 -0500

    Update column sizing breakpoints for report tables

--- front-end/src/app/reports/report-list/form3-list/form3-list.component.ts
+++ front-end/src/app/reports/report-list/form3-list/form3-list.component.ts
@@ -1,33 +1,21 @@
-import { Component, computed, inject, TemplateRef, viewChild } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Form3 } from 'app/shared/models';
 import { AbstractFormListComponent } from '../abstract-form-list.component';
 import { Form3Service } from 'app/shared/services/form-3.service';
-import { TableBodyContext, TableComponent } from 'app/shared/components/table/table.component';
-import { FecDatePipe } from '../../../shared/pipes/fec-date.pipe';
+import { TableComponent } from 'app/shared/components/table/table.component';
 import { SharedTemplatesComponent } from '../shared-templates.component';
 
 @Component({
   selector: 'app-form3-list',
-  imports: [TableComponent, FecDatePipe, SharedTemplatesComponent],
+  imports: [TableComponent, SharedTemplatesComponent],
   templateUrl: './form3-list.component.html',
 })
 export class Form3ListComponent extends AbstractFormListComponent<Form3> {
   readonly itemService = inject(Form3Service);
-  readonly coverageBodyTpl = viewChild.required<TemplateRef<TableBodyContext<Form3>>>('coverageBody');
   override readonly caption =
     'Data table of all F3X reports created by the committee broken down by report type, coverage date, status, version, Date filed, and actions.';
 
-  readonly columns = computed(() => {
-    const columns = this.baseColumns();
-    columns.splice(1, 0, {
-      field: 'coverage_through_date',
-      header: 'Coverage',
-      sortable: true,
-      cssClass: 'coverage-column',
-      bodyTpl: this.coverageBodyTpl(),
-    });
-    return columns;
-  });
+  override readonly includeCoverage = true;
 
   protected getEmptyItem(): Form3 {
     return new Form3();
