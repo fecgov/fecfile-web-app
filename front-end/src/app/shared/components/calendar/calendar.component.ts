b429dad76b61167ab7cb2a683167b7a0aebd65ac
toddlees <97105825+toddlees@users.noreply.github.com>
2026-04-08 16:55:23 -0400

    Revert "Feature/2966 - [UI] Date field update - Suggestive text and auto formatting"

--- front-end/src/app/shared/components/calendar/calendar.component.ts
+++ front-end/src/app/shared/components/calendar/calendar.component.ts
@@ -4,13 +4,12 @@ import { SubscriptionFormControl } from 'app/shared/utils/subscription-form-cont
 import { DateUtils } from 'app/shared/utils/date.utils';
 import { DatePicker } from 'primeng/datepicker';
 import { ErrorMessagesComponent } from '../error-messages/error-messages.component';
-import { InputMaskModule } from 'primeng/inputmask';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss',
-  imports: [DatePicker, ReactiveFormsModule, ErrorMessagesComponent, InputMaskModule],
+  imports: [DatePicker, ReactiveFormsModule, ErrorMessagesComponent],
 })
 export class CalendarComponent {
   readonly form = input.required<FormGroup>();
@@ -19,27 +18,21 @@ export class CalendarComponent {
   readonly label = input.required<string>();
   readonly showErrors = input(true);
   readonly requiredErrorMessage = input('This is a required field.');
-  readonly invalidFormatMessage = input('This date does not follow the correct format, e.g. 01/01/2020');
 
   calendarOpened = false;
   readonly control = computed(() => {
     const field = this.fieldName();
     const control = this.form()?.get(field);
     if (!control) return undefined;
-
     return control as SubscriptionFormControl;
   });
 
   constructor() {
     effect(() => {
       const control = this.control();
-      const value = control?.value;
-      if (value && typeof value === 'string' && value.length === 10 && !value.includes('D')) {
-        const date = DateUtils.parseDate(value);
-
-        if (date) {
-          control.setValue(date, { emitEvent: false });
-        }
+      if (control) {
+        const date = DateUtils.parseDate(control.value);
+        control.setValue(date);
       }
     });
   }
@@ -47,19 +40,11 @@ export class CalendarComponent {
   validateDate(calendarUpdate: boolean) {
     this.calendarOpened = calendarUpdate;
     const control = this.control();
-
     if (!this.calendarOpened && control) {
-      const inputElement = document.getElementById(this.fieldName()) as HTMLInputElement;
-      const currentValue = inputElement?.value;
-
-      if (currentValue && currentValue !== 'MM/DD/YYYY') {
-        const date = new Date(currentValue);
-        if (date instanceof Date && !Number.isNaN(date.getTime())) {
-          control.setValue(date);
-        }
-      }
-
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const pendingValue = (control as any)._pendingValue;
       control.markAsTouched();
+      control.setValue(pendingValue);
       control.updateValueAndValidity();
     }
   }
