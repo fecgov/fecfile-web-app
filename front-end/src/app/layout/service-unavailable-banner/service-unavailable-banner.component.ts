fed86a30f40ea385cc0676ab784478f6fdddb4bf
Elaine Krauss <elaine.krauss@tcg.com>
2026-04-20 12:32:08 -0400

    Adds the service unavailable banner

---
+++ front-end/src/app/layout/service-unavailable-banner/service-unavailable-banner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-unavailable-banner',
+  templateUrl: './service-unavailable-banner.component.html',
+  styleUrls: ['./service-unavailable-banner.component.scss'],
+})
+export class ServiceUnavailableBannerComponent {}
