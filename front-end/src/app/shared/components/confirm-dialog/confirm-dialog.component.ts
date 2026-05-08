9213ef8034f8c90f63bcbb62618bb5b2bdcde0a1
Max Zaremba <max.zaremba@tcg.com>
2026-05-08 14:01:56 -0400

    Patch for disabled buttons on confirm dialog close

--- front-end/src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
+++ front-end/src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -21,6 +21,12 @@ export class ConfirmDialogComponent {
     effect(() => {
       if (this.confirmation()) this.visible.set(true);
     });
+    effect(() => {
+      const confirmation = this.confirmation();
+      if (!this.visible() && confirmation) {
+        confirmation.reject?.();
+      }
+    });
   }
 
   cancelOption() {
