b271bccdbd2a299deff4178b17dbd3d4fd6be52a
Sasha Dresden <sasha.dresden@tcg.com>
2026-05-11 12:15:47 -0400

    Use directive to override the registerOnChange to force MM/DD/YYYY to be treated as null before it hits the validation step.

--- front-end/src/app/shared/components/calendar/calendar.component.ts
+++ front-end/src/app/shared/components/calendar/calendar.component.ts
@@ -1,17 +1,24 @@
-import { Component, computed, effect, input, viewChild } from '@angular/core';
+import { Component, computed, input, viewChild } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { DateUtils } from 'app/shared/utils/date.utils';
 import { SubscriptionFormControl } from 'app/shared/utils/subscription-form-control';
 import { ButtonModule } from 'primeng/button';
 import { DatePicker } from 'primeng/datepicker';
 import { InputMaskModule } from 'primeng/inputmask';
 import { ErrorMessagesComponent } from '../error-messages/error-messages.component';
+import { DateSanitizerDirective } from './date-sanitize.directive';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss',
-  imports: [DatePicker, ReactiveFormsModule, ErrorMessagesComponent, InputMaskModule, ButtonModule],
+  imports: [
+    DatePicker,
+    ReactiveFormsModule,
+    ErrorMessagesComponent,
+    InputMaskModule,
+    ButtonModule,
+    DateSanitizerDirective,
+  ],
 })
 export class CalendarComponent {
   readonly form = input.required<FormGroup>();
@@ -22,50 +29,7 @@ export class CalendarComponent {
   readonly requiredErrorMessage = input('This is a required field.');
   readonly datePicker = viewChild.required(DatePicker);
   readonly invalidFormatMessage = input('This date does not follow the correct format, e.g. 01/01/2020');
-
-  readonly control = computed(() => {
-    const field = this.fieldName();
-    const control = this.form()?.get(field);
-    if (!control) return undefined;
-
-    return control as SubscriptionFormControl;
-  });
-
-  constructor() {
-    effect(() => {
-      const control = this.control();
-      const value = control?.value;
-      if (value && typeof value === 'string' && value.length === 10 && !value.includes('D')) {
-        const date = DateUtils.parseDate(value);
-
-        if (date) {
-          control.setValue(date, { emitEvent: false });
-        }
-      }
-    });
-  }
-
-  validateDate() {
-    const control = this.control();
-
-    if (control) {
-      const currentValue = this.datePicker().el.nativeElement.children[0].value;
-
-      if ((currentValue && currentValue === 'MM/DD/YYYY') || currentValue.replaceAll(/[_/]/g, '') === '') {
-        control.setValue(null);
-      } else {
-        const date = new Date(currentValue);
-        if (date instanceof Date && !Number.isNaN(date.getTime())) {
-          const isDifferentDate = !(control.value instanceof Date) || control.value.getTime() !== date.getTime();
-          if (isDifferentDate) {
-            control.setValue(date);
-            control.markAsTouched();
-            control.updateValueAndValidity();
-          }
-        }
-      }
-    }
-  }
+  readonly control = computed(() => this.form().get(this.fieldName()) as SubscriptionFormControl);
 
   onYearChange(event: Event, delta: -1 | 1) {
     const datePicker = this.datePicker();
