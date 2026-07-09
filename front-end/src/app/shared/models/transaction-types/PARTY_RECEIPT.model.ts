c9c179d5d6d9d56277619066728db14aa9cf65d3
David Heitzer <david.heitzer@tcg.com>
2026-07-09 14:32:09 -0400

    3216 F3 party returned/bounced receipt

--- front-end/src/app/shared/models/transaction-types/PARTY_RECEIPT.model.ts
+++ front-end/src/app/shared/models/transaction-types/PARTY_RECEIPT.model.ts
@@ -1,17 +1,26 @@
 import { LabelUtils } from 'app/shared/utils/label.utils';
+import { COMMITTEE, COMMITTEE_FORM_FIELDS, ELECTION_FIELDS } from 'app/shared/utils/transaction-type-properties';
 import { schema } from 'fecfile-validate/fecfile_validate_js/dist/PARTY_RECEIPT';
+import { ReportTypes } from '..';
 import { SchATransactionType } from '../scha-transaction-type.model';
 import { SchATransaction, ScheduleATransactionTypeLabels, ScheduleATransactionTypes } from '../scha-transaction.model';
 import { STANDARD_CONTROLS, TransactionNavigationControls } from '../transaction-navigation-controls.model';
 import { AggregationGroups } from '../transaction.model';
-import { COMMITTEE, COMMITTEE_FORM_FIELDS } from 'app/shared/utils/transaction-type-properties';
 
 export class PARTY_RECEIPT extends SchATransactionType {
-  formFields = COMMITTEE_FORM_FIELDS;
+  formFields = [...COMMITTEE_FORM_FIELDS, ...ELECTION_FIELDS];
   contactTypeOptions = COMMITTEE;
   title = LabelUtils.get(ScheduleATransactionTypeLabels, ScheduleATransactionTypes.PARTY_RECEIPT);
   schema = schema;
   override navigationControls: TransactionNavigationControls = STANDARD_CONTROLS;
+
+  override hasElectionInformation(report_type: ReportTypes): boolean {
+    return [ReportTypes.F3].includes(report_type);
+  }
+
+  override get isReattributable() {
+    return false;
+  }
 
   getNewTransaction() {
     return SchATransaction.fromJSON({
