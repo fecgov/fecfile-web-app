c1a88c0339138f5b2f252513fc3795470d40455c
Sasha Dresden <sasha.dresden@tcg.com>
2026-08-13 16:09:02 -0400

    Clean up secutiy notice page

--- front-end/src/app/login/security-notice/security-notice.component.ts
+++ front-end/src/app/login/security-notice/security-notice.component.ts
@@ -1,59 +1,74 @@
-import { Component, effect, inject, OnInit, signal } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, computed, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { LoginService } from 'app/shared/services/login.service';
 import { UsersService } from 'app/shared/services/users.service';
-import { blurActiveInput, printFormErrors } from 'app/shared/utils/form.utils';
-import { SubscriptionFormControl } from 'app/shared/utils/subscription-form-control';
-import { singleClickEnableAction } from 'app/store/single-click.actions';
 import { userLoginDataUpdatedAction } from 'app/store/user-login-data.actions';
 import { selectUserLoginData } from 'app/store/user-login-data.selectors';
-import { Checkbox } from 'primeng/checkbox';
 import { ButtonModule } from 'primeng/button';
 import { environment } from 'environments/environment';
 import { ProdNoticeComponent } from './prod-notice.component';
 import { DevNoticeComponent } from './dev-notice.component';
 import { NgComponentOutlet } from '@angular/common';
+import { injectRouteData } from 'ngxtension/inject-route-data';
+import { SignalFormComponent } from 'app/shared/components/signal-form/signal-form.component';
+import { form, FormRoot, FormField, disabled } from '@angular/forms/signals';
+import { CheckboxInput } from 'app/shared/components/signal-inputs/checkbox-input/checkbox.input';
 
 export const SECURITY_CONSENT_VERSION = '1';
-
+interface SecurityConsent {
+  annualConsent: boolean;
+}
 @Component({
   selector: 'app-security-notice',
   templateUrl: './security-notice.component.html',
   styleUrls: ['./security-notice.component.scss'],
-  imports: [ReactiveFormsModule, Checkbox, ButtonModule, NgComponentOutlet],
+  imports: [ButtonModule, NgComponentOutlet, FormRoot, CheckboxInput, FormField],
 })
-export class SecurityNoticeComponent implements OnInit {
+export class SecurityNoticeComponent extends SignalFormComponent<SecurityConsent> {
   private readonly store = inject(Store);
   private readonly router = inject(Router);
   readonly loginService = inject(LoginService);
   private readonly usersService = inject(UsersService);
-  private readonly activatedRoute = inject(ActivatedRoute);
-  formSubmitted = false;
-  showForm = true;
+  readonly backgroundStyle = injectRouteData('backgroundStyle');
+
+  readonly showForm = computed(() => !!this.backgroundStyle());
   readonly userLoginData = this.store.selectSignal(selectUserLoginData);
 
   readonly hasScrolledToBottom = signal(false);
 
-  readonly form = new FormGroup(
-    { 'security-consent-annual': new SubscriptionFormControl(false) },
-    { updateOn: 'blur' },
+  readonly model = signal<SecurityConsent>({ annualConsent: false });
+  readonly form = form(
+    this.model,
+    (schema) => {
+      disabled(schema.annualConsent, () => !this.hasScrolledToBottom());
+    },
+    {
+      submission: {
+        action: async () => {
+          if (!this.hasScrolledToBottom()) return;
+          const updatedUserLoginData = {
+            ...this.userLoginData(),
+            security_consent_version: SECURITY_CONSENT_VERSION,
+          };
+
+          updatedUserLoginData.consent_for_one_year = this.form.annualConsent().value();
+
+          const retval = await this.usersService.updateCurrentUser(updatedUserLoginData);
+          this.store.dispatch(
+            userLoginDataUpdatedAction({
+              payload: {
+                ...retval,
+                security_consent_version_at_login: SECURITY_CONSENT_VERSION,
+              },
+            }),
+          );
+          this.router.navigate(['/select-committee']);
+        },
+      },
+    },
   );
   readonly componentToLoad = environment.name === 'test' ? DevNoticeComponent : ProdNoticeComponent;
-
-  constructor() {
-    this.activatedRoute.data.subscribe((d) => (this.showForm = !!d['backgroundStyle']));
-
-    effect(() => {
-      this.userLoginData();
-      this.formSubmitted = false;
-    });
-  }
-
-  ngOnInit() {
-    this.formSubmitted = false;
-  }
 
   onScroll(event: Event): void {
     const element = event.target as HTMLElement;
@@ -64,33 +79,5 @@ export class SecurityNoticeComponent implements OnInit {
     if (isAtBottom && !this.hasScrolledToBottom()) {
       this.hasScrolledToBottom.set(true);
     }
-  }
-
-  async signConsentForm() {
-    if (!this.hasScrolledToBottom()) return;
-    this.formSubmitted = true;
-    blurActiveInput(this.form);
-    if (this.form.invalid) printFormErrors(this.form);
-    if (this.form.invalid || !this.userLoginData()) {
-      this.store.dispatch(singleClickEnableAction());
-      return;
-    }
-    const updatedUserLoginData = {
-      ...this.userLoginData(),
-      security_consent_version: SECURITY_CONSENT_VERSION,
-    };
-
-    updatedUserLoginData.consent_for_one_year = this.form.get('security-consent-annual')?.value;
-
-    const retval = await this.usersService.updateCurrentUser(updatedUserLoginData);
-    this.store.dispatch(
-      userLoginDataUpdatedAction({
-        payload: {
-          ...retval,
-          security_consent_version_at_login: SECURITY_CONSENT_VERSION,
-        },
-      }),
-    );
-    this.router.navigate(['/select-committee']);
   }
 }
---
+++ front-end/src/app/shared/components/signal-inputs/checkbox-input/checkbox.input.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CheckboxInputComponent } from './checkbox.input';
+
+describe('CheckboxInputComponent', () => {
+  let component: CheckboxInputComponent;
+  let fixture: ComponentFixture<CheckboxInputComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CheckboxInputComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CheckboxInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
