c80a65102ac78266ec180c415dd20d4296d012b3
Sasha Dresden <sasha.dresden@tcg.com>
2026-09-02 13:04:45 -0400

    Missed providing to unassigned transactions

--- front-end/src/app/routes.ts
+++ front-end/src/app/routes.ts
@@ -9,6 +9,7 @@ import { committeeOwnerGuard } from './shared/guards/committee-owner.guard';
 import { CookiesDisabledComponent } from './shared/components/cookies-disabled/cookies-disabled.component';
 import { UnassociatedTransactionListComponent } from './unassociated-transaction-list/unassociated-transaction-list.component';
 import { featureFlagGuard } from 'app/shared/guards/feature-flag.guard';
+import { ReattRedesStore } from './shared/utils/reatt-redes/reatt-redes.store';
 
 export const ROUTES: Route[] = [
   {
@@ -59,6 +60,7 @@ export const ROUTES: Route[] = [
           committeeGuard,
           featureFlagGuard('showAllTransactionsPage'),
         ],
+        providers: [ReattRedesStore],
       },
       {
         path: 'contacts',
