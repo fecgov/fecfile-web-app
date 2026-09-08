17e2fd10216def582daeaff051e1be82bcf310f2
Elaine Krauss <elaine.krauss@tcg.com>
2026-09-08 09:14:23 -0400

    Adds a test for spam clicking the standard save button

--- front-end/cypress/e2e-smoke/F3X/single-click-protection.cy.ts
+++ front-end/cypress/e2e-smoke/F3X/single-click-protection.cy.ts
@@ -1,5 +1,4 @@
 import { ContactListPage } from '../pages/contactListPage';
-import { TransactionTableColumns } from '../pages/f3xTransactionListPage';
 import { Initialize } from '../pages/loginPage';
 import { currentYear, PageUtils } from '../pages/pageUtils';
 import { TransactionDetailPage } from '../pages/transactionDetailPage';
@@ -17,30 +16,12 @@ const scheduleData = {
   date_received: new Date(currentYear, 4 - 1, 27),
 };
 
-function checkTable(index: number, type: string, containMemo: boolean, value: string) {
-  cy.get('tbody tr').eq(index).as('row');
-
-  // this block of checks is here to ensure all columns exist
-  // (and to prove to knip that every part of the TransactionTableColumns enum is needed)
-  cy.get('@row').find('td').eq(TransactionTableColumns.line_number).should('exist');
-  cy.get('@row').find('td').eq(TransactionTableColumns.date).should('exist');
-  cy.get('@row').find('td').eq(TransactionTableColumns.amount).should('exist');
-  cy.get('@row').find('td').eq(TransactionTableColumns.actions).should('exist');
-
-  cy.get('@row').find('td').eq(TransactionTableColumns.transaction_type).should('contain', type);
-  cy.get('@row')
-    .find('td')
-    .eq(TransactionTableColumns.memo_code)
-    .should(containMemo ? 'contain' : 'not.contain', 'Y');
-  cy.get('@row').find('td').eq(TransactionTableColumns.aggregate).should('contain', value);
-}
-
 describe('Single Click Protected Buttons', () => {
   beforeEach(() => {
     Initialize();
   });
 
-  it('Create an Individual Receipt and spam the save button', () => {
+  it('Spam the save button', () => {
     cy.wrap(DataSetup({ individual: true })).then((result: any) => {
       ReportListPage.gotToReportTransactionListPage(result.report);
       StartTransaction.Receipts().Individual().IndividualReceipt();
@@ -49,25 +30,58 @@ describe('Single Click Protected Buttons', () => {
       ContactLookup.getContact(individual.last_name);
 
       TransactionDetailPage.enterScheduleFormData(scheduleData, false, '', true, 'contribution_date');
-      TransactionDetailPage.clickSave();
-      scheduleData.date_received = new Date(currentYear, 4 - 1, 27);
-      cy.get('tr').should('contain', 'Individual Receipt');
-      cy.get('tr').should('contain', 'Unitemized');
-      cy.get('tr').should('contain', `${individual['last_name']}, ${individual['first_name']}`);
-      cy.get('tr').should('contain', PageUtils.dateToString(scheduleData.date_received));
-      cy.get('tr').should('contain', '$' + scheduleData.amount);
 
-      // Check values of edit form
-      PageUtils.clickLink('Individual Receipt');
-      cy.get('#entity_type_dropdown.readonly').should('exist');
-      cy.get('#entity_type_dropdown').should('contain', 'Individual');
-      ContactListPage.assertFormData(individual, true);
-      TransactionDetailPage.assertFormData(scheduleData, '', '#contribution_date');
+      let requestCount = 0;
+      cy.intercept('POST', '**/api/v1/transactions/', (request)=>{
+        requestCount += 1;
 
-      while (true) {
-        TransactionDetailPage.clickSave();
-        cy.wait(100);
+        request.on('response', (response)=>{
+          response.setDelay(250);
+        });
+      });
+
+      for (let i = 0; i < 10; i++) {
+        cy.contains('Save').first().click();
       }
+
+      cy.waitForNetworkIdle(1000).then(()=>{
+        expect(requestCount).to.equal(1);
+      });
+    });
+  });
+
+  xit('Spam the save and clone button', () => {
+    cy.wrap(DataSetup({ individual: true })).then((result: any) => {
+      ReportListPage.gotToReportTransactionListPage(result.report);
+      StartTransaction.Receipts().Individual().IndividualReceipt();
+
+      const individual: ContactFormData = result.individual;
+      ContactLookup.getContact(individual.last_name);
+
+      TransactionDetailPage.enterScheduleFormData(scheduleData, false, '', true, 'contribution_date');
+
+      let requestCount = 0;
+      cy.intercept('POST', '**/api/v1/transactions/', (request)=>{
+        requestCount += 1;
+
+        request.on('response', (response)=>{
+          response.setDelay(250);
+        });
+      });
+
+      // This doesn't test reliably.
+      // Force-clicking is required for consistency, but force-clicking ignores
+      // the single click protections.  A user's standard click would not override
+      // the disabling of the button, so a force-click doing so doesn't seem like
+      // a fair fail state for the test...
+      for (let i = 0; i < 10; i++){
+        cy.get('.p-splitbutton-dropdown').click();
+        cy.get('.p-tieredmenu-item-content').eq(1).click();
+      }
+
+      cy.waitForNetworkIdle(1000).then(()=>{
+        expect(requestCount).to.equal(1);
+      });
     });
   });
 });
